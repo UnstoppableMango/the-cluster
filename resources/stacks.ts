0385077c21ff1b185eacecf734b899c496f7cfed
Erik Rasmussen <ecr.referee@hotmail.com>
2020-11-15 23:57:56 -0600

    Stub out pulumi operator stacks resource

---
+++ resources/stacks.ts
@@ -0,0 +1,21 @@
+import { ComponentResource, ComponentResourceOptions, Config } from '@pulumi/pulumi';
+import { Secret } from '@pulumi/kubernetes/core/v1';
+
+export class OperatorStacks extends ComponentResource {
+
+  private readonly _opts = {
+    provider: this.opts?.provider,
+    parent: this,
+  };
+
+  private readonly _config = new Config();
+
+  private readonly _accessToken = new Secret('access-token', {
+    data: { accessToken: this._config.requireSecret('pulumiAccessToken') },
+  }, this._opts);
+
+  constructor(name: string, private opts?: ComponentResourceOptions) {
+    super('unmango:pulumi:stacks', name, undefined, opts);
+  }
+
+}
