018a7fbdc43818ada51638f231713a842e9fa2a9
Erik Rasmussen <ecr.referee@hotmail.com>
2020-11-22 03:00:49 -0600

    Add flatten function

--- external/util.ts
+++ external/util.ts
@@ -4,3 +4,30 @@ export function toKebabCase(...src: string[]): string {
     .map(x => x.toLowerCase())
     .join('-');
 }
+
+// https://stackoverflow.com/questions/19098797/fastest-way-to-flatten-un-flatten-nested-json-objects
+export const flatten = (data: Record<string, unknown>): Record<string, unknown> => {
+  const result: Record<string, unknown> = {};
+  const recurse = (cur: Record<string, unknown>, prop: string) => {
+    if (Object(cur) !== cur) {
+      result[prop] = cur;
+    } else if (Array.isArray(cur)) {
+      let l = 0;
+      for(let i = 0, l = cur.length; i < l; i++)
+        recurse(cur[i], prop + '[' + i + ']');
+      if (l == 0)
+        result[prop] = [];
+    } else {
+      let isEmpty = true;
+      for (const p in cur) {
+        isEmpty = false;
+        // @ts-ignore
+        recurse(cur[p], prop ? prop + '.' + p : p);
+      }
+      if (isEmpty && prop)
+        result[prop] = {};
+    }
+  };
+  recurse(data, '');
+  return result;
+};
