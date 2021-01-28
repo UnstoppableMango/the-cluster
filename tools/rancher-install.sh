f7e2bddeced111fab1cf3e9ae0cd80c9c7683fe6
Erik Rasmussen <ecr.referee@hotmail.com>
2021-01-27 23:32:06 -0600

    Add some helm stuff to the rancher install script

--- tools/rancher-install.sh
+++ tools/rancher-install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
+helm repo update
+
+# https://stackoverflow.com/a/65411733/7341217
+# Create if not extists
+kubectl create namespace cattle-system --dry-run=client -o yaml | kubectl apply -f -
+
 helm install rancher rancher-latest/rancher \
     --namespace cattle-system \
     --set hostname=rancher.int.unmango.net \
