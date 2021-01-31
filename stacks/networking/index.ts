268b0fbe4e547d1bbd0a7b5eed111323bab6202a
Erik Rasmussen <ecr.referee@hotmail.com>
2021-01-31 13:56:43 -0600

    Comment out everything until we can get crap working

--- stacks/networking/index.ts
+++ stacks/networking/index.ts
@@ -1,32 +1,26 @@
 import * as pulumi from '@pulumi/pulumi';
 import * as rancher from '@pulumi/rancher2';
-import * as yaml from 'yaml';
 import { CatalogsExport } from '@unmango/shared';
 import { MetalLb, Traefik } from './resources';
 
 const config = new pulumi.Config();
 const remoteStack = config.require('remoteStack');
 
-const rancherRef = new pulumi.StackReference(`UnstoppableMango/rancher/${remoteStack}`);
-const theCluster = rancher.Cluster.get('local', rancherRef.requireOutput('clusterId'));
-const catalogs = rancherRef.requireOutput('catalogs').apply(x => x as CatalogsExport);
-const bitnamiCatalog = rancher.Catalog.get('bitnami', catalogs.bitnamiId);
+// const rancherRef = new pulumi.StackReference(`UnstoppableMango/rancher/${remoteStack}`);
+// const theCluster = rancher.Cluster.get('local', rancherRef.requireOutput('clusterId'));
+// const catalogs = rancherRef.requireOutput('catalogs').apply(x => x as CatalogsExport);
+// const bitnamiCatalog = rancher.Catalog.get('bitnami', catalogs.bitnamiId);
 
-const project = new rancher.Project('networking', {
-  name: 'Networking',
-  clusterId: theCluster.id,
-});
-
-const metallbNs = new rancher.Namespace('metallb', {
-  name: 'metallb-system',
-  projectId: project.id,
-});
+// const project = new rancher.Project('networking', {
+//   name: 'Networking',
+//   clusterId: theCluster.id,
+// });
 
 const metallb = new MetalLb('metallb', {
-  projectId: project.id,
-  catalogName: bitnamiCatalog.name,
-  version: '2.1.2',
-  namespace: metallbNs.name,
+  // projectId: project.id,
+  // catalogName: bitnamiCatalog.name,
+  // version: '2.1.2',
+  version: '0.9.5',
   addresses: ['192.168.1.75-192.168.1.99'],
 });
 
