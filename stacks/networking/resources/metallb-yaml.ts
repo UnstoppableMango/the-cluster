f144ea7358745a8c4c1c781de453fd4345b09612
Erik Rasmussen <ecr.referee@hotmail.com>
2021-01-31 14:08:33 -0600

    Use k8s to import

--- stacks/networking/resources/metallb-yaml.ts
+++ stacks/networking/resources/metallb-yaml.ts
@@ -1,51 +1,40 @@
-import { ConfigMap, Namespace, Secret } from '@pulumi/kubernetes/core/v1';
-import { ConfigFile } from '@pulumi/kubernetes/yaml';
+import * as k8s from '@pulumi/kubernetes';
 import { ComponentResource, ComponentResourceOptions, Input } from '@pulumi/pulumi';
 import { RandomId } from '@pulumi/random';
 import * as yaml from 'yaml';
 
 export class MetalLb extends ComponentResource {
 
-  public readonly ns: ConfigFile;
-  public readonly deployment: ConfigFile;
-  public readonly secret: Secret;
-  public readonly config: ConfigMap;
+  public readonly namespace: k8s.yaml.ConfigFile;
+  public readonly deployment: k8s.yaml.ConfigFile;
+  public readonly secret: k8s.core.v1.Secret;
+  public readonly config: k8s.core.v1.ConfigMap;
 
   constructor(name: string, args: MetalLbArgs, opts?: ComponentResourceOptions) {
     super('unmango:apps:metallb', name, undefined, opts);
 
     const baseUrl = `https://raw.githubusercontent.com/metallb/metallb/v${args.version}/manifests`;
 
-    this.ns = new ConfigFile('namespace', {
+    const ns = 'metallb-system';
+    this.namespace = new k8s.yaml.ConfigFile('namespace', {
       file: `${baseUrl}/namespace.yaml`,
     }, { parent: this });
 
-    this.deployment = new ConfigFile('metallb', {
+    this.deployment = new k8s.yaml.ConfigFile('metallb', {
       file: `${baseUrl}/metallb.yaml`,
-    }, { parent: this, dependsOn: this.ns });
-
-    const namespace = Namespace.get('namespace', 'metallb-system', {
-      parent: this,
-      dependsOn: this.ns,
-    });
+    }, { parent: this, dependsOn: this.namespace });
 
     const rand = new RandomId('secret', {
       byteLength: 128,
     }, { parent: this });
 
-    this.secret = new Secret('memberlist', {
-      metadata: {
-        name: 'memberlist',
-        namespace: namespace.metadata.name,
-      },
+    this.secret = new k8s.core.v1.Secret('memberlist', {
+      metadata: { name: 'memberlist', namespace: ns },
       data: { secretkey: rand.b64Std },
-    }, { parent: this, dependsOn: this.ns });
+    }, { parent: this, dependsOn: this.namespace });
 
-    this.config = new ConfigMap('metallb', {
-      metadata: {
-        name: 'metallb-config',
-        namespace: namespace.metadata.name,
-      },
+    this.config = new k8s.core.v1.ConfigMap('metallb', {
+      metadata: { name: 'metallb-config', namespace: ns },
       data: {
         config: yaml.stringify({
           'address-pools': [{
