c753a912ded4d387229a76a664fa21caeac66a89
Erik Rasmussen <ecr.referee@hotmail.com>
2021-02-04 22:32:34 -0600

    Cleanup rancher stuff

--- stacks/rancher/index.ts
+++ stacks/rancher/index.ts
@@ -1,25 +1,17 @@
 import * as pulumi from '@pulumi/pulumi';
 import * as rancher2 from '@pulumi/rancher2';
 import { createExport } from '@unmango/shared';
-import { Catalogs, CertManager, OperatorStacks, Rancher } from './resources';
+import { Catalogs, OperatorStacks } from './resources';
 
 const config = new pulumi.Config();
 
-// const certManager = new CertManager('cert-manager', {
-//   version: '1.1.0',
-// });
+const cluster = rancher2.Cluster.get('local', 'local');
 
-// const rancher = new Rancher('rancher', {}, { dependsOn: certManager });
-
-// const cluster = rancher2.Cluster.get('local', 'local', undefined, { dependsOn: rancher });
-
-// const clusterCatalogs = new Catalogs('catalogs', {
-//   clusterId: cluster.id,
-// }, { dependsOn: rancher });
+const clusterCatalogs = new Catalogs('catalogs', { clusterId: cluster.id });
 
 // const stacks = new OperatorStacks('the-cluster');
 
-// export const clusterId = cluster.id;
-// export const defaultProjectId = cluster.defaultProjectId;
+export const clusterId = cluster.id;
+export const defaultProjectId = cluster.defaultProjectId;
 
-// export const catalogs = createExport(clusterCatalogs);
+export const catalogs = createExport(clusterCatalogs);
