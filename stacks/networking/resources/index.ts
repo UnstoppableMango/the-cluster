38067417abe38567f5d4ebcdae36b79165fa7433
Erik Rasmussen <ecr.referee@hotmail.com>
2021-02-04 21:20:05 -0600

    Add tls store

--- stacks/networking/resources/index.ts
+++ stacks/networking/resources/index.ts
@@ -2,6 +2,7 @@ export * from './ingress-route';
 export * from './metallb-helm';
 // export * from './metallb-rancher';
 // export * from './metallb-yaml';
+export * from './tls-store';
 export * from './traefik-config';
 export * from './traefik-helm';
 // export * from './traefik-rancher';
