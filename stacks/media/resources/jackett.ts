7a021c4cee9396c34ab398cb24995859ce50d26a
Erik Rasmussen <ecr.referee@hotmail.com>
2021-02-04 23:57:37 -0600

    Refactor Jackett and add ingress

--- stacks/media/resources/jackett.ts
+++ stacks/media/resources/jackett.ts
@@ -1,65 +1,90 @@
+import * as k8s from '@pulumi/kubernetes';
 import * as kx from '@pulumi/kubernetesx';
 import { ComponentResource, ComponentResourceOptions, Input } from '@pulumi/pulumi';
+import { getNameResolver } from '@unmango/shared/util';
 
 export class Jackett extends ComponentResource {
 
-  public readonly config = new kx.PersistentVolumeClaim(this.getName('config'), {
-    metadata: { namespace: this.args.namespace },
-    spec: {
-      storageClassName: 'longhorn',
-      accessModes: ['ReadWriteOnce'],
-      resources: { requests: { storage: '1Gi' } },
-    },
-  }, { parent: this });
+  private readonly getName = getNameResolver('jackett', this.name);
 
-  private readonly _pb = new kx.PodBuilder({
-    containers: [{
-      // kx sets the selector to the container name.
-      // With multiple resources, it won't match correctly, so
-      // this is mostly a hack to get service discovery to work.
-      name: this.getName(),
-      securityContext: {
-        privileged: true,
-      },
-      image: 'linuxserver/jackett',
-      envFrom: [{
-        configMapRef: { name: this.args.linuxServer.metadata.name },
-      }],
-      env: {
-        AUTO_UPDATE: 'true', // Optional
-        // RUN_OPTS: '', // Optional
-      },
-      ports: {
-        http: 9117,
-      },
-      volumeMounts: [
-        this.config.mount('/config'),
-        // Docs mention needing a mount to pass .torrent files to
-        // the download client... add back if needed?
-        // this.args.downloads.mount('/downloads'),
-      ],
-    }],
-  });
-
-  public readonly deployment = new kx.Deployment(this.getName(), {
-    metadata: { namespace: this.args.namespace },
-    spec: this._pb.asDeploymentSpec(),
-  }, { parent: this });
-
-  public readonly service = this.deployment.createService({
-    type: kx.types.ServiceType.LoadBalancer,
-    ports: [{ name: 'http', port: 9117, targetPort: 9117 }],
-  })
+  public readonly config: kx.PersistentVolumeClaim;
+  public readonly deployment: kx.Deployment;
+  public readonly service: kx.Service;
+  public readonly ingress: k8s.networking.v1.Ingress;
 
   constructor(private name: string, private args: JackettArgs, opts?: ComponentResourceOptions) {
     super('unmango:apps:jackett', name, undefined, opts);
-    this.registerOutputs();
-  }
 
-  private getName(name?: string): string {
-    let values = [...new Set(['jackett', this.name])];
-    if (name) values = values.concat(name);
-    return values.join('-');
+    this.config = new kx.PersistentVolumeClaim(this.getName('config'), {
+      metadata: { namespace: this.args.namespace },
+      spec: {
+        storageClassName: 'longhorn',
+        accessModes: ['ReadWriteOnce'],
+        resources: { requests: { storage: '1Gi' } },
+      },
+    }, { parent: this });
+  
+    const pb = new kx.PodBuilder({
+      containers: [{
+        // kx sets the selector to the container name.
+        // With multiple resources, it won't match correctly, so
+        // this is mostly a hack to get service discovery to work.
+        name: this.getName(),
+        securityContext: {
+          privileged: true,
+        },
+        image: 'linuxserver/jackett',
+        envFrom: [{
+          configMapRef: { name: this.args.linuxServer.metadata.name },
+        }],
+        env: {
+          AUTO_UPDATE: 'true', // Optional
+          // RUN_OPTS: '', // Optional
+        },
+        ports: {
+          http: 9117,
+        },
+        volumeMounts: [
+          this.config.mount('/config'),
+          // Docs mention needing a mount to pass .torrent files to
+          // the download client... add back if needed?
+          // this.args.downloads.mount('/downloads'),
+        ],
+      }],
+    });
+  
+    this.deployment = new kx.Deployment(this.getName(), {
+      metadata: { namespace: this.args.namespace },
+      spec: pb.asDeploymentSpec(),
+    }, { parent: this });
+  
+    this.service = this.deployment.createService({
+      type: kx.types.ServiceType.LoadBalancer,
+      ports: [{ name: 'http', port: 9117, targetPort: 9117 }],
+    });
+
+    this.ingress = new k8s.networking.v1.Ingress('heimdall', {
+      metadata: { namespace: args.namespace },
+      spec: {
+        rules: [{
+          host: `${this.name}.int.unmango.net`,
+          http: {
+            paths: [{
+              backend: {
+                service: {
+                  name: this.service.metadata.name,
+                  port: { name: 'http' },
+                },
+              },
+              // TODO: Required ✓, Correct?
+              pathType: 'ImplementationSpecific',
+            }],
+          },
+        }],
+      },
+    }, { parent: this });
+
+    this.registerOutputs();
   }
 
 }
