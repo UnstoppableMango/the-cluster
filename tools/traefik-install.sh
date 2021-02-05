f2d080723c387223aed8d873098c5863ba3df5cd
Erik Rasmussen <ecr.referee@hotmail.com>
2021-02-04 21:17:34 -0600

    Update traefik install script

--- tools/traefik-install.sh
+++ tools/traefik-install.sh
@@ -5,21 +5,22 @@ if [ ! -z "$1" ]; then
   VERSION=$1
 fi
 
-# kubectl apply --validate=false \
-#     -f https://github.com/jetstack/cert-manager/releases/download/v$VERSION/cert-manager.crds.yaml
-
 # https://stackoverflow.com/a/65411733/7341217
 # Create if not extists
-# kubectl create namespace cert-manager --dry-run=client -o yaml | kubectl apply -f -
+kubectl create namespace traefik-system --dry-run=client -o yaml | kubectl apply -f -
 
 helm repo add traefik https://helm.traefik.io/traefik
-
-# Update your local Helm chart repository cache
 helm repo update
 
 helm install \
   traefik traefik/traefik \
+  --namespace traefik-system \
   --version v$VERSION \
   --set logs.general.level="DEBUG" \
   --set pilot.enabled=true \
   --set pilot.token=$(pulumi config get --path traefik.pilot.token -C ../stacks/networking) \
+  --set "ports.web.redirectTo=websecure" \
+  --set "ports.websecure.tls.enabled=true" \
+  # --set "volumes[0].name=default-tls-cert" \
+  # --set "volumes[0].mountPath=/certs" \
+  # --set "volumes[0].type=secret" \
