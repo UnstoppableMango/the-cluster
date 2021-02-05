a91bd95cfe741e559ef74172596c2d2fa6a14f59
Erik Rasmussen <ecr.referee@hotmail.com>
2021-02-05 00:07:06 -0600

    Refactor lidarr

--- stacks/media/resources/lidarr.ts
+++ stacks/media/resources/lidarr.ts
@@ -1,71 +1,96 @@
 import * as k8s from '@pulumi/kubernetes';
 import * as kx from '@pulumi/kubernetesx';
 import { ComponentResource, ComponentResourceOptions, Input } from '@pulumi/pulumi';
+import { getNameResolver } from '@unmango/shared/util';
 
 export class Lidarr extends ComponentResource {
 
-  public readonly config = new kx.PersistentVolumeClaim(this.getName('config'), {
-    metadata: { namespace: this.args.namespace },
-    spec: {
-      storageClassName: 'longhorn',
-      accessModes: ['ReadWriteOnce'],
-      resources: { requests: { storage: '2Gi' } },
-    },
-  }, { parent: this });
+  private readonly getName = getNameResolver('lidarr', this.name);
 
-  public readonly media = new kx.PersistentVolumeClaim(this.getName('media'), {
-    metadata: { namespace: this.args.namespace },
-    spec: {
-      accessModes: ['ReadWriteOnce', 'ReadOnlyMany'],
-      resources: { requests: { storage: '5000Gi' } },
-      storageClassName: 'nfs',
-      volumeName: this.args.musicVolume.metadata.name,
-    },
-  }, { parent: this });
-
-  private readonly _pb = new kx.PodBuilder({
-    containers: [{
-      // kx sets the selector to the container name.
-      // With multiple resources, it won't match correctly, so
-      // this is mostly a hack to get service discovery to work.
-      name: this.getName(),
-      securityContext: {
-        privileged: true,
-      },
-      image: 'linuxserver/lidarr',
-      envFrom: [{
-        configMapRef: { name: this.args.linuxServer.metadata.name },
-      }],
-      ports: {
-        http: 8686,
-      },
-      volumeMounts: [
-        this.config.mount('/config'),
-        this.media.mount('/music'),
-        this.args.downloads.mount('/downloads', 'completed'),
-      ],
-    }],
-  });
-
-  public readonly deployment = new kx.Deployment(this.getName(), {
-    metadata: { namespace: this.args.namespace },
-    spec: this._pb.asDeploymentSpec(),
-  }, { parent: this });
-
-  public readonly service = this.deployment.createService({
-    type: kx.types.ServiceType.LoadBalancer,
-    ports: [{ name: 'http', port: 8686, targetPort: 8686 }],
-  })
+  public readonly config: kx.PersistentVolumeClaim;
+  public readonly media: kx.PersistentVolumeClaim;
+  public readonly deployment: kx.Deployment;
+  public readonly service: kx.Service;
+  public readonly ingress: k8s.networking.v1.Ingress;
 
   constructor(private name: string, private args: LidarrArgs, opts?: ComponentResourceOptions) {
     super('unmango:apps:lidarr', name, undefined, opts);
-    this.registerOutputs();
-  }
 
-  private getName(name?: string): string {
-    let values = [...new Set(['lidarr', this.name])];
-    if (name) values = values.concat(name);
-    return values.join('-');
+    this.config = new kx.PersistentVolumeClaim(this.getName('config'), {
+      metadata: { namespace: this.args.namespace },
+      spec: {
+        storageClassName: 'longhorn',
+        accessModes: ['ReadWriteOnce', 'ReadWriteMany'],
+        resources: { requests: { storage: '2Gi' } },
+      },
+    }, { parent: this });
+  
+    this.media = new kx.PersistentVolumeClaim(this.getName('media'), {
+      metadata: { namespace: this.args.namespace },
+      spec: {
+        accessModes: ['ReadWriteOnce', 'ReadOnlyMany'],
+        resources: { requests: { storage: '5000Gi' } },
+        storageClassName: 'nfs',
+        volumeName: this.args.musicVolume.metadata.name,
+      },
+    }, { parent: this });
+  
+    const pb = new kx.PodBuilder({
+      containers: [{
+        // kx sets the selector to the container name.
+        // With multiple resources, it won't match correctly, so
+        // this is mostly a hack to get service discovery to work.
+        name: this.getName(),
+        securityContext: {
+          privileged: true,
+        },
+        image: 'linuxserver/lidarr',
+        envFrom: [{
+          configMapRef: { name: this.args.linuxServer.metadata.name },
+        }],
+        ports: {
+          http: 8686,
+        },
+        volumeMounts: [
+          this.config.mount('/config'),
+          this.media.mount('/music'),
+          this.args.downloads.mount('/downloads', 'completed'),
+        ],
+      }],
+    });
+  
+    this.deployment = new kx.Deployment(this.getName(), {
+      metadata: { namespace: this.args.namespace },
+      spec: pb.asDeploymentSpec(),
+    }, { parent: this });
+  
+    this.service = this.deployment.createService({
+      type: kx.types.ServiceType.LoadBalancer,
+      ports: [{ name: 'http', port: 8686, targetPort: 8686 }],
+    });
+
+    this.ingress = new k8s.networking.v1.Ingress(this.getName('ingress'), {
+      metadata: { namespace: args.namespace },
+      spec: {
+        rules: [{
+          host: `${this.name}.int.unmango.net`,
+          http: {
+            paths: [{
+              backend: {
+                service: {
+                  name: this.service.metadata.name,
+                  port: { name: 'http' },
+                },
+              },
+              // TODO: Required ✓, Correct?
+              pathType: 'ImplementationSpecific',
+            }],
+          },
+        }],
+      },
+    }, { parent: this });
+
+    this.registerOutputs();
   }
 
 }
