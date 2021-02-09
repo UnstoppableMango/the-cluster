f9c4e41745695e8cc6c79d75a1fe35137ff42cb3
Erik Rasmussen <ecr.referee@hotmail.com>
2021-02-08 22:31:43 -0600

    Mostly rider crap + delete appsettings

--- stacks/media/containers/service-connector/Program.cs
+++ stacks/media/containers/service-connector/Program.cs
@@ -1,21 +1,20 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace service_connector
 {
-    public class Program
+    public static class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureHostConfiguration(builder => {
+                    builder.AddKeyPerFile("/config");
+                })
+                .ConfigureAppConfiguration((hostContext, builder) => {
+                })
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.AddHostedService<Worker>();
