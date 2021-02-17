18734b254e12859a236fcd0a4c6f16f0e715298b
Erik Rasmussen <ecr.referee@hotmail.com>
2021-02-16 19:35:03 -0600

    WIP on the servarr advertiser

---
+++ stacks/media/containers/servarr-advertiser/Clients/RadarrClientWrapper.cs
@@ -0,0 +1,14 @@
+using System.Net.Http;
+using Clients;
+
+namespace ServarrAdvertiser.Clients
+{
+    public class RadarrClientWrapper
+    {
+        public RadarrClientWrapper()
+        {
+            var temp = new RadarrClient(new HttpClient());
+            temp.PutconfigUiAsync(new Body())
+        }
+    }
+}
--- stacks/media/containers/service-connector-common/IServarrClient.cs
+++ stacks/media/containers/service-connector-common/IServarrClient.cs
@@ -1,7 +1,14 @@
+using System.Threading;
+using System.Threading.Tasks;
+
 namespace ServiceConnector.Protos
 {
     public interface IServarrClient
     {
-        
+        Task<string> GetApiKeyAsync(CancellationToken cancellationToken = default);
+
+        Task<bool> ShouldImportAsync(IndexRequest request, CancellationToken cancellationToken = default);
+
+        Task ImportAsync(IndexRequest request, CancellationToken cancellationToken = default);
     }
 }
