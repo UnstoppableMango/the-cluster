3633eca59fc43a48e1ee25452395ba69d6eac737
Erik Rasmussen <ecr.referee@hotmail.com>
2021-11-09 22:06:49 -0600

    Comment out all the unused things
    For now...

--- stacks/media/index.ts
+++ stacks/media/index.ts
@@ -34,145 +34,145 @@ const namespace = new Namespace('media', {
   projectId: project.id,
 });
 
-const pia = config.requireObject<Pia>('pia');
-const delugeConfig = config.requireObject<DelugeConfig>('deluge');
+// const pia = config.requireObject<Pia>('pia');
+// const delugeConfig = config.requireObject<DelugeConfig>('deluge');
 
-const { puid, pgid, tz } = config.requireObject<LinuxServerConfig>('linuxserver');
-const linuxServerShared = new kx.ConfigMap('linuxserver-shared', {
-  metadata: { namespace: namespace.name },
-  data: {
-    PUID: `${puid}`,
-    PGID: `${pgid}`,
-    TZ: tz,
-  },
-});
+// const { puid, pgid, tz } = config.requireObject<LinuxServerConfig>('linuxserver');
+// const linuxServerShared = new kx.ConfigMap('linuxserver-shared', {
+//   metadata: { namespace: namespace.name },
+//   data: {
+//     PUID: `${puid}`,
+//     PGID: `${pgid}`,
+//     TZ: tz,
+//   },
+// });
 
-const { username, password } = config.requireObject<{
-  username: string, password: string,
-}>('registry');
+// const { username, password } = config.requireObject<{
+//   username: string, password: string,
+// }>('registry');
 
-// const serviceConnector = new ServiceConnector('service-connector', {
+// // const serviceConnector = new ServiceConnector('service-connector', {
+// //   namespace: namespace.name,
+// //   version: 'latest',
+// //   registryUsername: username,
+// //   registryPassword: password,
+// // });
+
+// // Deluge
+// // const delugeNs = new Namespace('deluge', {
+// //   name: 'deluge',
+// //   projectId: project.id,
+// // });
+
+// const deluge = new Deluge('deluge', {
+//   deluge: delugeConfig,
+//   namespace: namespace.name,
+//   pia,
+//   projectId: project.id,
+// });
+
+// // Jackett
+// // const jackettNs = new Namespace('jackett', {
+// //   name: 'jackett',
+// //   projectId: project.id,
+// // });
+
+// // const indexPublisher = new Image('index-publisher', {
+// //   build: {
+// //     context: './containers',
+// //     dockerfile: './containers/index-publisher/Dockerfile',
+// //   },
+// //   imageName: 'harbor.int.unmango.net/library/index-publisher:latest',
+// //   registry: {
+// //     server: 'https://harbor.int.unmango.net',
+// //     username,
+// //     password,
+// //   },
+// // });
+
+// const jackett = new Jackett('jackett', {
+//   namespace: namespace.name,
+//   linuxServer: linuxServerShared,
+// });
+
+// const flareSolverr = new FlareSolverr('flare-solverr', {
 //   namespace: namespace.name,
 //   version: 'latest',
-//   registryUsername: username,
-//   registryPassword: password,
 // });
 
-// Deluge
-// const delugeNs = new Namespace('deluge', {
-//   name: 'deluge',
-//   projectId: project.id,
+// // Servarr
+// // const advertiserImage = new Image('servarr-advertiser', {
+// //   build: {
+// //     context: './containers',
+// //     dockerfile: './containers/servarr-advertiser/Dockerfile',
+// //   },
+// //   imageName: 'harbor.int.unmango.net/library/servarr-advertiser:latest',
+// //   registry: {
+// //     server: 'https://harbor.int.unmango.net',
+// //     username,
+// //     password,
+// //   },
+// // });
+
+// // Sonarr
+// // const sonarrNs = new Namespace('sonarr', {
+// //   name: 'sonarr',
+// //   projectId: project.id,
+// // });
+
+// const tv = new Sonarr('tv', {
+//   namespace: namespace.name,
+//   linuxServer: linuxServerShared,
+//   downloads: deluge.downloads,
+//   tvVolume: createMediaVolume('tv', namespace.name, '/tank1/media/tv'),
 // });
 
-const deluge = new Deluge('deluge', {
-  deluge: delugeConfig,
-  namespace: namespace.name,
-  pia,
-  projectId: project.id,
-});
-
-// Jackett
-// const jackettNs = new Namespace('jackett', {
-//   name: 'jackett',
-//   projectId: project.id,
+// const tv4k = new Sonarr('tv4k', {
+//   namespace: namespace.name,
+//   linuxServer: linuxServerShared,
+//   downloads: deluge.downloads,
+//   tvVolume: createMediaVolume('tv4k', namespace.name, '/tank1/media/tv4k'),
 // });
 
-// const indexPublisher = new Image('index-publisher', {
-//   build: {
-//     context: './containers',
-//     dockerfile: './containers/index-publisher/Dockerfile',
-//   },
-//   imageName: 'harbor.int.unmango.net/library/index-publisher:latest',
-//   registry: {
-//     server: 'https://harbor.int.unmango.net',
-//     username,
-//     password,
-//   },
+// const anime = new Sonarr('anime', {
+//   namespace: namespace.name,
+//   linuxServer: linuxServerShared,
+//   downloads: deluge.downloads,
+//   tvVolume: createMediaVolume('anime', namespace.name, '/tank1/media/anime'),
 // });
 
-const jackett = new Jackett('jackett', {
-  namespace: namespace.name,
-  linuxServer: linuxServerShared,
-});
+// // Radarr
+// // const radarrNs = new Namespace('radarr', {
+// //   name: 'radarr',
+// //   projectId: project.id,
+// // });
 
-const flareSolverr = new FlareSolverr('flare-solverr', {
-  namespace: namespace.name,
-  version: 'latest',
-});
-
-// Servarr
-// const advertiserImage = new Image('servarr-advertiser', {
-//   build: {
-//     context: './containers',
-//     dockerfile: './containers/servarr-advertiser/Dockerfile',
-//   },
-//   imageName: 'harbor.int.unmango.net/library/servarr-advertiser:latest',
-//   registry: {
-//     server: 'https://harbor.int.unmango.net',
-//     username,
-//     password,
-//   },
+// const movies = new Radarr('movies', {
+//   namespace: namespace.name,
+//   linuxServer: linuxServerShared,
+//   downloads: deluge.downloads,
+//   moviesVolume: createMediaVolume('movies', namespace.name, '/tank1/media/movies'),
 // });
 
-// Sonarr
-// const sonarrNs = new Namespace('sonarr', {
-//   name: 'sonarr',
-//   projectId: project.id,
+// const movies4k = new Radarr('movies4k', {
+//   namespace: namespace.name,
+//   linuxServer: linuxServerShared,
+//   downloads: deluge.downloads,
+//   moviesVolume: createMediaVolume('movies4k', namespace.name, '/tank1/media/movies4k'),
 // });
 
-const tv = new Sonarr('tv', {
-  namespace: namespace.name,
-  linuxServer: linuxServerShared,
-  downloads: deluge.downloads,
-  tvVolume: createMediaVolume('tv', namespace.name, '/tank1/media/tv'),
-});
+// // Lidarr
+// // const lidarrNs = new Namespace('lidarr', {
+// //   name: 'lidarr',
+// //   projectId: project.id,
+// // });
 
-const tv4k = new Sonarr('tv4k', {
-  namespace: namespace.name,
-  linuxServer: linuxServerShared,
-  downloads: deluge.downloads,
-  tvVolume: createMediaVolume('tv4k', namespace.name, '/tank1/media/tv4k'),
-});
-
-const anime = new Sonarr('anime', {
-  namespace: namespace.name,
-  linuxServer: linuxServerShared,
-  downloads: deluge.downloads,
-  tvVolume: createMediaVolume('anime', namespace.name, '/tank1/media/anime'),
-});
-
-// Radarr
-// const radarrNs = new Namespace('radarr', {
-//   name: 'radarr',
-//   projectId: project.id,
+// const lidarr = new Lidarr('lidarr', {
+//   namespace: namespace.name,
+//   linuxServer: linuxServerShared,
+//   downloads: deluge.downloads,
+//   musicVolume: createMediaVolume('music', namespace.name, '/tank1/media/music'),
 // });
-
-const movies = new Radarr('movies', {
-  namespace: namespace.name,
-  linuxServer: linuxServerShared,
-  downloads: deluge.downloads,
-  moviesVolume: createMediaVolume('movies', namespace.name, '/tank1/media/movies'),
-});
-
-const movies4k = new Radarr('movies4k', {
-  namespace: namespace.name,
-  linuxServer: linuxServerShared,
-  downloads: deluge.downloads,
-  moviesVolume: createMediaVolume('movies4k', namespace.name, '/tank1/media/movies4k'),
-});
-
-// Lidarr
-// const lidarrNs = new Namespace('lidarr', {
-//   name: 'lidarr',
-//   projectId: project.id,
-// });
-
-const lidarr = new Lidarr('lidarr', {
-  namespace: namespace.name,
-  linuxServer: linuxServerShared,
-  downloads: deluge.downloads,
-  musicVolume: createMediaVolume('music', namespace.name, '/tank1/media/music'),
-});
 
 const deemixConfig = config.requireObject<{ arl: string }>('deemix');
 
