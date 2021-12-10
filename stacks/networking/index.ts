f039ed45e8b05217c0237eb5c5ad1ffa00baf870
Erik Rasmussen <ecr.referee@hotmail.com>
2021-12-10 15:51:28 -0600

    Add mor config + fix cert-manager stuff

--- stacks/networking/index.ts
+++ stacks/networking/index.ts
@@ -36,56 +36,56 @@ const cloudflareSecret = new Secret('cloudflare', {
 const leConfig = config.requireObject<LetsEnryptConfig>('letsEncrypt');
 const leEnv = leConfig.useProd ? leConfig.prod : leConfig.staging;
 
-const leIssuer = new certManager.ClusterIssuer('letsencrypt', {
-  spec: {
-    acme: {
-      email: leConfig.email,
-      server: leEnv.server,
-      privateKeySecretRef: {
-        name: leEnv.accountKeyName,
-      },
-      solvers: [{
-        dns01: {
-          cloudflare: {
-            email: leConfig.email,
-            // Only one is required
-            // apiKeySecretRef: {
-            //   name: cloudflareSecret.metadata.name,
-            //   key: 'apiKey',
-            // },
-            apiTokenSecretRef: {
-              name: cloudflareSecret.metadata.name,
-              key: 'apiToken',
-            },
-          },
-        },
-      }],
-    },
-  },
-});
+// const leIssuer = new certManager.ClusterIssuer('letsencrypt', {
+//   spec: {
+//     acme: {
+//       email: leConfig.email,
+//       server: leEnv.server,
+//       privateKeySecretRef: {
+//         name: leEnv.accountKeyName,
+//       },
+//       solvers: [{
+//         dns01: {
+//           cloudflare: {
+//             email: leConfig.email,
+//             // Only one is required
+//             // apiKeySecretRef: {
+//             //   name: cloudflareSecret.metadata.name,
+//             //   key: 'apiKey',
+//             // },
+//             apiTokenSecretRef: {
+//               name: cloudflareSecret.metadata.name,
+//               key: 'apiToken',
+//             },
+//           },
+//         },
+//       }],
+//     },
+//   },
+// });
 
-const defaultCert = new certManager.Certificate('default', {
-  metadata: { namespace: 'traefik-system' },
-  spec: {
-    secretName: 'default-tls-cert',
-    duration: '2160h', // 90d
-    renewBefore: '360h', //15d
-    dnsNames: [
-      'unmango.net',
-      '*.unmango.net',
-      '*.int.unmango.net',
-    ],
-    issuerRef: {
-      name: pulumi.output(leIssuer.metadata).apply(x => x?.name ?? ''),
-      kind: leIssuer.kind.apply(x => x ?? ''),
-    },
-  },
-});
+// const defaultCert = new certManager.Certificate('default', {
+//   metadata: { namespace: 'traefik-system' },
+//   spec: {
+//     secretName: 'default-tls-cert',
+//     duration: '2160h', // 90d
+//     renewBefore: '360h', //15d
+//     dnsNames: [
+//       'unmango.net',
+//       '*.unmango.net',
+//       '*.int.unmango.net',
+//     ],
+//     issuerRef: {
+//       name: pulumi.output(leIssuer.metadata).apply(x => x?.name ?? ''),
+//       kind: leIssuer.kind.apply(x => x ?? ''),
+//     },
+//   },
+// });
 
-const tlsStore = new TlsStore('default', {
-  metadata: { name: 'default', namespace: 'traefik-system' },
-  secretName: defaultCert.spec.secretName,
-});
+// const tlsStore = new TlsStore('default', {
+//   metadata: { name: 'default', namespace: 'traefik-system' },
+//   secretName: defaultCert.spec.secretName,
+// });
 
 const dashboard = new IngressRoute('dashboard', {
   // metadata: { namespace: this.namespace.metadata.name },
