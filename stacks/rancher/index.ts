8c7e5f3d9127ff6e94fdf6a4bec078364e48be16
UnstoppableMango <ecr.referee@hotmail.com>
2021-12-13 00:23:47 -0600

    Import Rancher to Pulumi (#727)

--- stacks/rancher/index.ts
+++ stacks/rancher/index.ts
@@ -1,14 +1,24 @@
-import * as pulumi from '@pulumi/pulumi';
+import * as k8s from '@pulumi/kubernetes';
 import * as rancher2 from '@pulumi/rancher2';
-import { BackupRestore, Catalogs, OperatorStacks } from './resources';
+import { BackupRestore, Catalogs } from './resources';
 
-const config = new pulumi.Config();
+const rancherRelease = new k8s.helm.v3.Release('rancher', {
+  name: 'rancher',
+  chart: 'rancher',
+  namespace: 'cattle-system',
+  version: '2.6.2',
+  repositoryOpts: {
+    repo: 'https://releases.rancher.com/server-charts/latest',
+  },
+  values: {
+    hostname: 'rancher.int.unmango.net',
+    tls: 'external',
+  },
+});
 
 const cluster = rancher2.Cluster.get('local', 'local');
 
 const clusterCatalogs = new Catalogs('catalogs', { clusterId: cluster.id });
-
-// const stacks = new OperatorStacks('the-cluster');
 
 const cattleResources = new rancher2.Namespace('cattle-resources-system', {
   name: 'cattle-resources-system',
