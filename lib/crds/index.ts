e6bed7cf9c8b2ec04e139d4b78839b1c97c7cc8c
Erik Rasmussen <ecr.referee@hotmail.com>
2021-12-12 19:57:11 -0600

    Re-generate crds under common package with script

--- lib/crds/index.ts
+++ lib/crds/index.ts
@@ -8,10 +8,14 @@ import * as utilities from "./utilities";
 export * from "./provider";
 
 // Export sub-modules:
+import * as acme from "./acme";
+import * as certmanager from "./certmanager";
 import * as traefik from "./traefik";
 import * as types from "./types";
 
 export {
+    acme,
+    certmanager,
     traefik,
     types,
 };
