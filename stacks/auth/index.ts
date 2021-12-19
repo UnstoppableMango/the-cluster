e3c7996d0439f6f04582e6161bf2904ec6571097
Erik Rasmussen <ecr.referee@hotmail.com>
2021-12-18 23:53:24 -0600

    WIP setting up traefik-forward-auth

--- stacks/auth/index.ts
+++ stacks/auth/index.ts
@@ -1,4 +1,6 @@
 import * as pulumi from '@pulumi/pulumi';
+import * as kx from '@pulumi/kubernetesx';
+import { IngressRoute, Middleware } from '@pulumi/crds/traefik/v1alpha1';
 import { Project } from '@pulumi/rancher2';
 
 const config = new pulumi.Config();
@@ -18,6 +20,34 @@ const project = new Project('auth', {
 //   version: '2.0.1',
 // });
 
+const github = config.requireObject<GitHubConfig>('github');
+
+// https://github.com/thomseddon/traefik-forward-auth
+const tfa = new kx.PodBuilder({
+  containers: [{
+    image: 'thomseddon/traefik-forward-auth:2',
+    name: 'traefik-forward-auth',
+    ports: [{
+      containerPort: 4181,
+    }],
+    env: {
+      PROVIDERS_GENERIC_OAUTH_AUTH_URL: 'https://github.com/login/oauth/authorize',
+      PROVIDERS_GENERIC_OAUTH_TOKEN_URL: 'https://github.com/login/oauth/access_token',
+      PROVIDERS_GENERIC_OAUTH_USER_URL: 'https://api.github.com/user',
+      PROVIDERS_GENERIC_OAUTH_CLIENT_ID: github.clientId,
+      PROVIDERS_GENERIC_OAUTH_CLIENT_SECRET: github.clientSecret,
+      // PROVIDERS_GENERIC_OAUTH_SCOPE: '',
+      // PROVIDERS_GENERIC_OAUTH_TOKEN_STYLE: '',
+      // PROVIDERS_GENERIC_OAUTH_RESOURCE: '',
+    },
+  }],
+});
+
 // export const keycloakAdminPassword = pulumi.secret(keycloak.adminPassword.result);
 // export const keycloakManagementPassword = pulumi.secret(keycloak.managementPassword.result);
 // export const keycloakPostgresqlPassword = pulumi.secret(keycloak.postgresPassword.result);
+
+interface GitHubConfig {
+  clientId: string;
+  clientSecret: string;
+}
