7dd77b297ec910614bb80fcc87cbb96dfd92e5f9
UnstoppableMango <ecr.referee@hotmail.com>
2021-12-23 13:59:54 -0600

    Remove tfa stuff and setup basic auth (#757)

--- stacks/auth/index.ts
+++ stacks/auth/index.ts
@@ -21,101 +21,118 @@ const project = new rancher.Project('auth', {
 //   version: '2.0.1',
 // });
 
-const tfaNamespace = new rancher.Namespace('traefik-forward-auth', {
-  name: 'traefik-forward-auth',
+const basicAuth = config.requireObject<BasicAuthConfig>('basic');
+const basicSecret = new kx.Secret('basic-auth', {
+  metadata: {
+    name: 'basic-auth',
+    namespace: 'traefik-system',
+  },
+  stringData: {
+    // TODO: Find out how to support multiple users
+    users: basicAuth.users[0],
+  },
+});
+
+const basicMiddleware = new Middleware('basic-auth', {
+  metadata: {
+    name: 'basic-auth',
+    namespace: 'traefik-system',
+  },
+  spec: {
+    basicAuth: {
+      secret: basicSecret.metadata.name,
+      removeHeader: true,
+    },
+  },
+});
+
+const faNamespace = new rancher.Namespace('forward-auth', {
+  name: 'forward-auth',
   projectId: project.id,
 });
 
 const github = config.requireObject<GitHubConfig>('github');
-
-const tfaSecretString = new random.RandomString('traefik-forward-auth', {
-  length: 69,
-});
-
-const tfaSecret = new kx.Secret('traefik-forward-auth', {
+const faSecret = new kx.Secret('forward-auth', {
   metadata: {
-    name: 'traefik-forward-auth',
-    namespace: tfaNamespace.name,
+    name: 'forward-auth',
+    namespace: faNamespace.name,
   },
   stringData: {
     'github-client-id': github.clientId,
     'github-client-secret': github.clientSecret,
-    secret: tfaSecretString.result,
   },
 });
-
-const tfaPort = 4181;
 
 // https://github.com/thomseddon/traefik-forward-auth
-const tfapb = new kx.PodBuilder({
-  containers: [{
-    image: 'thomseddon/traefik-forward-auth:2',
-    name: 'traefik-forward-auth',
-    ports: [{
-      containerPort: tfaPort,
-    }],
-    env: {
-      AUTH_HOST: 'auth.thecluster.io',
-      COOKIE_DOMAIN: 'thecluster.io',
-      DEFAULT_PROVIDER: 'generic-oauth',
-      PROVIDERS_GENERIC_OAUTH_AUTH_URL: 'https://github.com/login/oauth/authorize',
-      PROVIDERS_GENERIC_OAUTH_TOKEN_URL: 'https://github.com/login/oauth/access_token',
-      PROVIDERS_GENERIC_OAUTH_USER_URL: 'https://api.github.com/user',
-      PROVIDERS_GENERIC_OAUTH_CLIENT_ID: tfaSecret.asEnvValue('github-client-id'),
-      PROVIDERS_GENERIC_OAUTH_CLIENT_SECRET: tfaSecret.asEnvValue('github-client-secret'),
-      // PROVIDERS_GENERIC_OAUTH_SCOPE: '',
-      // PROVIDERS_GENERIC_OAUTH_TOKEN_STYLE: '',
-      // PROVIDERS_GENERIC_OAUTH_RESOURCE: '',
-      SECRET: tfaSecret.asEnvValue('secret'),
-    },
-  }],
-});
+// const tfapb = new kx.PodBuilder({
+//   containers: [{
+//     image: 'thomseddon/traefik-forward-auth:2',
+//     name: 'traefik-forward-auth',
+//     ports: [{
+//       containerPort: tfaPort,
+//     }],
+//     env: {
+//       AUTH_HOST: 'auth.thecluster.io',
+//       COOKIE_DOMAIN: 'thecluster.io',
+//       DEFAULT_PROVIDER: 'generic-oauth',
+//       PROVIDERS_GENERIC_OAUTH_AUTH_URL: 'https://github.com/login/oauth/authorize',
+//       PROVIDERS_GENERIC_OAUTH_TOKEN_URL: 'https://github.com/login/oauth/access_token',
+//       PROVIDERS_GENERIC_OAUTH_USER_URL: 'https://api.github.com/user',
+//       PROVIDERS_GENERIC_OAUTH_CLIENT_ID: faSecret.asEnvValue('github-client-id'),
+//       PROVIDERS_GENERIC_OAUTH_CLIENT_SECRET: faSecret.asEnvValue('github-client-secret'),
+//       // PROVIDERS_GENERIC_OAUTH_SCOPE: '',
+//       // PROVIDERS_GENERIC_OAUTH_TOKEN_STYLE: '',
+//       // PROVIDERS_GENERIC_OAUTH_RESOURCE: '',
+//       SECRET: faSecret.asEnvValue('secret'),
+//     },
+//   }],
+// });
 
-const tfaDeployment = new kx.Deployment('traefik-forward-auth', {
-  metadata: {
-    name: 'traefik-forward-auth',
-    namespace: tfaNamespace.name,
-  },
-  spec: tfapb.asDeploymentSpec({
-    strategy: { type: 'Recreate' },
-  }),
-});
+// const faDeployment = new kx.Deployment('forward-auth', {
+//   metadata: {
+//     name: 'forward-auth',
+//     namespace: faNamespace.name,
+//   },
+//   spec: tfapb.asDeploymentSpec({
+//     strategy: { type: 'Recreate' },
+//   }),
+// });
 
-const tfaService = tfaDeployment.createService({
-  type: kx.types.ServiceType.ClusterIP,
-  ports: [{ port: tfaPort }],
-});
+// const faService = faDeployment.createService({
+//   type: kx.types.ServiceType.ClusterIP,
+//   ports: [{ port: tfaPort }],
+// });
 
-const tfaIngressRoute = new IngressRoute('traefik-forward-auth', {
-  metadata: {
-    name: 'traefik-forward-auth',
-    namespace: tfaNamespace.name,
-  },
-  spec: {
-    entryPoints: ['websecure'],
-    routes: [{
-      kind: 'Rule',
-      match: 'Host(`auth.thecluster.io`)',
-      services: [{
-        name: tfaService.metadata.name,
-        port: tfaPort,
-      }],
-    }],
-  },
-});
+// const tfaIngressRoute = new IngressRoute('forward-auth', {
+//   metadata: {
+//     name: 'forward-auth',
+//     namespace: faNamespace.name,
+//   },
+//   spec: {
+//     entryPoints: ['websecure'],
+//     routes: [{
+//       kind: 'Rule',
+//       match: 'Host(`auth.thecluster.io`)',
+//       services: [{
+//         name: faService.metadata.name,
+//         port: tfaPort,
+//       }],
+//     }],
+//   },
+// });
 
-const tfaMiddleware = new Middleware('traefik-forward-auth', {
-  metadata: {
-    name: 'traefik-forward-auth',
-    namespace: tfaNamespace.name,
-  },
-  spec: {
-    forwardAuth: {
-      address: pulumi.interpolate`http://${tfaService.metadata.name}:${tfaPort}`,
-      authResponseHeaders: ['X-Forwarded-User'],
-    },
-  },
-});
+// const tfaMiddleware = new Middleware('traefik-forward-auth', {
+//   metadata: {
+//     name: 'traefik-forward-auth',
+//     namespace: tfaNamespace.name,
+//   },
+//   spec: {
+//     forwardAuth: {
+//       address: pulumi.interpolate`http://${tfaService.metadata.name}:${tfaPort}`,
+//       authResponseHeaders: ['X-Forwarded-User'],
+//     },
+//   },
+// });
 
 // export const keycloakAdminPassword = pulumi.secret(keycloak.adminPassword.result);
 // export const keycloakManagementPassword = pulumi.secret(keycloak.managementPassword.result);
@@ -124,4 +141,9 @@ const tfaMiddleware = new Middleware('traefik-forward-auth', {
 interface GitHubConfig {
   clientId: string;
   clientSecret: string;
+}
+
+interface BasicAuthConfig {
+  // Should be user:hashed-password
+  users: string[];
 }
