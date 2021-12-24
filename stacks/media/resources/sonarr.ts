2ddf63d2a72e62bef4e9be33177a1e170f9aa7a1
UnstoppableMango <ecr.referee@hotmail.com>
2021-12-24 12:16:32 -0600

    Get Sonarr back up (#762)

--- stacks/media/resources/sonarr.ts
+++ stacks/media/resources/sonarr.ts
@@ -1,41 +1,38 @@
-import * as k8s from '@pulumi/kubernetes';
 import * as kx from '@pulumi/kubernetesx';
+import * as traefik from '@pulumi/crds/traefik/v1alpha1';
 import { ComponentResource, ComponentResourceOptions, Input } from '@pulumi/pulumi';
 import { getNameResolver } from '@unmango/shared/util';
+import { matchBuilder } from '@unmango/shared/traefik';
 
 export class Sonarr extends ComponentResource {
 
   private readonly getName = getNameResolver('sonarr', this.name);
 
   public readonly config: kx.PersistentVolumeClaim;
-  public readonly media: kx.PersistentVolumeClaim;
   public readonly deployment: kx.Deployment;
   public readonly service: kx.Service;
-  public readonly ingress: k8s.networking.v1.Ingress;
+  public readonly ingressRoute: traefik.IngressRoute;
 
   constructor(private name: string, private args: SonarrArgs, opts?: ComponentResourceOptions) {
     super('unmango:apps:sonarr', name, undefined, opts);
 
-    this.config = new kx.PersistentVolumeClaim(this.getName('data'), {
-      metadata: { namespace: this.args.namespace },
+    this.config = new kx.PersistentVolumeClaim(this.getName(), {
+      metadata: {
+        name: this.getName(),
+        namespace: this.args.namespace,
+      },
       spec: {
         storageClassName: 'longhorn',
         accessModes: ['ReadWriteOnce'],
-        resources: { requests: { storage: '2Gi' } },
-      },
-    }, { parent: this });
-  
-    this.media = new kx.PersistentVolumeClaim(this.getName('media'), {
-      metadata: { namespace: this.args.namespace },
-      spec: {
-        accessModes: ['ReadWriteOnce', 'ReadOnlyMany'],
-        resources: { requests: { storage: '5000Gi' } },
-        storageClassName: 'nfs',
-        volumeName: this.args.tvVolume.metadata.name,
+        resources: { requests: { storage: '5Gi' } },
       },
     }, { parent: this });
   
     const pb = new kx.PodBuilder({
+      volumes: [
+        { name: 'downloads', nfs: args.downloads },
+        { name: 'tv', nfs: args.tv },
+      ],
       containers: [{
         // kx sets the selector to the container name.
         // With multiple resources, it won't match correctly, so
@@ -44,23 +41,32 @@ export class Sonarr extends ComponentResource {
         securityContext: {
           privileged: true,
         },
-        image: 'linuxserver/sonarr:version-2.0.0.5344',
+        image: 'linuxserver/sonarr:3.0.6.1342-ls129',
         envFrom: [{
-          configMapRef: { name: this.args.linuxServer.metadata.name },
+          configMapRef: {
+            name: this.args.linuxServer.metadata.name,
+          },
         }],
+        env: {
+          DOCKER_MODS: 'ghcr.io/gilbn/theme.park:sonarr',
+          TP_THEME: 'plex',
+        },
         ports: {
           http: 8989,
         },
         volumeMounts: [
           this.config.mount('/config'),
-          this.media.mount('/tv'),
-          this.args.downloads.mount('/downloads', 'completed'),
+          { name: 'downloads', mountPath: '/downloads' },
+          { name: 'tv', mountPath: '/tv' },
         ],
       }],
     });
   
-    this.deployment = new kx.Deployment(this.getName('deployment'), {
-      metadata: { namespace: this.args.namespace },
+    this.deployment = new kx.Deployment(this.getName(), {
+      metadata: {
+        name: this.getName(),
+        namespace: this.args.namespace,
+      },
       spec: pb.asDeploymentSpec({
         strategy: { type: 'Recreate' },
       }),
@@ -68,26 +74,25 @@ export class Sonarr extends ComponentResource {
   
     this.service = this.deployment.createService({
       type: kx.types.ServiceType.ClusterIP,
-      ports: [{ name: 'http', port: 8989, targetPort: 8989 }],
     });
 
-    this.ingress = new k8s.networking.v1.Ingress(this.getName('ingress'), {
-      metadata: { namespace: args.namespace },
+    this.ingressRoute = new traefik.IngressRoute(this.getName(), {
+      metadata: {
+        name: this.getName(),
+        namespace: this.args.namespace,
+      },
       spec: {
-        rules: [{
-          host: `${this.name}.int.unmango.net`,
-          http: {
-            paths: [{
-              backend: {
-                service: {
-                  name: this.service.metadata.name,
-                  port: { name: 'http' },
-                },
-              },
-              // TODO: Required ✓, Correct?
-              pathType: 'ImplementationSpecific',
-            }],
-          },
+        entryPoints: ['websecure'],
+        routes: [{
+          kind: 'Rule',
+          match: matchBuilder()
+            .host('media.int.unmango.net').and().pathPrefix(`/${this.name}`)
+            .or().host(`${this.name}.int.unmango.net`)
+            .build(),
+          services: [{
+            name: this.service.metadata.name,
+            port: this.service.spec.ports[0].port,
+          }],
         }],
       },
     }, { parent: this });
@@ -97,9 +102,14 @@ export class Sonarr extends ComponentResource {
 
 }
 
+interface NfsArgs {
+  server: Input<string>;
+  path: Input<string>;
+}
+
 export interface SonarrArgs {
   namespace: Input<string>;
   linuxServer: kx.ConfigMap;
-  downloads: kx.PersistentVolumeClaim;
-  tvVolume: k8s.core.v1.PersistentVolume;
+  downloads: Input<NfsArgs>;
+  tv: Input<NfsArgs>;
 }
