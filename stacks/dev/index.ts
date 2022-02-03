2222a70fd1a695c24163d0ff8ec310fe4739aca9
Erik Rasmussen <ecr.referee@hotmail.com>
2022-02-02 21:54:17 -0600

    Remove code server resources

--- stacks/dev/index.ts
+++ stacks/dev/index.ts
@@ -5,12 +5,3 @@ const project = new Project('dev', {
   name: 'Dev',
   clusterId: 'local',
 });
-
-const codeServerNs = new Namespace('code-server', {
-  name: 'code-server',
-  projectId: project.id,
-});
-
-const codeServer = new CodeServer('code-server', {
-  namespace: codeServerNs.name,
-});
