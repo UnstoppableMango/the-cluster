4d48e180c6ae2c90dd3cfadc7685b72437beb653
UnstoppableMango <ecr.referee@hotmail.com>
2022-02-24 20:22:01 -0600

    Set modified stacks based on package dependencies (#857)

--- tools/set-modified-stacks.ts
+++ tools/set-modified-stacks.ts
@@ -9,12 +9,15 @@ const execAsync = util.promisify(exec.exec);
     'git', 'diff', '--name-only', 'origin/main',
   ].join(' ');
 
-  const changedFiles = await execAsync(diffCommand).then(x => x.stdout.split('\n'));
+  const changedFiles = await execAsync(diffCommand).then(x => x.stdout.split(os.EOL));
 
-  const triggerAllFiles = [
-    'lib',
-    'package-lock.json',
-    'package.json',
+  const triggerAllFiles = ['lib'];
+
+  const triggerAllDependencies = [
+    /"@pulumi\/.*"/gm,
+    /"@unmango\/.*"/gm,
+    /"typescript"/gm,
+    /"yaml"/gm,
   ];
 
   const stacks = [
@@ -34,14 +37,38 @@ const execAsync = util.promisify(exec.exec);
     process.exit(0);
   }
 
+  if (changedFiles.some(f => f === 'package.json' || f === 'package-lock.json')) {
+    const command = [
+      'git', 'diff', '-U0', 'origin/main', 'package.json',
+    ].join(' ');
+
+    const packageJsonDiff = await execAsync(command).then(x => x.stdout.split(os.EOL));
+
+    const modified = packageJsonDiff
+      .filter(l => /^[+]/gm.test(l))
+      .filter(l => !/^(--- a\/|\+\+\+ b\/)/gm.test(l));
+
+    if (modified.some(x => triggerAllDependencies.some(t => t.test(x)))) {
+      const setOutputCommand = createSetOutputCommand(stacks);
+      console.log('Running command: ' + setOutputCommand);
+      process.stdout.write(setOutputCommand + os.EOL);
+      process.exit(0);
+    }
+  }
+
   const filteredStacks = stacks.filter(s => changedFiles.some(f => f.startsWith(`stacks/${s}`)));
 
-  if (filteredStacks.length >= 0) {
+  if (filteredStacks.length > 0) {
     const setOutputCommand = createSetOutputCommand(filteredStacks);
     console.log('Running command: ' + setOutputCommand);
     process.stdout.write(setOutputCommand + os.EOL);
+    process.exit(0);
   }
 
+  // TODO: Github does not like empty arrays for the strategy matrix
+  // Workflow expects valid json
+  const setOutputCommand = createSetOutputCommand([]);
+  process.stdout.write(setOutputCommand + os.EOL);
 })();
 
 function createSetOutputCommand(stacks: string[]): string {
