c5f08a1841083b4b3a6e2940ec75eb7454a47bce
UnstoppableMango <ecr.referee@hotmail.com>
2022-04-09 18:26:15 -0500

    Add plex tunnel (#918)

--- stacks/networking/resources/tunnel.ts
+++ stacks/networking/resources/tunnel.ts
@@ -1,6 +1,5 @@
 import * as pulumi from '@pulumi/pulumi';
 import * as cf from '@pulumi/cloudflare';
-import * as k8s from '@pulumi/kubernetes';
 import * as kx from '@pulumi/kubernetesx';
 import * as random from '@pulumi/random';
 import * as YAML from 'yaml';
