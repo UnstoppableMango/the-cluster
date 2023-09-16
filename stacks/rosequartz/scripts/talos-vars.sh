c9d7652f988862451ab143931ba9f811584a3f4d
Erik Rasmussen <ecr.referee@hotmail.com>
2023-09-16 00:39:58 -0500

    Allow overriding script vars

--- stacks/rosequartz/scripts/talos-vars.sh
+++ stacks/rosequartz/scripts/talos-vars.sh
@@ -4,11 +4,11 @@ set -eu
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
-TALOS_DIR="$(dirname "$SCRIPT_DIR")/.talos"
-KUBE_DIR="$(dirname "$SCRIPT_DIR")/.kube"
+TALOS_DIR="${ROSEQUARTZ_TALOS_DIR:-"$(dirname "$SCRIPT_DIR")/.talos"}"
+KUBE_DIR="${ROSEQUARTZ_KUBE_DIR:-"$(dirname "$SCRIPT_DIR")/.kube"}"
 
-TALOSCONFIG="$TALOS_DIR/config"
-TALOS_STATE="$TALOS_DIR/clusters"
+TALOSCONFIG="${ROSEQUARTZ_TALOSCONFIG:-$TALOS_DIR/config}"
+TALOS_STATE="${ROSEQUARTZ_STATE:-$TALOS_DIR/clusters}"
 CLUSTER_NAME="${ROSEQUARTZ_CLUSTER_NAME:-talos-rosequartz}"
 
 export KUBECONFIG="$KUBE_DIR/config"
