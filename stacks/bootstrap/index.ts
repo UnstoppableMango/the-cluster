f2822f732d01c7d7e330629e9117ef9d4df89db4
Erik Rasmussen <ecr.referee@hotmail.com>
2023-09-24 17:48:33 -0500

    More robust bootstrap

--- stacks/bootstrap/index.ts
+++ stacks/bootstrap/index.ts
@@ -4,20 +4,30 @@ import * as cloudflare from "@pulumi/cloudflare";
 const config = new pulumi.Config();
 const zoneId: string = '22f1d42ba0fbe4f924905e1c6597055c';
 
-const primaryCname = new cloudflare.Record('man.thecluster.io', {
-  name: 'man',
+const primaryDns = new cloudflare.Record('primaryDns', {
+  name: config.require('primaryDnsName'),
   zoneId: zoneId,
   value: config.requireSecret('publicIp'),
   type: 'A',
-  proxied: true,
+  proxied: false, // TODO: Figure out proxy issues
 });
 
-const zoneSettings = new cloudflare.ZoneSettingsOverride('man.thecluster.io', {
+// This rule probably isn't doing anything with the proxy turned off
+const rule = new cloudflare.Ruleset('ssl', {
+  name: pulumi.interpolate`${primaryDns.hostname} SSL`,
+  description: pulumi.interpolate`Set SSL to a value that works for ${primaryDns.hostname}`,
+  kind: 'zone',
+  phase: 'http_config_settings',
   zoneId: zoneId,
-  settings: {
-    ssl: 'full',
-    alwaysUseHttps: 'on',
-  },
-});
+  rules: [
+    {
+      expression: pulumi.interpolate`(http.host eq "${primaryDns.hostname}")`,
+      action: 'set_config',
+      actionParameters: {
+        ssl: 'full',
+      }
+    }
+  ]
+}, { deleteBeforeReplace: true });
 
-export const hostname = primaryCname.hostname
+export const hostname = primaryDns.hostname
