fb0e0f046c67b0d182163bafba5039651d0ea72e
Erik Rasmussen <ecr.referee@hotmail.com>
2023-10-01 23:10:44 -0500

    Working on the install

--- stacks/cert-manager/index.ts
+++ stacks/cert-manager/index.ts
@@ -1,13 +1,16 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as k8s from "@pulumi/kubernetes";
 
-// const chart = new k8s.helm.v3.Chart('cert-manager', {
-//   path: './',
-// });
+const ns = new k8s.core.v1.Namespace('cert-manager', {
+  metadata: { name: 'cert-manager' },
+});
 
 // Use a release because the cert-manager helm chart uses hooks
 const release = new k8s.helm.v3.Release('cert-manager', {
   chart: './',
+  name: 'cert-manager',
+  namespace: ns.metadata.name,
+  createNamespace: false,
   atomic: true,
   dependencyUpdate: true,
   lint: true,
