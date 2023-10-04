cf00709a9aa8c0ce9f7baed73a173d24d9b3bcb7
Erik Rasmussen <ecr.referee@hotmail.com>
2023-10-04 15:50:46 -0500

    Add chart, dep, and WIP on values

--- stacks/dex/index.ts
+++ stacks/dex/index.ts
@@ -1,1 +1,18 @@
 import * as pulumi from "@pulumi/pulumi";
+import * as k8s from '@pulumi/kubernetes';
+
+const ns = new k8s.core.v1.Namespace('dex', {
+    metadata: { name: 'dex' },
+});
+
+const chart = new k8s.helm.v3.Chart('dex', {
+    path: './',
+    namespace: ns.metadata.name,
+    values: {
+        config: {
+            issuer: 'TODO',
+            storage: { type: 'memory' },
+            
+        },
+    },
+});
