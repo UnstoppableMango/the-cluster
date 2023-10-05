d6096cbfe2d4c19a068f6e085ca0ed21ade83dcb
Erik Rasmussen <ecr.referee@hotmail.com>
2023-10-05 15:07:28 -0500

    Properly set values

--- stacks/cloudflare-ingress/index.ts
+++ stacks/cloudflare-ingress/index.ts
@@ -11,11 +11,17 @@ const chart = new k8s.helm.v3.Chart('cloudflare-ingress', {
     path: './',
     namespace: ns.metadata.name,
     values: {
-      cloudflare: {
-        // TODO: Create a new token specifically for this
-        apiToken: cfConfig.requireSecret('apiToken'),
-        accountId: cfConfig.require('accountId'),
-        tunnelName: 'rosequartz-ingress',
-      }
+      'cloudflare-tunnel-ingress-controller': {
+        cloudflare: {
+          // TODO: Create a new token specifically for this
+          apiToken: cfConfig.requireSecret('apiToken'),
+          accountId: cfConfig.require('accountId'),
+          tunnelName: 'rosequartz-ingress',
+        },
+        image: {
+          repository: 'ghcr.io/unstoppablemango/cloudflare-tunnel-ingress-controller',
+          tag: 'sha-c92febc@sha256:7e4c9374891a1bd4058c09865d9b431779510de39c6ebdb8b13045f4935c87ed',
+        },
+      },
     },
 });
