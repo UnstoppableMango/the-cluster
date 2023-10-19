e4056760c3e90bd3e13cc1cd1f922f4549c1a80d
Erik Rasmussen <ecr.referee@hotmail.com>
2023-10-18 22:43:11 -0500

    Generate crds

--- stacks/clusterapi/scripts/gen-crds.sh
+++ stacks/clusterapi/scripts/gen-crds.sh
@@ -4,13 +4,20 @@ set -e
 
 if ! command -v crd2pulumi >/dev/null 2>&1; then
     echo "Install crd2pulumi first https://github.com/pulumi/crd2pulumi#building-and-installation"
-    exit 0
+    exit 1
 fi
 
 cwd="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 root="$(dirname "$cwd")"
 
+rm -r "$root/crds"
+
 echo "Generating CRDs for nodejs..."
-crd2pulumi providers/*/crds.yaml \
-    --nodejsPath="$root/crds" \
-    --force
+
+# Currently an error with rolloutStrategy in talos-controlplane
+# crd2pulumi providers/*/crds.yaml \
+#     --nodejsPath="$root/crds" \
+#     --force
+
+crd2pulumi $(ls $root/providers/*/crds.yaml | grep -viE "core|proxmox|talos-controlplane") \
+    --nodejsPath="$root/crds"
