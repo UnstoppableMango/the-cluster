d4539bcc9487f829df2c4519d9a383ecdeab1de9
Erik Rasmussen <ecr.referee@hotmail.com>
2023-10-21 22:43:22 -0500

    Most of the cluster stuff

--- stacks/pinkdiamond/index.ts
+++ stacks/pinkdiamond/index.ts
@@ -1,1 +1,22 @@
-import * as pulumi from "@pulumi/pulumi";
+import * as pulumi from '@pulumi/pulumi';
+import * as k8s from '@pulumi/kubernetes';
+import * as cloudflare from '@pulumi/cloudflare';
+
+const config = new pulumi.Config();
+
+// TODO: Tunnel
+const publicEndpoint = new cloudflare.Record('pd.thecluster.io', {
+  name: 'pd.thecluster.io',
+  type: 'A',
+  zoneId: config.require('zoneId'),
+  proxied: false,
+  value: config.requireSecret('publicIp'),
+});
+
+const ns = new k8s.core.v1.Namespace('pink-diamond', {
+  metadata: { name: 'pink-diamond' },
+});
+
+const manifests = new k8s.kustomize.Directory('manifests', {
+  directory: './manifests',
+}, { dependsOn: [publicEndpoint, ns] });
