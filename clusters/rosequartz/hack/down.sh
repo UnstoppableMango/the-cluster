2f145bb98a2b21cc8570dec08fc6b63d687b7efc
Erik Rasmussen <ecr.referee@hotmail.com>
2023-11-05 13:34:39 -0600

    Rosequartz ci fixes

--- clusters/rosequartz/hack/down.sh
+++ clusters/rosequartz/hack/down.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
-
 set -eum
 
 root="$(git rev-parse --show-toplevel)/clusters/rosequartz"
-cwd="$root/hack"
+stack="${RQ_STACK:-"$(hostname)"}"
 
-stack="$(hostname)"
-
-if [ -z "$(pulumi -C "$root" stack ls --json | jq -r ".[].name | select(. == \"$stack\")")" ]; then
-    echo "Initializing stack for $stack..."
-    pulumi stack init $stack --copy-config-from local
-    pulumi stack tag set local true
-else
+if [ -n "$(pulumi -C "$root" stack ls --json | jq -r ".[].name | select(. == \"$stack\")")" ]; then
     echo "Selecting stack $stack..."
     pulumi stack select "$stack"
 fi
 
 echo "Destroying cluster..."
-docker compose -f "$cwd/docker-compose.yaml" down -v
+docker compose -f "$root/hack/docker-compose.yaml" down -v
 
 [ -f "$root/.kube/$stack/config" ] && rm "$root/.kube/$stack/config"
 [ -f "$root/.talos/$stack/talosconfig.yaml" ] && rm "$root/.talos/$stack/talosconfig.yaml"
--- clusters/rosequartz/spec/e2e.sh
+++ clusters/rosequartz/spec/e2e.sh
@@ -2,12 +2,22 @@
 set -eum
 
 root="$(git rev-parse --show-toplevel)/clusters/rosequartz"
+
+# Create local docker nodes
 . "$root/hack/up.sh"
+
+# Create cluster
 pulumi -C "$root" up -yf
+
+# Setup config files
 . "$root/hack/write-config-files.sh"
+
+# Run tests
 talosctl health --nodes "$(pulumi -C "$root" config get endpoint)"
 bash "$root/spec/verify.sh"
 . "$root/spec/etcd-backup.sh"
 RQ_DRY_RUN=true bash "$root/scripts/k8s-upgrade.sh"
+
+# Teardown infastructure
 pulumi -C "$root" down -yf
 . "$root/hack/down.sh"
