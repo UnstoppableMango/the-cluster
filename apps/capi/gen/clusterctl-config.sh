a7ba2c1304c539eb7fd38abf291f8ed13977acce
Erik Rasmussen <ecr.referee@hotmail.com>
2023-11-08 16:35:00 -0600

    Copy the CAPI app for each provider

---
+++ apps/capi/gen/clusterctl-config.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -eum
+
+if ! command -v pulumi >/dev/null 2>&1; then
+    echo "Install pulumi first https://www.pulumi.com/docs/install/"
+    exit 1
+fi
+
+root="$(git rev-parse --show-toplevel)/apps/clusterapi"
+
+function version() {
+    dep=$1
+    pulumi -C "$root" config get --path "versions.$dep"
+}
+
+echo "Getting versions..."
+proxmoxVersion="$(version "proxmox")"
+
+cat >"$root/clusterctl.yaml" <<EOL
+providers:
+  - name: proxmox
+    url: https://github.com/sp-yduck/cluster-api-provider-proxmox/releases/v$proxmoxVersion/infrastructure-components.yaml
+    type: InfrastructureProvider
+EOL
---
+++ apps/cappx/index.ts
@@ -0,0 +1,151 @@
+import * as pulumi from '@pulumi/pulumi';
+import * as k8s from '@pulumi/kubernetes';
+import * as path from 'path';
+
+interface Versions {
+  uefi: string;
+}
+
+const config = new pulumi.Config();
+const versions = config.requireObject<Versions>('versions');
+const stack = pulumi.getStack();
+const metallbStack = new pulumi.StackReference('metallb', {
+  name: `UnstoppableMango/thecluster-metallb/${stack}`,
+});
+
+const core = new k8s.yaml.ConfigFile('core', {
+  file: path.join('manifests', stack, 'cluster-api-core', 'output.yaml'),
+}, {
+  // Would be nice to find a way to ignore just the aggregated manager role rules, not all rules
+  ignoreChanges: [
+    'spec.conversion.webhook.clientConfig.caBundle', // cert-manager injects `caBundle`s
+    'rules', // Aggregated ClusterRole will get rules filled in by controlplane
+  ],
+});
+
+const bootstrap = new k8s.yaml.ConfigGroup('bootstrap', {
+  files: path.join('manifests', stack, 'talos-bootstrap', 'output.yaml'),
+  transformations: [patchControllerManagerPorts],
+}, {
+  dependsOn: core.ready,
+  ignoreChanges: [
+    'spec.conversion.webhook.clientConfig.caBundle', // cert-manager injects `caBundle`s
+  ],
+});
+
+const controlplane = new k8s.yaml.ConfigGroup('controlplane', {
+  files: path.join('manifests', stack, 'talos-control-plane', 'output.yaml'),
+  transformations: [patchControllerManagerPorts],
+}, { dependsOn: bootstrap.ready });
+
+const infrastructure = new k8s.yaml.ConfigGroup('infrastructure', {
+  files: [
+    'sidero-infrastructure',
+    'proxmox-infrastructure',
+  ].map(x => path.join('manifests', stack, x, 'output.yaml')),
+  transformations: [patchSideroManager, patchProxmoxService],
+}, {
+  dependsOn: controlplane.ready,
+  ignoreChanges: [
+    'spec.conversion.webhook.clientConfig.caBundle', // cert-manager injects `caBundle`s
+  ],
+});
+
+const sideroLb = new k8s.core.v1.Service('siderolb', {
+  metadata: {
+    name: 'siderolb',
+    namespace: 'sidero-system'
+  },
+  spec: {
+    type: k8s.types.enums.core.v1.ServiceSpecType.LoadBalancer,
+    loadBalancerClass: metallbStack.requireOutput('loadBalancerClass'),
+    ports: [{
+      name: 'dhcp',
+      port: 67,
+      protocol: 'UDP',
+      targetPort: 'dhcp',
+    }, {
+      name: 'http',
+      port: 8081,
+      protocol: 'TCP',
+      targetPort: 'http',
+    }, {
+      name: 'siderolink',
+      port: 51821,
+      protocol: 'UDP',
+      targetPort: 'siderolink',
+    }, {
+      name: 'tftp',
+      port: 69,
+      protocol: 'UDP',
+      targetPort: 'tftp',
+    }],
+    selector: {
+      app: 'sidero',
+      'cluster.x-k8s.io/provider': 'sidero',
+      'cluster.x-k8s.io/v1alpha3': 'v1alpha3',
+      'cluster.x-k8s.io/v1alpha4': 'v1alpha3',
+      'cluster.x-k8s.io/v1beta1': 'v1alpha3',
+      'control-plane': 'sidero-controller-manager',
+    },
+  },
+}, { dependsOn: infrastructure.ready });
+
+function patchControllerManagerPorts(obj: any, opts: pulumi.CustomResourceOptions): void {
+  if (obj.kind !== 'Deployment') return;
+
+  const deployments = ['cabpt-controller-manager', 'cacppt-controller-manager'];
+
+  if (!deployments.includes(obj.metadata.name)) return;
+
+  // Seems to be a bug in the templates. Metrics binds to 8080, but there is no corresponding port
+  obj.spec.template.spec.containers[0].ports.push({
+    containerPort: 8080,
+    name: 'https',
+    protocol: 'TCP',
+  });
+}
+
+function patchProxmoxService(obj: any, opts: pulumi.CustomResourceOptions): void {
+  if (obj.kind !== 'Service') return;
+  if (obj.metadata.name !== 'cluster-api-provider-proxmox-controller-manager-metrics-service') return;
+
+  obj.metadata.labels['control-plane'] = 'capp-controller-manager';
+  obj.spec.selector['control-plane'] = 'capp-controller-manager';
+  obj.spec.selector['cluster.x-k8s.io/aggregate-to-manager'] = undefined
+}
+
+function patchSideroManager(obj: any, opts: pulumi.CustomResourceOptions): void {
+  if (obj.kind !== 'Deployment') return;
+  if (obj.metadata.name !== 'sidero-controller-manager') return;
+
+  if (!obj.spec.template.spec.volumes) {
+    obj.spec.template.spec.volumes = [];
+  }
+
+  obj.spec.template.spec.volumes.push({
+    name: 'tftp-folder',
+    emptyDir: {},
+  });
+
+  if (!obj.spec.template.spec.initContainers) {
+    obj.spec.template.spec.initContainers = [];
+  }
+
+  obj.spec.template.spec.initContainers.push({
+    image: `ghcr.io/unstoppablemango/raspberrypi4-uefi:${versions.uefi}`,
+    imagePullPolicy: 'Always',
+    name: 'tftp-folder-setup',
+    command: ['cp'],
+    args: ['-r', '/tftp', '/var/lib/sidero'],
+    volumeMounts: [{
+      mountPath: '/var/lib/sidero/tftp',
+      name: 'tftp-folder',
+    }],
+  });
+
+  obj.spec.template.spec.containers.find((x: any) => x.name === 'manager').volumeMounts.push({
+    mountPath: '/var/lib/sidero/tftp',
+    name: 'tftp-folder',
+  });
+}
