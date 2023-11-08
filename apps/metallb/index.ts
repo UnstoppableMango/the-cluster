44e3969b761b1bca73d47db635d4712fed9d94a0
Erik Rasmussen <ecr.referee@hotmail.com>
2023-11-07 20:24:48 -0600

    WIP generating CRDs

--- apps/metallb/index.ts
+++ apps/metallb/index.ts
@@ -1,17 +1,12 @@
-import * as pulumi from '@pulumi/pulumi';
 import * as k8s from '@pulumi/kubernetes';
-
-const ns = new k8s.core.v1.Namespace('metallb-system', {
-  metadata: { name: 'metallb-system' },
-});
 
 const chart = new k8s.helm.v3.Chart('metallb', {
   path: './',
-  namespace: ns.metadata.name,
   skipCRDRendering: true,
   values: {
     metallb: {
       crds: { enabled: false },
+      loadBalancerClass: 'metallb',
     },
   },
 });
