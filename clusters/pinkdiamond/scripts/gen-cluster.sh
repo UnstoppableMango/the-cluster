508067441e5972649be1880ce5b346d27242a099
Erik Rasmussen <ecr.referee@hotmail.com>
2023-11-08 16:04:01 -0600

    Copy stuff from split-capi branch
    This needs some tweaks to make it work for pink diamond so doing it in a separate commit

--- clusters/pinkdiamond/scripts/gen-cluster.sh
+++ clusters/pinkdiamond/scripts/gen-cluster.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 set -eu
 
-root="$(git rev-parse --show-toplevel)/clusters/pinkdiamond"
+root="$(git rev-parse --show-toplevel)/clusters/rosequartz"
 
-talosVersion="$(cat $root/.versions | yq -r '."siderolabs/talos"')"
-k8sVersion="$(cat $root/.versions | yq -r '."kubernetes/kubernetes"')"
-sideroVersion="$(cat $root/.versions | yq -r '."siderolabs/sidero"')"
+stack="$(pulumi -C "$root" stack --show-name)"
+talosVersion="$(pulumi -C "$root" config get --path 'versions.talos')"
+k8sVersion="$(pulumi -C "$root" config get --path 'versions.k8s')"
+sideroVersion="$(pulumi -C "$root" config get --path 'versions.sidero')"
+clusterName="$(pulumi -C "$root" config get clusterName)"
 
-export CONTROL_PLANE_SERVERCLASS=rpi4.md
-export WORKER_SERVERCLASS=rpi4.md
+export CONTROL_PLANE_SERVERCLASS="$(pulumi -C "$root" config get --path 'serverClass.controlPlane')"
+export WORKER_SERVERCLASS="$(pulumi -C "$root" config get --path 'serverClass.worker')"
 export TALOS_VERSION="v$talosVersion"
 export KUBERNETES_VERSION="v$k8sVersion"
-export CONTROL_PLANE_PORT=6444
-export CONTROL_PLANE_ENDPOINT="192.168.1.100"
+export CONTROL_PLANE_PORT="$(pulumi -C "$root" config get --path 'controlPlane.port')"
+export CONTROL_PLANE_ENDPOINT="$(pulumi -C "$root" config get --path 'controlPlane.endpoint')"
 
 echo "Talos Version:      $TALOS_VERSION"
 echo "Kubernetes Version: $KUBERNETES_VERSION"
-echo "Sidero Version:     $sideroVersion"
+echo "Sidero Version:     v$sideroVersion"
+echo ""
+
+mkdir -p "$root/manifests/$stack"
 
 echo "Generating cluster..."
-clusterctl generate cluster pink-diamond \
+clusterctl generate cluster "$clusterName" \
     --infrastructure "sidero:v$sideroVersion" \
-    --target-namespace "pink-diamond" \
-    --control-plane-machine-count "3" \
-    --worker-machine-count "2" \
-    > "$root/manifests/cluster.yaml"
+    --target-namespace "$clusterName" \
+    --control-plane-machine-count "$(pulumi -C "$root" config get --path 'controlPlane.machineCount')" \
+    --worker-machine-count "$(pulumi -C "$root" config get --path 'worker.machineCount')" \
+    > "$root/manifests/$stack/cluster.yaml"
