c76c31a0a2f15d202d1d770b47a3bce6fe2a00f0
Erik Rasmussen <ecr.referee@hotmail.com>
2023-11-14 16:04:15 -0600

    WIP migrating to talos

--- clusters/pinkdiamond/index.ts
+++ clusters/pinkdiamond/index.ts
@@ -1,10 +1,7 @@
 import * as pulumi from '@pulumi/pulumi';
 import * as k8s from '@pulumi/kubernetes';
 import * as cloudflare from '@pulumi/cloudflare';
-import * as cluster from '@pulumi/crds/cluster/v1beta1';
-import * as infra from '@pulumi/crds/infrastructure';
-import * as metal from '@pulumi/crds/metal/v1alpha2';
-import * as controlPlane from '@pulumi/crds/controlplane/v1alpha3';
+import * as talos from '@pulumiverse/talos';
 import { Node, Versions } from './types';
 
 const templatesRef = new pulumi.StackReference('templates', {
@@ -12,26 +9,18 @@ const templatesRef = new pulumi.StackReference('templates', {
 });
 
 const config = new pulumi.Config();
-const controlPlaneConfig = config.requireObject<ControlPlaneConfig>('controlPlane');
+const controlPlaneConfig = config.requireObject<Node[]>('controlPlanes');
+const workerConfig = config.requireObject<Node[]>('workers');
 const versions = config.requireObject<Versions>('versions');
-const proxmox = config.requireObject<Proxmox>('proxmox');
-
-const commonLabels: Record<string, string> = {
-  'cluster.x-k8s.io/cluster-name': config.require('clusterName'),
-};
 
 // TODO: Tunnel
-const publicEndpoint = new cloudflare.Record('pd.thecluster.io', {
-  name: 'pd.thecluster.io',
-  type: 'A',
-  zoneId: config.require('zoneId'),
-  proxied: false,
-  value: config.requireSecret('publicIp'),
-});
-
-const ns = new k8s.core.v1.Namespace('pinkdiamond', {
-  metadata: { name: 'pinkdiamond' },
-});
+// const publicEndpoint = new cloudflare.Record('pd.thecluster.io', {
+//   name: 'pd.thecluster.io',
+//   type: 'A',
+//   zoneId: config.require('zoneId'),
+//   proxied: false,
+//   value: config.requireSecret('publicIp'),
+// });
 
 // Subject Alternative Names to use for certificates
 const certSans = [
@@ -44,102 +33,3 @@ const certSans = [
 
 const controlPlaneTemplate = templatesRef.requireOutput('rp4.md') as pulumi.Output<infra.v1alpha3.MetalMachineTemplate>;
 
-const talosControlPlane = new controlPlane.TalosControlPlane('pinkdiamond', {
-  metadata: {
-    name: 'pinkdiamond',
-    namespace: ns.metadata.name,
-  },
-  spec: {
-    version: `v${versions.k8s}`,
-    replicas: controlPlaneConfig.machineCount,
-    controlPlaneConfig: {
-      controlplane: {
-        generateType: 'controlplane',
-        talosVersion: `v${versions.talos}`,
-        configPatches: [{
-          op: 'replace',
-          path: '/cluster/extraManifests',
-          value: [
-            `https://raw.githubusercontent.com/alex1989hu/kubelet-serving-cert-approver/v${versions.ksca}/deploy/standalone-install.yaml`,
-          ],
-        }, {
-          op: 'replace',
-          path: '/cluster/apiServer/certSANs',
-          value: certSans,
-        }, {
-          op: 'replace',
-          path: '/machine/certSANs',
-          value: certSans,
-        }, {
-          op: 'add',
-          path: '/machine/kubelet/extraArgs',
-          value: {
-            'rotate-server-certificates': true,
-          },
-        }, {
-          op: 'add',
-          path: '/machine/network',
-          value: {
-            interfaces: [{
-              deviceSelector: {
-                hardwareAddr: 'd8:3a:dd:*',
-              },
-              dhcp: true,
-              vip: {
-                ip: config.require('vip'),
-              },
-            }],
-          },
-        }],
-      },
-    },
-    infrastructureTemplate: {
-      apiVersion: controlPlaneTemplate.apiVersion.apply(x => x ?? ''),
-      kind: controlPlaneTemplate.kind.apply(x => x ?? ''),
-      name: pulumi.output(controlPlaneTemplate.metadata).apply(x => x?.name ?? ''),
-    },
-  },
-});
-
-const proxmoxCluster = new infra.v1beta1.ProxmoxCluster('pinkdiamond', {
-  metadata: {
-    name: 'pink-diamond',
-    namespace: ns.metadata.name,
-  },
-  spec: {
-    serverRef: {
-      endpoint: proxmox.endpoint,
-      secretRef: {
-        name: '', // TODO
-      },
-    },
-  },
-});
-
-const pinkdiamondCluster = new cluster.Cluster('pinkdiamond', {
-  metadata: {
-    name: config.require('clusterName'),
-    namespace: ns.metadata.name,
-    labels: commonLabels,
-  },
-  spec: {
-    clusterNetwork: {
-      pods: {
-        cidrBlocks: ['10.244.0.0/16'],
-      },
-      services: {
-        cidrBlocks: ['10.96.0.0/12'],
-      },
-    },
-    controlPlaneRef: {
-      apiVersion: talosControlPlane.apiVersion.apply(x => x ?? ''),
-      kind: talosControlPlane.kind.apply(x => x ?? ''),
-      name: pulumi.output(talosControlPlane.metadata).apply(x => x?.name ?? ''),
-    },
-    infrastructureRef: {
-      apiVersion: proxmoxCluster.apiVersion.apply(x => x ?? ''),
-      kind: proxmoxCluster.kind.apply(x => x ?? ''),
-      name: pulumi.output(proxmoxCluster.metadata).apply(x => x?.name ?? ''),
-    },
-  },
-});
