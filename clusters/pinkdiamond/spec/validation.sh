d67417ac82e91912b33132df78933d1f4242ff05
Erik Rasmussen <ecr.referee@hotmail.com>
2023-11-14 16:00:14 -0600

    WIP fetching stuff from the other branch

--- clusters/pinkdiamond/spec/validation.sh
+++ clusters/pinkdiamond/spec/validation.sh
@@ -6,10 +6,6 @@ root="$(git rev-parse --show-toplevel)/stacks/pinkdiamond"
 
 echo -e "Running Pink Diamond tests\n"
 
-if ! $root/spec/manifest-validation.sh; then
-    exitCode=1
-fi
-
 if ! $root/spec/infra-validation.sh; then
     exitCode=1
 fi
