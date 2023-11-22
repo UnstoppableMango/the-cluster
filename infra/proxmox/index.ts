175ac107d7576980877486350e3937a8ec03e9c1
Erik Rasmussen <ecr.referee@hotmail.com>
2023-11-21 21:20:46 -0600

    Add stack for provisioning proxmox infrastructure (#1633)

---
+++ infra/proxmox/index.ts
@@ -0,0 +1,77 @@
+import * as pulumi from '@pulumi/pulumi';
+import * as pve from '@muhlba91/pulumi-proxmoxve';
+import { Versions, Node } from './types';
+
+const config = new pulumi.Config();
+const suffix = config.get('suffix') ?? '';
+const versions = config.requireObject<Versions>('versions');
+const controlPlaneConfig = config.requireObject<Node[]>('controlPlanes');
+const workerConfig = config.requireObject<Node[]>('workers');
+const iso = config.requireObject<{ node: string }>('iso');
+const provider = new pve.Provider('zeus', config.requireObject('proxmox'));
+const commonOpts: pulumi.CustomResourceOptions = { provider };
+
+const talosIso = new pve.storage.File(`talos${suffix}`, {
+  datastoreId: 'isos',
+  nodeName: iso.node,
+  overwrite: true,
+  sourceFile: {
+    path: `https://github.com/siderolabs/talos/releases/download/v${versions.talos}/metal-amd64.iso`,
+    checksum: '9bdbd2ecd35fe94298d017bc137540a439ab3ca4c077e24712632aef2d202bf7',
+    fileName: `talos-${versions.talos}-metal-amd64.iso`,
+  },
+}, commonOpts);
+
+export const controlPlanes = controlPlaneConfig.map(newNode('c'));
+export const workers = workerConfig.map(newNode('w'));
+
+function newNode(type: string): (data: Node, i: number) => pve.vm.VirtualMachine {
+  return (data, i) => new pve.vm.VirtualMachine(`px${type}k8s${i}${suffix}`, {
+    name: `px${type}k8s${i}${suffix}`,
+    nodeName: data.node,
+    bios: 'ovmf',
+    operatingSystem: {
+      type: 'l26',
+    },
+    efiDisk: {
+      datastoreId: 'spool',
+      fileFormat: 'raw',
+    },
+    onBoot: true,
+    cpu: {
+      // https://www.talos.dev/v1.5/talos-guides/install/virtualized-platforms/proxmox/#create-vms
+      type: 'host',
+      cores: data.cpu,
+      units: 1024,
+    },
+    memory: {
+      dedicated: data.mem,
+    },
+    bootOrders: [
+      'scsi0',
+      'ide2',
+    ],
+    cdrom: {
+      enabled: true,
+      fileId: talosIso.id,
+      interface: 'ide2',
+    },
+    scsiHardware: 'virtio-scsi-single',
+    disks: [
+      {
+        interface: 'scsi0',
+        datastoreId: 'spool',
+        size: 100,
+        ssd: true,
+        discard: 'on',
+        fileFormat: 'raw',
+      },
+    ],
+    networkDevices: [{
+      bridge: data.net,
+    }],
+  }, {
+    ...commonOpts,
+    ignoreChanges: ['disks[0].speed'],
+  });
+}
---
+++ infra/proxmox/types.ts
@@ -0,0 +1,10 @@
+export interface Versions {
+  talos: string;
+}
+
+export interface Node {
+  node: string;
+  cpu: number;
+  mem: number;
+  net: string;
+}
