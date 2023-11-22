1a7b73ad44743af61c2519615f4deffd1b6990ef
Erik Rasmussen <ecr.referee@hotmail.com>
2023-11-21 21:05:50 -0600

    Cleanin up

--- infra/proxmox/index.ts
+++ infra/proxmox/index.ts
@@ -11,23 +11,19 @@ const iso = config.requireObject<{ node: string }>('iso');
 const provider = new pve.Provider('zeus', config.requireObject('proxmox'));
 const commonOpts: pulumi.CustomResourceOptions = { provider };
 
-// const pveNodes = [...controlPlaneConfig]
-//   .map(x => x.node)
-//   .filter((x, i, a) => a.indexOf(x) === i);
-
 const talosIso = new pve.storage.File(`talos${suffix}`, {
   datastoreId: 'isos',
   nodeName: iso.node,
   overwrite: true,
   sourceFile: {
     path: `https://github.com/siderolabs/talos/releases/download/v${versions.talos}/metal-amd64.iso`,
-    // checksum: '9bdbd2ecd35fe94298d017bc137540a439ab3ca4c077e24712632aef2d202bf7',
+    checksum: '9bdbd2ecd35fe94298d017bc137540a439ab3ca4c077e24712632aef2d202bf7',
     fileName: `talos-${versions.talos}-metal-amd64.iso`,
   },
 }, commonOpts);
 
-const controlPlanes = controlPlaneConfig.map(newNode('c'));
-const workers = workerConfig.map(newNode('w'));
+export const controlPlanes = controlPlaneConfig.map(newNode('c'));
+export const workers = workerConfig.map(newNode('w'));
 
 function newNode(type: string): (data: Node, i: number) => pve.vm.VirtualMachine {
   return (data, i) => new pve.vm.VirtualMachine(`px${type}k8s${i}${suffix}`, {
@@ -42,19 +38,9 @@ function newNode(type: string): (data: Node, i: number) => pve.vm.VirtualMachine
       fileFormat: 'raw',
     },
     onBoot: true,
-    // kvmArguments: '-cpu kvm64,+cx16,+lahf_lm,+popcnt,+sse3,+ssse3,+sse4.1,+sse4.2',
     cpu: {
       // https://www.talos.dev/v1.5/talos-guides/install/virtualized-platforms/proxmox/#create-vms
       type: 'host',
-      // flags: [
-      //   '+cx16',
-      //   '+lahf_lm',
-      //   '+popcnt',
-      //   '+sse3',
-      //   '+ssse3',
-      //   '+sse4.1',
-      //   '+sse4.2',
-      // ],
       cores: data.cpu,
       units: 1024,
     },
@@ -64,7 +50,6 @@ function newNode(type: string): (data: Node, i: number) => pve.vm.VirtualMachine
     bootOrders: [
       'scsi0',
       'ide2',
-      // 'net0',
     ],
     cdrom: {
       enabled: true,
@@ -80,10 +65,13 @@ function newNode(type: string): (data: Node, i: number) => pve.vm.VirtualMachine
         ssd: true,
         discard: 'on',
         fileFormat: 'raw',
-      }
+      },
     ],
     networkDevices: [{
       bridge: data.net,
     }],
-  }, commonOpts);
+  }, {
+    ...commonOpts,
+    ignoreChanges: ['disks[0].speed'],
+  });
 }
