a726334b399fd5b6dbb87d6dd1aeb44b9f89cd1f
Erik Rasmussen <ecr.referee@hotmail.com>
2023-11-23 14:03:03 -0600

    Install cert-manager on pinkdiamond (#1657)

--- apps/cert-manager/index.ts
+++ apps/cert-manager/index.ts
@@ -1,8 +1,22 @@
+import * as pulumi from '@pulumi/pulumi';
 import * as k8s from '@pulumi/kubernetes';
+
+const config = new pulumi.Config();
+
+let provider: k8s.Provider | undefined = undefined;
+if (config.get('useStackRef')) {
+  const stackRef = new pulumi.StackReference(
+    'UnstoppableMango/thecluster-pinkdiamond/prod',
+  );
+  
+  provider = new k8s.Provider(pulumi.getStack(), {
+    kubeconfig: stackRef.requireOutput('kubeconfig'),
+  });
+}
 
 const ns = new k8s.core.v1.Namespace('cert-manager', {
   metadata: { name: 'cert-manager' },
-});
+}, { provider });
 
 // Use a release because the cert-manager helm chart uses hooks
 const release = new k8s.helm.v3.Release('cert-manager', {
@@ -14,4 +28,4 @@ const release = new k8s.helm.v3.Release('cert-manager', {
   dependencyUpdate: true,
   lint: true,
   skipCrds: true,
-});
+}, { provider });
