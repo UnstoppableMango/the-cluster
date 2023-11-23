94cd06049441073a6f0b3a652b2e19a705c46758
Erik Rasmussen <ecr.referee@hotmail.com>
2023-11-22 19:52:32 -0600

    Update rq hack scripts to use .config dir (#1641)

--- clusters/rosequartz/hack/source-me.sh
+++ clusters/rosequartz/hack/source-me.sh
@@ -1,4 +1,4 @@
 root="$(git rev-parse --show-toplevel)/clusters/rosequartz"
 stack="$(pulumi -C "$root" stack --show-name)"
-export KUBECONFIG="$root/.kube/$stack/config"
-export TALOSCONFIG="$root/.talos/$stack/talosconfig.yaml"
+export KUBECONFIG="$root/.config/$stack/kubeconfig"
+export TALOSCONFIG="$root/.config/$stack/talosconfig.yaml"
