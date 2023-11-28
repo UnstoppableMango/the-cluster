73084118574a2f6c3a9194ae1575bd760e8e3775
Erik Rasmussen <ecr.referee@hotmail.com>
2023-11-28 00:29:07 -0600

    Fiddlin

--- apps/unifi/index.ts
+++ apps/unifi/index.ts
@@ -1,4 +1,3 @@
-import * as pulumi from '@pulumi/pulumi';
 import * as k8s from '@pulumi/kubernetes';
 import { provider } from './clusters';
 
@@ -9,5 +8,11 @@ const ns = new k8s.core.v1.Namespace('unifi', {
 const chart = new k8s.helm.v3.Chart('unifi', {
   path: './',
   namespace: ns.metadata.name,
-  values: {},
+  values: {
+    unifi: {
+      image: {
+        // tag: '',
+      },
+    }
+  },
 }, { provider });
