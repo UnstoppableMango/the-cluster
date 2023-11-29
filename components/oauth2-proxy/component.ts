e76a8042d3942f73a76b056dd6431a7b4ac14391
Erik Rasmussen <ecr.referee@hotmail.com>
2023-11-28 18:31:15 -0600

    WIP on component

--- components/oauth2-proxy/component.ts
+++ components/oauth2-proxy/component.ts
@@ -1,2 +1,15 @@
-import { ComponentResource } from '@pulumi/pulumi';
+import { ComponentResource, ComponentResourceOptions } from '@pulumi/pulumi';
 import * as k8s from '@pulumi/kubernetes';
+
+export interface Oauth2ProxyArgs {
+  redirectUrl: string;
+}
+
+export class Oauth2Proxy extends ComponentResource {
+
+  constructor(name: string, args: Oauth2ProxyArgs, opts: ComponentResourceOptions) {
+    super('unmango:OAuth2Proxy', name, args, opts);
+    this.registerOutputs();
+  }
+
+}
