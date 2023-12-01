e84c0bda66835cf23742efa075698177531287e5
Erik Rasmussen <ecr.referee@hotmail.com>
2023-12-01 16:43:38 -0600

    Fix metallb by manually creating the Service

--- apps/nginx-ingress/apps/metallb.ts
+++ apps/nginx-ingress/apps/metallb.ts
@@ -2,7 +2,7 @@ import { StackReference } from '@pulumi/pulumi';
 import { cluster } from '../config';
 
 const ref = new StackReference('metallb', {
-  name: `UntoppableMango/thecluster-metallb/${cluster}`,
+  name: `UnstoppableMango/thecluster-metallb/${cluster}`,
 });
 
 export const loadBalancerClass = ref.requireOutput('loadBalancerClass');
--- apps/nginx-ingress/index.ts
+++ apps/nginx-ingress/index.ts
@@ -1,6 +1,8 @@
+import * as pulumi from '@pulumi/pulumi';
 import * as k8s from '@pulumi/kubernetes';
 import * as nginx from '@pulumi/crds/charts/v1alpha1/nginxIngress';
 import { provider } from './clusters';
+import { loadBalancerClass } from './apps/metallb';
 import { versions } from './config';
 
 const internalNs = new k8s.core.v1.Namespace('internal-ingress', {
@@ -31,9 +33,42 @@ const internal = new nginx.NginxIngress('internal', {
       healthStatus: true,
       hostnetwork: false,
       enableSnippets: true,
+      service: { create: false },
     },
   },
 }, { provider });
+
+const internalSvc = new k8s.core.v1.Service('internal', {
+  metadata: {
+    name: pulumi.interpolate`${internal.metadata.apply(x => x?.name)}-nginx-ingress-controller`,
+    namespace: internalNs.metadata.name,
+  },
+  spec: {
+    loadBalancerClass: loadBalancerClass,
+    // Copied from the template
+    type: 'LoadBalancer',
+    selector: {
+      'app.kubernetes.io/name': 'nginx-ingress',
+      'app.kubernetes.io/instance': 'internal',
+    },
+    allocateLoadBalancerNodePorts: true,
+    externalTrafficPolicy: 'Local',
+    ports: [
+      {
+        name: 'http',
+        protocol: 'TCP',
+        port: 80,
+        targetPort: 80,
+      },
+      {
+        name: 'https',
+        protocol: 'TCP',
+        port: 443,
+        targetPort: 443,
+      },
+    ],
+  },
+}, { provider, dependsOn: internal });
 
 const clusterNs = new k8s.core.v1.Namespace('cluster-ingress', {
   metadata: { name: 'cluster-ingress' },
