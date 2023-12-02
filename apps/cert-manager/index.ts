80ad1c07baef4378bace46a375e7890aae7865e6
Erik Rasmussen <ecr.referee@hotmail.com>
2023-12-01 22:03:04 -0600

    Add cloudflare issuers (#1739)

--- apps/cert-manager/index.ts
+++ apps/cert-manager/index.ts
@@ -1,17 +1,13 @@
 import * as pulumi from '@pulumi/pulumi';
 import * as k8s from '@pulumi/kubernetes';
+import * as cf from '@pulumi/cloudflare';
+import * as acme from '@pulumi/crds/acme/v1';
+import * as cm from '@pulumi/crds/certmanager/v1';
+import { provider } from './clusters';
+import { permissionGroups, suffix, zone } from './config';
 
-const config = new pulumi.Config();
-
-let provider: k8s.Provider | undefined = undefined;
-if (config.get('useStackRef')) {
-  const stackRef = new pulumi.StackReference(
-    'UnstoppableMango/thecluster-pinkdiamond/prod',
-  );
-  
-  provider = new k8s.Provider(pulumi.getStack(), {
-    kubeconfig: stackRef.requireOutput('kubeconfig'),
-  });
+function appendIf(x: string, o?: string | undefined | null): string {
+  return o ? x + o : x;
 }
 
 const ns = new k8s.core.v1.Namespace('cert-manager', {
@@ -29,3 +25,75 @@ const release = new k8s.helm.v3.Release('cert-manager', {
   lint: true,
   skipCrds: true,
 }, { provider });
+
+const apiToken = new cf.ApiToken('cert-manager', {
+  name: appendIf('THECLUSTER-cert-manager', suffix),
+  policies: [
+    {
+      permissionGroups: permissionGroups.apply(g => [
+        g.zone['Zone Read'],
+        g.zone['DNS Write'],
+      ]),
+      resources: zone.apply(z => ({
+        [`com.cloudflare.api.account.zone.${z.zoneId}`]: '*',
+      })),
+    },
+  ],
+});
+
+const tokenSecret = new k8s.core.v1.Secret('cloudflare-api-token', {
+  metadata: {
+    name: 'cloudflare-api-token',
+    namespace: ns.metadata.name,
+  },
+  stringData: {
+    'api-token': apiToken.value,
+  },
+}, { provider });
+
+const cloudflareStagingIssuer = new cm.ClusterIssuer('le-stage-cloudflare', {
+  metadata: { name: 'le-stage-cloudflare' },
+  spec: {
+    acme: {
+      privateKeySecretRef: {
+        name: 'cloudflare-staging-account-key',
+      },
+      server: 'https://acme-staging-v02.api.letsencrypt.org/directory',
+      solvers: [{
+        dns01: {
+          cloudflare: {
+            apiTokenSecretRef: {
+              name: tokenSecret.metadata.name,
+              key: 'api-token',
+            },
+          },
+        },
+      }],
+    },
+  },
+}, { provider });
+
+const cloudflareIssuer = new cm.ClusterIssuer('le-cloudflare', {
+  metadata: { name: 'le-cloudflare' },
+  spec: {
+    acme: {
+      privateKeySecretRef: {
+        name: 'cloudflare-account-key',
+      },
+      server: 'https://acme-v02.api.letsencrypt.org/directory',
+      solvers: [{
+        dns01: {
+          cloudflare: {
+            apiTokenSecretRef: {
+              name: tokenSecret.metadata.name,
+              key: 'api-token',
+            },
+          },
+        },
+      }],
+    },
+  },
+}, { provider });
+
+export const staging = pulumi.output(cloudflareStagingIssuer.metadata).apply(x => x?.name);
+export const prod = pulumi.output(cloudflareIssuer.metadata).apply(x => x?.name);
