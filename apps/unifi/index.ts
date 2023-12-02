f0fbe9b875fc1c9c24cae898be0dd5817ac9eb01
Erik Rasmussen <ecr.referee@hotmail.com>
2023-12-02 00:06:55 -0600

    Just raw-doggin this one

--- apps/unifi/index.ts
+++ apps/unifi/index.ts
@@ -1,5 +1,7 @@
 import * as k8s from '@pulumi/kubernetes';
+import * as pihole from '@unmango/pulumi-pihole';
 import { clusterIssuers, ingresses, provider } from '@unstoppablemango/thecluster/cluster/from-stack';
+import { versions } from './config';
 
 const loadBalancerIP = '192.168..1.87';
 
@@ -7,41 +9,109 @@ const ns = new k8s.core.v1.Namespace('unifi', {
   metadata: { name: 'unifi' },
 }, { provider });
 
-const chart = new k8s.helm.v3.Chart('unifi', {
-  path: './',
-  namespace: ns.metadata.name,
-  values: {
-    unifi: {
-      image: {
-        // tag: '',
-      },
-      service: {
-        main: {
-          loadBalancerIP,
-          sharedKey: 'unifi-svc',
-        },
-        comm: {
-          loadBalancerIP,
-          sharedKey: 'unifi-svc',
-        },
-        stun: {
-          loadBalancerIP,
-          sharedKey: 'unifi-svc',
-        },
-        speedtest: {
-          loadBalancerIP,
-          sharedKey: 'unifi-svc',
-        },
-        guestportal: {
-          loadBalancerIP,
-          sharedKey: 'unifi-svc',
-        },
+// const chart = new k8s.helm.v3.Chart('unifi', {
+//   path: './',
+//   namespace: ns.metadata.name,
+//   values: {
+//     unifi: {
+//       image: {
+//         // tag: '',
+//       },
+//     }
+//   },
+// }, { provider });
+
+// const service = chart.getResource('v1/Service', 'unifi/unifi');
+
+const pvc = new k8s.core.v1.PersistentVolumeClaim('unifi', {
+  metadata: {
+    name: 'unifi',
+    namespace: ns.metadata.name,
+  },
+  spec: {
+    storageClassName: storageClass,
+    accessModes: ['ReadWriteOnce'],
+    resources: {
+      requests: {
+        storage: '10Gi',
       },
     },
   },
 }, { provider });
 
-const service = chart.getResource('v1/Service', 'unifi/unifi');
+const deployment = new k8s.apps.v1.Deployment('unifi', {
+  metadata: {
+    name: 'unifi',
+    namespace: ns.metadata.name,
+  },
+  spec: {
+    selector: {
+      matchLabels: {
+        app: 'unifi',
+      },
+    },
+    template: {
+      metadata: {
+        name: 'unifi',
+        labels: {
+          app: 'unifi',
+        },
+      },
+      spec: {
+        securityContext: {
+          runAsNonRoot: true,
+          runAsUser: 999,
+          runAsGroup: 999,
+        },
+        containers: [{
+          name: 'unifi',
+          image: `jacobalberty/unifi:${versions.unifi}`,
+          env: [
+            { name: 'TZ', value: 'America/Chicago' },
+          ],
+          ports: [
+            {
+              name: 'device',
+              containerPort: 8080,
+            },
+            {
+              name: 'web',
+              containerPort: 8443,
+            },
+            {
+              name: 'stun',
+              containerPort: 3478,
+              protocol: 'UDP',
+            },
+            {
+              name: 'portal-https',
+              containerPort: 8843,
+            },
+            {
+              name: 'portal-http',
+              containerPort: 8880,
+            },
+            {
+              name: 'speed-test',
+              containerPort: 6789,
+            },
+          ],
+          volumeMounts: [{
+            name: 'unif',
+            mountPath: '/unifi',
+          }],
+        }],
+        volumes: [{
+          name: 'unifi',
+          persistentVolumeClaim: {
+            claimName: pvc.metadata.name,
+          },
+        }],
+      },
+    },
+  },
+}, { provider });
+
 const ingress = new k8s.networking.v1.Ingress('unifi', {
   metadata: {
     name: 'unifi',
