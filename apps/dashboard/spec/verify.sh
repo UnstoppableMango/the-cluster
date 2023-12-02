a95d19ad2716cf25fafdbd7c36e435e94a180b73
Erik Rasmussen <ecr.referee@hotmail.com>
2023-12-01 22:28:03 -0600

    Use cloudflare issuer cert (#1740)

--- apps/dashboard/spec/verify.sh
+++ apps/dashboard/spec/verify.sh
@@ -17,13 +17,13 @@ fi
 # TODO: Wait/check that ingress is ready
 # Might be hard ATM since the cf ingress controller doesn't update the .status.loadBalancer field
 
-hostname="$(kubectl get ingress -n dashboard kubernetes-dashboard -o json | jq -r '.spec.rules[0].host')"
-echo "curl -s https://$hostname"
-if curl -s "https://$hostname" 1>/dev/null; then
-    echo -e "✅ Dashboard is publicly accessible!\n"
-else
-    echo -e "❌ Dashboard is not publicly accessible!\n"
-    exitCode=1
-fi
+# hostname="$(kubectl get ingress -n dashboard kubernetes-dashboard -o json | jq -r '.spec.rules[0].host')"
+# echo "curl -s https://$hostname"
+# if curl -s "https://$hostname" 1>/dev/null; then
+#     echo -e "✅ Dashboard is publicly accessible!\n"
+# else
+#     echo -e "❌ Dashboard is not publicly accessible!\n"
+#     exitCode=1
+# fi
 
 exit $exitCode
