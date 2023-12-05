854b477a53569cd707fe6860b35ec5d2bf5114ca
Erik Rasmussen <ecr.referee@hotmail.com>
2023-12-05 16:31:16 -0600

    Limit exports so stack refs only run when used

--- lib/nodejs/index.ts
+++ lib/nodejs/index.ts
@@ -1,8 +1,2 @@
-export { rbdStorageClass, cephfsStorageClass } from './apps/ceph-csi';
-export { clusterIssuers } from './apps/cert-manager';
-export { ingressClass } from './apps/cloudflare-ingress';
-export { pool } from './apps/metallb';
-export { clusterClass, internalClass } from './apps/nginx-ingress';
-
 export * from './types';
 export * from './util';
