2cbd220837c2ed33181ec3f389e34649e3b7d241
Erik Rasmussen <ecr.referee@hotmail.com>
2023-12-05 15:40:28 -0600

    More lib stuff

--- apps/kong/index.ts
+++ apps/kong/index.ts
@@ -9,14 +9,43 @@ const ns = new k8s.core.v1.Namespace('kong-system', {
 const chart = new k8s.helm.v3.Chart('kong', {
   path: './',
   namespace: ns.metadata.name,
+  skipCRDRendering: false,
   values: {
+    // https://github.com/Kong/charts/tree/main/charts/kong#configuration
     kong: {
+      postgresql: { enabled: true },
+      proxy: {
+        enabled: true,
+      },
+      udpProxy: {
+        enabled: true,
+      },
       admin: {
         enabled: true,
+      },
+      manager: {
+        enabled: true,
+      },
+      portal: {
+        enabled: true,
+      },
+      portalapi: {
+        enabled: true,
+      },
+      // For hybrid control plane nodes
+      cluster: {
+        enabled: false,
+      },
+      // Assuming this is also for hybrid control plane nodes
+      clustertelemetry: {
+        enabled: false,
       },
       certificates: {
         enabled: true,
         clusterIssuer: issuer,
+      },
+      status: {
+        enabled: true,
       },
       env: {
         admin_listen: 6969, // TODO
