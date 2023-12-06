87b37cf4acf6a047044699b59970192ed0582c11
Erik Rasmussen <ecr.referee@hotmail.com>
2023-12-05 22:07:32 -0600

    Lay the kong down (#1775)
    * I can't read
    
    * WIP trying to fix config
    
    * More attempting. I'm just incopetent
    
    * Turn off kong

--- apps/kong/index.ts
+++ apps/kong/index.ts
@@ -1,7 +1,9 @@
+import * as pulumi from '@pulumi/pulumi';
 import * as k8s from '@pulumi/kubernetes';
 import * as random from '@pulumi/random';
 import * as pihole from '@unmango/pulumi-pihole';
 import { provider } from '@unmango/thecluster/cluster/from-stack';
+import { loadBalancerClass } from '@unmango/thecluster/apps/metallb';
 import { clusterIssuers } from '@unmango/thecluster/apps/cert-manager';
 import { ingressClass as cloudflareIngress } from '@unmango/thecluster/apps/cloudflare-ingress';
 import { internalClass as nginxIngress, loadBalancerIp } from '@unmango/thecluster/apps/nginx-ingress';
@@ -15,9 +17,29 @@ const ns = new k8s.core.v1.Namespace('kong-system', {
 
 export const ingressClass = 'kong';
 
-const pgPassword = new random.RandomPassword('postgresql', {
+const pgAdminPassword = new random.RandomPassword('postgres-password', {
   length: 32,
 });
+
+const pgKongPassword = new random.RandomPassword('postgres-kong', {
+  length: 32,
+});
+
+const pgReplicationPassword = new random.RandomPassword('postgres-replication', {
+  length: 32,
+});
+
+const pgSecret = new k8s.core.v1.Secret('postgres', {
+  metadata: {
+    name: 'kong-postgresql',
+    namespace: ns.metadata.name,
+  },
+  stringData: {
+    'postgres-password': pgAdminPassword.result,
+    password: pgKongPassword.result,
+    'replication-password': pgReplicationPassword.result,
+  },
+}, { provider });
 
 const chart = new k8s.helm.v3.Chart('kong', {
   path: './',
@@ -31,54 +53,28 @@ const chart = new k8s.helm.v3.Chart('kong', {
         type: 'ClusterIP',
         ingress: {
           enabled: true,
-          ingressClassName: nginxIngress,
+          ingressClassName: cloudflareIngress,
           hostname: hostnames.proxy,
-          tls: 'proxy-tls',
-        },
-      },
-      udpProxy: {
-        enabled: true,
-        ingress: {
-          enabled: true,
-          ingressClassName: nginxIngress,
-          hostname: hostnames.udpProxy,
-          tls: 'udpProxy-tls',
+          tls: 'kong-kong-proxy-cert',
         },
       },
       admin: {
         enabled: true,
+        type: 'ClusterIP',
         ingress: {
           enabled: true,
-          ingressClassName: nginxIngress,
+          ingressClassName: cloudflareIngress,
           hostname: hostnames.admin,
-          tls: 'admin-tls',
+          tls: 'kong-kong-admin-cert',
         },
       },
       manager: {
         enabled: true,
+        type: 'ClusterIP',
         ingress: {
           enabled: true,
-          ingressClassName: nginxIngress,
+          ingressClassName: cloudflareIngress,
           hostname: hostnames.manager,
-          tls: 'manager-tls',
-        },
-      },
-      portal: {
-        enabled: true,
-        ingress: {
-          enabled: true,
-          ingressClassName: nginxIngress,
-          hostname: hostnames.portal,
-          tls: 'portal-tls',
-        },
-      },
-      portalapi: {
-        enabled: true,
-        ingress: {
-          enabled: true,
-          ingressClassName: nginxIngress,
-          hostname: hostnames.portalapi,
-          tls: 'portalapi-tls',
         },
       },
       // For hybrid control plane nodes
@@ -91,12 +87,7 @@ const chart = new k8s.helm.v3.Chart('kong', {
       },
       status: {
         enabled: true,
-        ingress: {
-          enabled: true,
-          ingressClassName: nginxIngress,
-          hostname: hostnames.status,
-          tls: 'status-tls',
-        },
+        // tls: { enabled: true },
       },
       ingressController: {
         ingressClass,
@@ -105,42 +96,59 @@ const chart = new k8s.helm.v3.Chart('kong', {
       },
       autoscaling: { enabled: true },
       podDistruptionBudget: { enabled: true },
-      podSecurityPolicy: { enabled: true },
-      certificates: {
+      // certificates: {
+      //   enabled: true,
+      //   clusterIssuer: clusterIssuers.selfSigned,
+      //   proxy: {
+      //     commonName: 'proxy.kong.thecluster.lan',
+      //     dnsNames: ['proxy.kong.thecluster.lan'],
+      //   },
+      //   admin: {
+      //     commonName: 'admin.kong.thecluster.lan',
+      //     dnsNames: ['admin.kong.thecluster.lan'],
+      //   },
+      //   cluster: { enabled: false },
+      // },
+      // https://github.com/bitnami/charts/tree/main/bitnami/postgresql
+      postgresql: {
+        // Worth noting this uses an old postgres version
+        // https://github.com/Kong/charts/blob/05ce54f3f5399174f37e0c8cae0b38e0b620e5f5/charts/kong/values.yaml#L702
         enabled: true,
-        clusterIssuer: clusterIssuers.selfSigned,
-      },
-    },
-    // https://github.com/bitnami/charts/tree/main/bitnami/postgresql
-    postgresql: {
-      // Worth noting this uses an old postgres version
-      // https://github.com/Kong/charts/blob/05ce54f3f5399174f37e0c8cae0b38e0b620e5f5/charts/kong/values.yaml#L702
-      enabled: true,
-      global: {
-        storageClass: rbdStorageClass,
-      },
-      auth: {
-        password: pgPassword.result,
-      },
-      architecture: 'replicated',
-      replication: {
-        applicationName: 'kong',
+        global: {
+          storageClass: rbdStorageClass,
+        },
+        auth: {
+          existingSecret: pgSecret.metadata.name,
+        },
+        architecture: 'replicated',
+        replication: {
+          applicationName: 'kong',
+        },
       },
     },
   },
+  transformations: [
+    // (obj: any, opts: pulumi.CustomResourceOptions) => {
+    //   opts.ignoreChanges = ['data.["tls.crt"]', 'data.["tls.key"]', 'webhooks.[*].clientConfig.caBundle'];
+    // },
+    (obj: any, opts: pulumi.CustomResourceOptions) => {
+      if (obj.kind !== 'Ingress') return;
+      obj.metadata.annotations = { 'pulumi.com/skipAwait': 'true' };
+    },
+  ],
 }, { provider });
 
-const operator = new k8s.yaml.ConfigGroup('gateway-operator', {
-  files: [
-    `https://docs.konghq.com/assets/gateway-operator/${versions.gatewayOperator}/crds.yaml`,
-    `https://docs.konghq.com/assets/gateway-operator/${versions.gatewayOperator}/default.yaml`,
-  ],
-}, { provider, dependsOn: chart.ready })
+// const operator = new k8s.yaml.ConfigGroup('gateway-operator', {
+//   files: [
+//     `https://docs.konghq.com/assets/gateway-operator/${versions.gatewayOperator}/crds.yaml`,
+//     `https://docs.konghq.com/assets/gateway-operator/${versions.gatewayOperator}/default.yaml`,
+//   ],
+// }, { provider, dependsOn: chart.ready });
 
 export { hostnames };
 
-const dns = Object.entries(hostnames)
-  .map(([name, host]) => new pihole.DnsRecord(name, {
-    domain: host,
-    ip: loadBalancerIp,
-  }, { provider: piholeProvider, dependsOn: chart.ready }));
+// const dns = Object.entries(hostnames)
+//   .map(([name, host]) => new pihole.DnsRecord(name, {
+//     domain: host,
+//     ip: loadBalancerIp,
+//   }, { provider: piholeProvider, dependsOn: chart.ready }));
