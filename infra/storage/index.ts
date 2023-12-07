35a304c1d4ff96eca3e7ef536800ac103466d1b8
Erik Rasmussen <ecr.referee@hotmail.com>
2023-12-06 19:17:58 -0600

    Add test pvc and then start to rethink things

--- infra/storage/index.ts
+++ infra/storage/index.ts
@@ -1,2 +1,25 @@
 import * as pulumi from '@pulumi/pulumi';
 import * as k8s from '@pulumi/kubernetes';
+import { provider } from '@unmango/thecluster/cluster/from-stack';
+import { rbdStorageClass } from '@unmango/thecluster/apps/ceph-csi';
+
+const ns = new k8s.core.v1.Namespace('storage', {
+  metadata: { name: 'storage' },
+}, { provider });
+
+const testPvc = new k8s.core.v1.PersistentVolumeClaim('test', {
+  metadata: {
+    name: 'test',
+    namespace: ns.metadata.name,
+  },
+  spec: {
+    storageClassName: rbdStorageClass,
+    volumeMode: 'Block',
+    accessModes: ['ReadWriteOnce'],
+    resources: {
+      requests: {
+        storage: '15Gi',
+      },
+    },
+  },
+}, { provider });
