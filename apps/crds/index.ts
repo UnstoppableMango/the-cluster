9e31e0dc55b379fdc0e79ec4d43d97318977d868
Erik Rasmussen <ecr.referee@hotmail.com>
2023-12-10 17:10:58 -0600

    Use shared lib

--- apps/crds/index.ts
+++ apps/crds/index.ts
@@ -1,6 +1,6 @@
 import * as k8s from '@pulumi/kubernetes';
 import * as path from 'path';
-import { provider } from './clusters';
+import { provider } from '@unmango/thecluster/cluster/from-stack';
 import { enabled, versions } from './config';
 
 const paths: string[] = [];
