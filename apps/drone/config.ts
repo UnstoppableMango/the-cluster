f24a70649450548236d0a8a220e499c3446f9043
Erik Rasmussen <ecr.referee@hotmail.com>
2023-12-10 19:12:10 -0600

    Getting it slightly closer to working... (#1817)

--- apps/drone/config.ts
+++ apps/drone/config.ts
@@ -3,6 +3,7 @@ import { Config } from '@pulumi/pulumi';
 export interface GitHub {
   clientId: string;
   clientSecret: string;
+  token: string;
 }
 
 export interface Hosts {
@@ -22,4 +23,7 @@ export const github = config.requireObject<GitHub>('github');
 export const hosts = config.requireObject<Hosts>('hosts');
 export const versions = config.requireObject<Versions>('versions');
 export const userFilter = config.requireObject<string[]>('userFilter');
+export const repoFilter = config.requireObject<string[]>('repoFilter');
 export const dockerHost = config.require('dockerHost');
+export const runnerRepos = config.requireObject<string[]>('runnerRepos');
+export const seedUser = config.require('seedUser');
