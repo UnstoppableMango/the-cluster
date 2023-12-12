9d489c93243674b5e388967505c69f5942e3039a
Erik Rasmussen <ecr.referee@hotmail.com>
2023-12-12 09:45:53 -0600

    Cert manager CSI driver

--- apps/cert-manager/config.ts
+++ apps/cert-manager/config.ts
@@ -1,8 +1,14 @@
-import { Config, getStack, output } from '@pulumi/pulumi';
+import { Config, getStack } from '@pulumi/pulumi';
 import * as cf from '@pulumi/cloudflare';
+
+export interface Versions {
+  certManagerCsi: string;
+  csiNodeDriverRegistrar: string;
+}
 
 const config = new Config();
 export const suffix = config.get('suffix');
 export const zone = cf.getZoneOutput({ name: config.require('zoneName') });
 export const permissionGroups = cf.getApiTokenPermissionGroupsOutput();
 export const cluster = getStack();
+export const versions = config.requireObject<Versions>('versions');
