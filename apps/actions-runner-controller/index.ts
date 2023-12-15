3c6b79a76c922e3765aca2ba2db54f5308380ac1
Erik Rasmussen <ecr.referee@hotmail.com>
2023-12-15 16:25:08 -0600

    I guess the arc chart is soft deprecated

--- apps/actions-runner-controller/index.ts
+++ apps/actions-runner-controller/index.ts
@@ -1,8 +1,8 @@
 import * as pulumi from '@pulumi/pulumi';
 import * as random from '@pulumi/random';
 import * as k8s from '@pulumi/kubernetes';
-import { ingresses, provider } from '@unmango/thecluster/cluster/from-stack';
-import { github, hosts, privateKey, versions } from './config';
+import { provider } from '@unmango/thecluster/cluster/from-stack';
+import { github, privateKey, versions } from './config';
 
 const ns = new k8s.core.v1.Namespace('actions-runner-system', {
   metadata: { name: 'actions-runner-system' },
@@ -24,158 +24,10 @@ const webhookSecretToken = new random.RandomId('webhook-secret', {
   byteLength: 16,
 }).hex;
 
-const webhookSecret = new k8s.core.v1.Secret('webhook-server', {
-  metadata: {
-    name: 'webhook-server',
-    namespace: ns.metadata.name,
-  },
-  stringData: {
-    github_webhook_secret_token: webhookSecretToken,
-  },
-}, { provider });
-
-const actionsMetricsSecret = new k8s.core.v1.Secret('actions-metrics', {
-  metadata: {
-    name: 'actions-metrics',
-    namespace: ns.metadata.name,
-  },
-  stringData: {
-    github_webhook_secret_token: webhookSecretToken,
-  },
-}, { provider });
-
 const chart = new k8s.helm.v3.Chart('actions-runner-controller', {
   path: './',
   namespace: ns.metadata.name,
   values: {
-    // https://github.com/actions/actions-runner-controller/blob/master/charts/actions-runner-controller/README.md#values
-    'actions-runner-controller': {
-      replicaCount: 2,
-      webhookPort: 9443,
-      syncPeriod: '1m',
-      // logLevel: '',
-      logFormat: 'text', // `text` or `json`
-      authSecret: {
-        name: authSecret.metadata.name,
-      },
-      image: {
-        repository: 'summerwind/actions-runner-controller',
-        tag: versions.actionsRunnerController,
-        actionsRunnerRepositoryAndTag: `summerwind/actions-runner:${versions.actionsRunner}`,
-        dindSidecarRepositoryAndTag: `docker:${versions.dind}`,
-      },
-      metrics: {
-        port: 8443,
-        proxy: {
-          image: {
-            repository: 'quay.io/brancz/kube-rbac-proxy',
-            tag: versions.rbacProxy,
-          },
-        },
-      },
-      service: {
-        type: 'ClusterIP',
-        port: 443,
-      },
-      resources: {
-        limits: {
-          cpu: '100m',
-          memory: '128Mi',
-        },
-        requests: {
-          cpu: '100m',
-          memory: '128Mi',
-        },
-      },
-      certManagerEnabled: true,
-      runner: {
-        statusUpdateHook: {
-          enabled: true,
-        },
-      },
-      githubWebhookServer: {
-        enabled: true,
-        // logLevel: '',
-        logFormat: 'text', // `text` or `json`
-        replicaCount: 2,
-        useRunnerGroupsVisibility: true,
-        secret: {
-          enabled: true,
-          name: webhookSecret.metadata.name,
-        },
-        service: {
-          type: 'ClusterIP',
-        },
-        ingress: {
-          enabled: true,
-          annotations: {
-            'cloudflare-tunnel-ingress-controller.strrl.dev/backend-protocol': 'http',
-            'pulumi.com/skipAwait': 'true',
-          },
-          hosts: [{
-            host: hosts.webhook.external,
-            paths: [{
-              path: '/',
-              pathType: 'Prefix',
-            }],
-          }],
-          ingressClassName: ingresses.cloudflare,
-        },
-        resources: {
-          limits: {
-            cpu: '100m',
-            memory: '128Mi',
-          },
-          requests: {
-            cpu: '100m',
-            memory: '128Mi',
-          },
-        },
-      },
-      actionsMetricsServer: {
-        enabled: true,
-        port: 8443,
-        secret: {
-          enabled: true,
-          name: actionsMetricsSecret.metadata.name,
-        },
-        service: {
-          type: 'ClusterIP',
-        },
-        ingress: {
-          enabled: true,
-          annotations: {
-            'cloudflare-tunnel-ingress-controller.strrl.dev/backend-protocol': 'http',
-            'pulumi.com/skipAwait': 'true',
-          },
-          hosts: [{
-            host: hosts.metrics.external,
-            pathType: 'Prefix',
-            paths: [{
-              path: '/',
-              pathType: 'Prefix',
-            }],
-          }],
-          ingressClassName: ingresses.cloudflare,
-        },
-        resources: {
-          limits: {
-            cpu: '100m',
-            memory: '128Mi',
-          },
-          requests: {
-            cpu: '100m',
-            memory: '128Mi',
-          },
-        },
-        proxy: {
-          image: {
-            repository: 'quay.io/brancz/kube-rbac-proxy',
-            tag: versions.rbacProxy,
-          },
-        },
-      },
-    },
     // https://github.com/actions/actions-runner-controller/blob/master/charts/gha-runner-scale-set-controller/values.yaml
     'gha-runner-scale-set-controller': {
       replicaCount: 2,
@@ -216,7 +68,9 @@ const chart = new k8s.helm.v3.Chart('actions-runner-controller', {
 export const namespace = ns.metadata.name;
 export const serviceAccount = chart.getResource(
   'v1/ServiceAccount',
-  'actions-runner-system/actions-runner-controller')
+  'actions-runner-system/actions-runner-controller-gha-rs-controller')
   .metadata.name;
 
 export const webhookToken = pulumi.secret(webhookSecretToken);
+const authSecretName = authSecret.metadata.name;
+export { authSecretName as authSecret };
--- lib/nodejs/apps/actions-runner-controller.ts
+++ lib/nodejs/apps/actions-runner-controller.ts
@@ -1,6 +1,7 @@
 import { Refs } from '../internal';
 
 export class ActionsRunnerController {
+  public authSecret = this._refs.actionsRunnerController.requireOutput('authSecret');
   public namespace = this._refs.actionsRunnerController.requireOutput('namespace');
   public serviceAccount = this._refs.actionsRunnerController.requireOutput('serviceAccount');
   constructor(private _refs: Refs) {}
