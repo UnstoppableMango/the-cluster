8b7a829d6809c0b593b88495bbca12258dde513e
Erik Rasmussen <ecr.referee@hotmail.com>
2023-12-26 23:50:05 -0600

    Updating the pgadmin db

--- dbs/pgadmin/index.ts
+++ dbs/pgadmin/index.ts
@@ -1,30 +1,43 @@
-import * as pulumi from '@pulumi/pulumi';
-import { Database, Role, Schema } from '@pulumi/postgresql';
+import { Database, Grant, Role } from '@pulumi/postgresql';
 import { apps } from '@unmango/thecluster/cluster/from-stack';
+import { allDbPermissions } from '@unmango/thecluster/dbs/postgres';
 
 const { provider } = apps.postgresqlLa;
 
-const owner = new Role('owner', {
+const ownerGroup = new Role('owner', {
   name: 'pgadmin_owner',
 }, { provider });
 
-const database = new Database('pgadmin', {
+const db = new Database('pgadmin', {
   name: 'pgadmin',
-  owner: owner.name,
+  owner: ownerGroup.name,
 }, { provider })
 
-const pgadmin = new Role('pgadmin', {
+const owner = new Role('pgadmin', {
   name: 'pgadmin',
   login: true,
-  roles: [owner.name],
+  roles: [ownerGroup.name],
 }, { provider });
 
-// const grant = new pg.Grant('schema', {
-//   database,
-//   role: pgadmin.name,
-//   objectType: 'schema',
-//   schema: schema.name,
-//   privileges: ['CONNECT'],
-// });
+const grant = new Grant('all', {
+  objectType: 'database',
+  database: db.name,
+  privileges: allDbPermissions,
+  role: ownerGroup.name,
+  columns: [],
+  objects: [],
+}, { provider });
 
-export const ownerRole = owner.name;
+export const ip = apps.postgresqlLa.ip;
+export const hostname = apps.postgresqlLa.hosts.internal;
+export const clusterHostname = apps.postgresqlLa.clusterHostname;
+export const port = apps.postgresqlLa.port;
+export const database = db.name;
+
+const ownerGroupOutput = ownerGroup.name;
+const ownerOutput = owner.name;
+
+export {
+  ownerGroupOutput as ownerGroup,
+  ownerOutput as owner,
+};
