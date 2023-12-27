5af798b2d021e7b5f8186287f1f44f63202201ea
Erik Rasmussen <ecr.referee@hotmail.com>
2023-12-27 13:48:17 -0600

    Image tools + pulumi operator CRDs (#1932)
    * WIP updating the operator
    
    * Use gh workspace for safe dir instead of pwd
    
    * More runner image tools
    
    * Safe runner

--- lib/nodejs/internal/shared.ts
+++ lib/nodejs/internal/shared.ts
@@ -6,6 +6,7 @@ export interface Namespaces {
   postgres: Output<string>;
   keycloak: Output<string>;
   pgadmin: Output<string>;
+  pulumiOperator: Output<string>;
 }
 
 export class Shared {
@@ -14,14 +15,6 @@ export class Shared {
 
   public get ref(): StackReference {
     return this._ref.value;
-  }
-
-  public get mediaNamespace(): Output<string> {
-    return this.ref.requireOutput('mediaNamespace') as Output<string>;
-  }
-
-  public get postgresNamespace(): Output<string> {
-    return this.ref.requireOutput('postgresNamespace') as Output<string>;
   }
 
   public get namespaces(): Namespaces {
