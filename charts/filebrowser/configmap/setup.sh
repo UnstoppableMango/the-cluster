29f12f32e19f0432cc30c86d59aab8728e15fab9
Erik Rasmussen <ecr.referee@hotmail.com>
2023-12-27 20:29:15 -0600

    Chart fixes and updates

--- charts/filebrowser/configmap/setup.sh
+++ charts/filebrowser/configmap/setup.sh
@@ -4,7 +4,6 @@ set -eum
 [ -z "${DB_PATH+x}" ] && echo "DB_PATH not set" && exit 1;
 # [ -z "${PUID+x}" ] && echo "PUID not set" && exit 1;
 # [ -z "${PGID+x}" ] && echo "PGID not set" && exit 1;
-# [ ! -f "$DB_PATH" ] && filebrowser config init -d "$DB_PATH"
 # chown -R "$PUID:$PGID" "$(dirname "$DB_PATH")"
 
 if [ ! -f "$DB_PATH" ]; then
@@ -15,3 +14,10 @@ else
     echo "DB already exists at $DB_PATH"
     ls "$(dirname "$DB_PATH")"
 fi
+
+[ -n "${FILEBROWSER_USER_HEADER+x}" ] && {
+    echo "Configuring auth header to be $FILEBROWSER_USER_HEADER"
+    filebrowser config set -d "$DB_PATH" --auth.method="proxy" --auth.header "$FILEBROWSER_USER_HEADER"
+}
+
+filebrowser config -d "$DB_PATH" cat
