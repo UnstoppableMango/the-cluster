31ca3401a30114925136625a9eb4304a7a92fee2
Erik Rasmussen <ecr.referee@hotmail.com>
2023-12-27 22:54:41 -0600

    Chart fixes and enhancements

--- charts/filebrowser/configmap/setup.sh
+++ charts/filebrowser/configmap/setup.sh
@@ -2,22 +2,52 @@
 set -eum
 
 [ -z "${DB_PATH+x}" ] && echo "DB_PATH not set" && exit 1;
-# [ -z "${PUID+x}" ] && echo "PUID not set" && exit 1;
-# [ -z "${PGID+x}" ] && echo "PGID not set" && exit 1;
-# chown -R "$PUID:$PGID" "$(dirname "$DB_PATH")"
+[ -z "${CONFIG_PATH+x}" ] && echo "CONFIG_PATH not set" && exit 1;
+[ -z "${PUID+x}" ] && echo "PUID not set" && exit 1;
+[ -z "${PGID+x}" ] && echo "PGID not set" && exit 1;
 
-if [ ! -f "$DB_PATH" ]; then
-    echo "Initializing filebrowser db at $DB_PATH"
-    filebrowser config init -d "$DB_PATH"
-    echo "Initialized db"
-else
+DB_DIR="$(dirname "$DB_PATH")"
+echo "chown -R $PUID:$PGID $DB_DIR"
+chown -R "$PUID:$PGID" "$DB_DIR"
+
+echo "chown -R $PUID:$PGID /srv"
+chown -R "$PUID:$PGID" /srv
+
+if [ -f "$DB_PATH" ]; then
     echo "DB already exists at $DB_PATH"
-    ls "$(dirname "$DB_PATH")"
+    echo "\$> ls $DB_DIR"
+    ls "$DB_DIR"
+else
+    echo "Initializing filebrowser db at $DB_PATH"
+    filebrowser config init --config "$CONFIG_PATH" --database "$DB_PATH"
+    echo "✅ Successfully initialized db"
 fi
 
-[ -n "${FILEBROWSER_USER_HEADER+x}" ] && {
-    echo "Configuring auth header to be $FILEBROWSER_USER_HEADER"
-    filebrowser config set -d "$DB_PATH" --auth.method="proxy" --auth.header "$FILEBROWSER_USER_HEADER"
-}
+echo "
+filebrowser config set
+    --config $CONFIG_PATH
+    --database $DB_PATH
+    --auth.method $FILEBROWSER_AUTH_METHOD
+    --auth.header $FILEBROWSER_AUTH_HEADER
+    --branding.color $FILEBROWSER_BRANDING_THEME
+    --branding.name $FILEBROWSER_BRANDING_NAME
+    --branding.disableUsedPercentage $FILEBROWSER_DISABLE_USED_PERCENTAGE
+    --viewMode $FILEBROWSER_VIEW_MODE
+"
 
-filebrowser config -d "$DB_PATH" cat
+args=()
+if [ -n "${FILEBROWSER_DISABLE_USED_PERCENTAGE+x}" ]; then
+    args+=("--branding.disableUsedPercentage")
+fi
+
+# https://filebrowser.org/cli/filebrowser-config-set
+filebrowser config set \
+    --config "$CONFIG_PATH" \
+    --database "$DB_PATH" \
+    --auth.method "$FILEBROWSER_AUTH_METHOD" \
+    --auth.header "$FILEBROWSER_AUTH_HEADER" \
+    --branding.color "$FILEBROWSER_BRANDING_THEME" \
+    --branding.name "$FILEBROWSER_BRANDING_NAME" \
+    --viewMode "$FILEBROWSER_VIEW_MODE" \
+    "${args[@]}"
+echo "✅ Successfully applied configuration"
