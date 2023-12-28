146e6b0fabb38f4667b67b324855214c63eae34a
Erik Rasmussen <ecr.referee@hotmail.com>
2023-12-28 00:57:29 -0600

    Working filebrowser

--- apps/filebrowser/index.ts
+++ apps/filebrowser/index.ts
@@ -1,226 +1,152 @@
 import * as fs from 'node:fs/promises';
-import * as pulumi from '@pulumi/pulumi';
-import * as k8s from '@pulumi/kubernetes';
-import { clusterIssuers, ingresses, provider, shared, storageClasses } from '@unmango/thecluster/cluster/from-stack';
-import { jsonStringify } from '@unmango/thecluster';
-import { hosts, versions } from './config';
+import { interpolate } from '@pulumi/pulumi';
+import { ConfigMap, Namespace, Pod, PodArgs } from '@pulumi/kubernetes/core/v1';
+import { Chart } from '@pulumi/kubernetes/helm/v3';
+import { clusterIssuers, ingresses, provider, realms, shared, storageClasses } from '@unmango/thecluster/cluster/from-stack';
+import { client, readersGroup } from './oauth';
+import { hosts, mediaVolumes, releaseName, resources, servicePort, versions } from './config';
+import { core } from '@pulumi/kubernetes/types/input';
 
-const ns = k8s.core.v1.Namespace.get('media', 'media', { provider });
+type Volume = core.v1.Volume;
+type VolumeMount = core.v1.VolumeMount;
 
-const servicePort = 8080;
-const containerPort = 8080;
+const ns = Namespace.get(
+  'media',
+  shared.namespaces.media,
+  { provider },
+);
 
-const config = new k8s.core.v1.ConfigMap('config', {
+const assets = new ConfigMap('assets', {
   metadata: {
-    name: 'config',
+    name: 'assets',
     namespace: ns.metadata.name,
   },
-  data: {
-    'settings.json': jsonStringify({
-      port: containerPort,
-      baseUrl: '',
-      address: '',
-      log: 'stdout',
-      database: '/database/filebrowser.db',
-      root: '/srv',
-    }),
-    'setup.sh': fs.readFile('setup.sh', { encoding: 'utf-8' }),
+  binaryData: {
+    'logo.svg': fs.readFile('assets/logo.svg', 'base64'),
+    // 'favicon.ico': fs.readFile('assets/favicon.ico', 'base64'),
   },
 }, { provider });
 
-const chart = new k8s.helm.v3.Chart('filebrowser', {
+const chart = new Chart(releaseName, {
   path: '../../charts/filebrowser',
   namespace: ns.metadata.name,
   values: {
+    global: {
+      uid: 1001,
+      gid: 1001,
+    },
+    auth: {
+      method: 'noauth',
+      header: 'x-auth-request-preferred-username', // Might not be using this/getting set right
+    },
+    branding: {
+      name: 'THECLUSTER',
+      files: '/assets',
+    },
+    resources,
+    image: { tag: versions.filebrowser },
+    init: {
+      image: { tag: versions.filebrowser },
+      resources,
+    },
     persistence: {
       storageClassName: storageClasses.rbd,
     },
+    extraVolumes: mediaVolumes.map<Volume>(v => ({
+      name: v,
+      persistentVolumeClaim: {
+        claimName: v,
+      },
+    })).concat([
+      {
+        name: 'media',
+        persistentVolumeClaim: {
+          claimName: 'media',
+        },
+      },
+      {
+        name: 'assets',
+        configMap: {
+          name: assets.metadata.name,
+        },
+      }
+    ]),
+    extraVolumeMounts: mediaVolumes.map<VolumeMount>(v => ({
+      name: v,
+      mountPath: `/srv/${v}`,
+    })).concat([
+      {
+        name: 'media',
+        mountPath: '/srv',
+      },
+      {
+        name: 'assets',
+        mountPath: '/assets/branding/img/logo.svg',
+        subPath: 'logo.svg',
+        readOnly: true,
+      },
+      // {
+      //   name: 'assets',
+      //   mountPath: '/assets/branding/img/icons/favicon.ico',
+      //   subPath: 'favicon.ico',
+      //   readOnly: true,
+      // },
+    ]),
     ingress: {
       enabled: true,
+      ingressClassName: ingresses.internal,
+      annotations: {
+        'cert-manager.io/cluster-issuer': clusterIssuers.stage,
+        'external-dns.alpha.kubernetes.io/hostname': [
+          hosts.internal,
+          ...hosts.aliases.internal,
+        ].join(','),
+      },
+      host: hosts.internal,
+      tls: {
+        enabled: true,
+      },
     },
     'oauth2-proxy': {
       enabled: true,
+      resources,
+      config: {
+        clientID: client.clientId,
+        clientSecret: client.clientSecret,
+      },
+      extraEnv: [
+        { name: 'OAUTH2_PROXY_PROVIDER', value: 'keycloak-oidc' },
+        { name: 'OAUTH2_PROXY_UPSTREAMS', value: `http://${releaseName}:${servicePort}` },
+        { name: 'OAUTH2_PROXY_HTTP_ADDRESS', value: 'http://0.0.0.0:4180' },
+        { name: 'OAUTH2_PROXY_REDIRECT_URL', value: interpolate`https://${hosts.external}/oauth2/callback` },
+        { name: 'OAUTH2_PROXY_OIDC_ISSUER_URL', value: realms.external.issuerUrl },
+        { name: 'OAUTH2_PROXY_CODE_CHALLENGE_METHOD', value: 'S256' },
+        { name: 'OAUTH2_PROXY_ERRORS_TO_INFO_LOG', value: 'true' },
+        { name: 'OAUTH2_PROXY_PASS_ACCESS_TOKEN', value: 'true' },
+        { name: 'OAUTH2_PROXY_COOKIE_SECURE', value: 'true' },
+        { name: 'OAUTH2_PROXY_REVERSE_PROXY', value: 'true' },
+        { name: 'OAUTH2_PROXY_PASS_USER_HEADERS', value: 'true' }, // Not passing them for some reaons...
+        { name: 'OAUTH2_PROXY_EMAIL_DOMAINS', value: '*' },
+        { name: 'OAUTH2_PROXY_SKIP_PROVIDER_BUTTON', value: 'true' },
+        { name: 'OAUTH2_PROXY_SET_XAUTHREQUEST', value: 'true' },
+        { name: 'OAUTH2_PROXY_ALLOWED_GROUPS', value: interpolate`/${readersGroup.name},/WebAppReaders` },
+        { name: 'OAUTH2_PROXY_OIDC_GROUPS_CLAIM', value: realms.groupsScopeName },
+      ],
+      service: {
+        type: 'ClusterIP',
+      },
+      ingress: {
+        enabled: true,
+        className: ingresses.cloudflare,
+        pathType: 'Prefix',
+        hosts: [hosts.external],
+        annotations: {
+          'cloudflare-tunnel-ingress-controller.strrl.dev/backend-protocol': 'http',
+          'pulumi.com/skipAwait': 'true',
+        },
+      },
     },
   },
 }, { provider });
 
-// const service = new k8s.core.v1.Service('filebrowser', {
-//   metadata: {
-//     name: 'filebrowser',
-//     namespace: ns.metadata.name,
-//   },
-//   spec: {
-//     type: 'ClusterIP',
-//     selector: {
-//       app: 'filebrowser',
-//     },
-//     ports: [{
-//       name: 'http',
-//       port: servicePort,
-//     }],
-//   },
-// }, { provider });
-
-// const app = new k8s.apps.v1.StatefulSet('filebrowser', {
-//   metadata: {
-//     name: 'filebrowser',
-//     namespace: ns.metadata.name,
-//   },
-//   spec: {
-//     selector: {
-//       matchLabels: {
-//         app: 'filebrowser',
-//       },
-//     },
-//     // Chicken and egg with the service
-//     serviceName: 'filebrowser',
-//     template: {
-//       metadata: {
-//         labels: {
-//           app: 'filebrowser',
-//         },
-//       },
-//       spec: {
-//         // securityContext: {
-//         //   // runAsNonRoot: true,
-//         //   runAsUser: 1001,
-//         //   runAsGroup: 1001,
-//         // },
-//         initContainers: [{
-//           name: 'setup',
-//           image: `filebrowser/filebrowser:${versions.filebrowser}`,
-//           command: ['bash', '/setup.sh'],
-//           env: [
-//             // { name: 'PUID', value: '1001' },
-//             // { name: 'PGID', value: '1001' },
-//             { name: 'DB_PATH', value: '/db/filebrowser.db' },
-//           ],
-//           volumeMounts: [{
-//             name: 'database',
-//             mountPath: '/db',
-//           }, {
-//             name: 'config',
-//             mountPath: '/setup.sh',
-//             subPath: 'setup.sh',
-//           }],
-//         }],
-//         containers: [{
-//           name: 'filebrowser',
-//           image: `filebrowser/filebrowser:${versions.filebrowser}`,
-//           ports: [{ containerPort, name: 'http' }],
-//           // securityContext: {
-//           //   allowPrivilegeEscalation: true,
-//           //   privileged: false,
-//           //   // runAsNonRoot: true,
-//           //   runAsUser: 1001,
-//           //   runAsGroup: 1001,
-//           //   capabilities: { drop: ['ALL'] },
-//           // },
-//           // env: [
-//           //   { name: 'PUID', value: '1001' },
-//           //   { name: 'PGID', value: '1001' },
-//           // ],
-//           volumeMounts: [{
-//             name: 'database',
-//             mountPath: '/database',
-//           }, {
-//             name: 'config',
-//             mountPath: '/config/settings.json',
-//             subPath: 'settings.json',
-//           }, {
-//             name: 'music',
-//             mountPath: '/srv/music',
-//           }, {
-//             name: 'movies',
-//             mountPath: '/srv/movies',
-//           }, {
-//             name: 'tv',
-//             mountPath: '/srv/tv',
-//           }, {
-//             name: 'anime',
-//             mountPath: '/srv/anime',
-//           }],
-//           readinessProbe: {
-//             httpGet: {
-//               port: containerPort,
-//             },
-            
-//           },
-//           livenessProbe: {
-//             httpGet: {
-//               port: containerPort,
-//             },
-//           },
-//           resources: {
-//             requests: {
-//               cpu: '10m',
-//               memory: '64Mi',
-//             },
-//             limits: {
-//               cpu: '10m',
-//               memory: '64Mi',
-//             },
-//           },
-//         }],
-//         volumes: [
-//           {
-//             name: 'config',
-//             configMap: {
-//               name: config.metadata.name,
-//             },
-//           },
-//           ...['music', 'movies', 'tv', 'anime'].map(name => ({
-//             name,
-//             persistenVolumeClaim: {
-//               claimName: name,
-//             },
-//           })),
-//         ],
-//       },
-//     },
-//     volumeClaimTemplates: [{
-//       metadata: { name: 'database' },
-//       spec: {
-//         storageClassName: storageClasses.rbd,
-//         accessModes: ['ReadWriteOnce'],
-//         resources: {
-//           requests: {
-//             storage: '1Gi',
-//           },
-//         },
-//       },
-//     }],
-//   },
-// }, { provider });
-
-// const ingress = new k8s.networking.v1.Ingress('filebrowser', {
-//   metadata: {
-//     name: 'filebrowser',
-//     namespace: ns.metadata.name,
-//     annotations: {
-//       'cert-manager.io/cluster-issuer': clusterIssuers.stage,
-//     },
-//   },
-//   spec: {
-//     ingressClassName: ingresses.internal,
-//     rules: [{
-//       host: hosts.internal,
-//       http: {
-//         paths: [{
-//           path: '/',
-//           pathType: 'ImplementationSpecific',
-//           backend: {
-//             service: {
-//               name: service.metadata.name,
-//               port: { name: 'http' },
-//             },
-//           },
-//         }],
-//       },
-//     }],
-//     tls: [{
-//       hosts: [hosts.internal],
-//       secretName: 'filebrowser-internal-tls',
-//     }],
-//   },
-// }, { provider });
+export { hosts, versions }
