e4846b424a4a6c92f830c66327ac56c5e4a3cc93
Erik Rasmussen <ecr.referee@hotmail.com>
2023-12-30 00:38:12 -0600

    Hack together a job to clear PVs (#1949)
    * Hack together a job to clear pvs
    
    * Some heft

--- infra/cron/index.ts
+++ infra/cron/index.ts
@@ -1,8 +1,40 @@
 import { CronJob } from '@pulumi/kubernetes/batch/v1';
-import { Namespace } from '@pulumi/kubernetes/core/v1';
+import { Namespace, ServiceAccount } from '@pulumi/kubernetes/core/v1';
+import { ClusterRole, ClusterRoleBinding } from '@pulumi/kubernetes/rbac/v1';
 import { provider } from '@unmango/thecluster/cluster/from-stack';
 
 const actionsNs = Namespace.get('actions-runner-system', 'actions-runner-system', { provider });
+
+const pvPatcherRole = new ClusterRole('pv-patcher', {
+  metadata: { name: 'pv-patcher' },
+  rules: [{
+    apiGroups: [''],
+    resources: ['persistentvolumes'],
+    verbs: ['get', 'list', 'patch'],
+  }],
+}, { provider });
+
+const pvPatcherAccount = new ServiceAccount('pv-patcher', {
+  metadata: {
+    name: 'pv-patcher',
+    namespace: actionsNs.metadata.name,
+  },
+  automountServiceAccountToken: true,
+}, { provider });
+
+const pvPatcherBinding = new ClusterRoleBinding('pv-patcher', {
+  metadata: { name: 'pv-patcher' },
+  roleRef: {
+    apiGroup: 'rbac.authorization.k8s.io',
+    kind: pvPatcherRole.kind,
+    name: pvPatcherRole.metadata.name,
+  },
+  subjects: [{
+    kind: pvPatcherAccount.kind,
+    name: pvPatcherAccount.metadata.name,
+    namespace: actionsNs.metadata.name,
+  }],
+}, { provider });
 
 const cleanActionsPvs = new CronJob('clean-actions-pvs', {
   metadata: {
@@ -10,26 +42,21 @@ const cleanActionsPvs = new CronJob('clean-actions-pvs', {
     namespace: actionsNs.metadata.name,
   },
   spec: {
-    schedule: '*/5 * * * *',
+    schedule: '*/10 * * * *',
+    successfulJobsHistoryLimit: 1,
+    failedJobsHistoryLimit: 2,
+    concurrencyPolicy: 'Forbid',
     jobTemplate: {
       spec: {
-        selector: {
-          matchLabels: {
-            'kubernetes.io/job': 'clean-actions-pvs',
-          },
-        },
         template: {
-          metadata: {
-            labels: {
-              'kubernetes.io/job': 'clean-actions-pvs',
-            },
-          },
           spec: {
-            containers: [],
-            volumes: [{
-              name: 'scripts',
-              configMap: {},
+            serviceAccountName: pvPatcherAccount.metadata.name,
+            containers: [{
+              name: 'work',
+              image: 'ghcr.io/unstoppablemango/clean-pvs-cron-job:main',
+              imagePullPolicy: 'Always',
             }],
+            restartPolicy: 'Never',
           },
         },
       },
