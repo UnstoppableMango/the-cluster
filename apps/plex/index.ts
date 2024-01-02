ac833c10ab2f446aaa721901980246408e757231
Erik Rasmussen <ecr.referee@hotmail.com>
2024-01-02 01:43:27 -0600

    GAH we got so close
    GAH we got so close
    
    - Added .helmignore file to ignore certain patterns when building packages
    - Added Chart.lock file with dependencies for plex chart
    - Added Chart.yaml file with metadata

---
+++ apps/plex/index.ts
@@ -0,0 +1,138 @@
+import * as path from 'node:path';
+import * as pulumi from '@pulumi/pulumi';
+import * as random from '@pulumi/random';
+import * as k8s from '@pulumi/kubernetes';
+import { ConfigMap, Namespace } from '@pulumi/kubernetes/core/v1';
+import { Certificate } from '@unstoppablemango/thecluster-crds/certmanager/v1';
+import { clusterIssuers, databases, ingresses, provider, shared, storageClasses } from '@unstoppablemango/thecluster/cluster/from-stack';
+import { claimToken, hosts, versions } from './config';
+import { required } from '@unstoppablemango/thecluster/util';
+
+const ns = new Namespace('plex', {
+  metadata: { name: 'plex' },
+}, { provider });
+
+// const cert = new Certificate('plex', {
+//   metadata: {
+//     name: 'plex',
+//     namespace: ns.metadata.name,
+//   },
+//   spec: {
+//     secretName: 'plex-tls',
+//     issuerRef: clusterIssuers.ref(x => x.plex),
+//     duration: '2160h0m0s', // 90d
+//     renewBefore: '360h0m0s', // 15d
+//     commonName: 'kc.thecluster.io',
+//     subject: {
+//       organizations: ['unmango'],
+//     },
+//     // TODO: Verify these will work
+//     privateKey: {
+//       algorithm: 'ECDSA',
+//       size: 256,
+//       rotationPolicy: 'Always',
+//     },
+//     usages: [
+//       'server auth',
+//       'client auth',
+//     ],
+//     dnsNames: [
+//       hosts.external,
+//       ...hosts.aliases.external,
+//       hosts.internal,
+//       ...hosts.aliases.internal,
+//     ],
+//   },
+// }, { provider });
+
+// const config = new ConfigMap('plex', {
+//   metadata: {
+//     name: 'plex',
+//     namespace: ns.metadata.name,
+//   },
+//   data: {
+//   },
+// }, { provider });
+
+// const secret = new k8s.core.v1.Secret('plex', {
+//   metadata: {
+//     name: 'plex',
+//     namespace: ns.metadata.name,
+//   },
+//   stringData: {
+//   },
+// }, { provider });
+
+const chart = new k8s.helm.v3.Chart('plex', {
+  path: './',
+  namespace: ns.metadata.name,
+  values: {
+    // https://github.com/ressu/kube-plex/blob/main/charts/kube-plex/values.yaml
+    'kube-plex': {
+      image: {
+        repository: 'plexinc/pms-docker',
+        tag: versions.plex,
+      },
+      kubePlex: {
+        image: {
+          repository: 'ghcr.io/resu/kube-plex',
+          tag: versions.kubePlex,
+        },
+        // resources: {},
+      },
+      claimToken,
+      timezone: 'America/Chicago',
+      extraEnv: {
+        puid: 1001,
+        pgid: 1001,
+        // NVIDIA_VISIBLE_DEVICES: 'all',
+        // NVIDIA_DRIVER_CAPABILITIES: 'video,compute,utility',
+      },
+      runtimeClassName: 'nvidia',
+      service: { type: 'ClusterIP', port: 32400 },
+      ingress: {
+        enabled: true,
+        hosts: [
+          'plex.lan.thecluster.io',
+          'media.lan.thecluster.io',
+        ],
+        annotations: {
+          'cert-manager.io/cluster-issuer': clusterIssuers.stage,
+        },
+        tls: [{
+          secretName: 'plex-tls',
+          hosts: [
+            'plex.lan.thecluster.io',
+            'media.lan.thecluster.io',
+          ],
+        }],
+      },
+      persistence: {
+        transcode: {
+          storageClass: storageClasses.cephfs,
+          subPath: 'plex',
+          size: '100Gi',
+          accessMode: 'ReadWriteMany',
+        },
+        data: {
+          storageClass: storageClasses.rbd,
+          subPath: 'plex',
+          size: '100Gi',
+          accessMode: 'ReadWriteOnce',
+        },
+        extraData: ['movies', 'tv', 'anime', 'music', 'movies4k', 'tv4k', 'photos'].map(x => ({
+          claimName: x,
+          name: x,
+        })),
+        config: {
+          claimName: 'plex',
+          subPath: 'plex',
+          accessMode: 'ReadWriteMany',
+        },
+      },
+      // resources: {},
+    },
+  }
+}, { provider });
+
+export { hosts };
