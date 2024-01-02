4763e2232c0ad5ada705962aec10ff03246cd677
Erik Rasmussen <ecr.referee@hotmail.com>
2024-01-01 20:05:45 -0600

    Still gotta do this though

--- lib/crds/gen/sdks.sh
+++ lib/crds/gen/sdks.sh
@@ -116,6 +116,8 @@ renamePulumi "$nodejsDir/index.ts"
 echo -e "\nUpdating nodejs lib name..."
 packageJson="$(cat "$nodejsDir/package.json")"
 echo "$packageJson" | jq '.name = "@unmango/thecluster-crds" | .version = "0.1.0"' >"$nodejsDir/package.json"
+packageJson="$(cat "$nodejsDir/package.json")"
+echo "$packageJson" | jq '.repository.type = "git" | .repository.url = "git+https://github.com/UnstoppableMango/the-cluster.git"' >"$nodejsDir/package.json"
 
 function patchDotnet() {
     echo -ne "\\r\033[2KFixing $1..."
