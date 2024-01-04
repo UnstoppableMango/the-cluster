d7a58086105508add83343b9752d78dfecf75b44
Erik Rasmussen <ecr.referee@hotmail.com>
2024-01-04 00:08:18 -0600

    feat: Add Velero installation for THECLUSTER
    This commit adds the necessary configuration and resources to install Velero in THECLUSTER. The following significant changes were made:
    
    - Added a new file `Pulumi.pinkdiamond.yaml` with configuration settings for Minio and Backblaze
    - Updated `Pulumi.yaml` to include versions of dependencies (bitnamiKubectl, velero, veleroCsi, veleroAws)
    - Updated `config.ts` to include a new interface `Backblaze` and added required properties for Backblaze credentials
    - Updated `index.ts` to create an S3 bucket for backups, configure secrets for Minio and Backblaze credentials, and deploy Velero using Helm
    - Updated `package-lock.json` with new dependencies
    
    These changes enable the installation of Velero in THECLUSTER with support for both Minio and Backblaze as backup storage locations.

--- apps/velero/config.ts
+++ apps/velero/config.ts
@@ -1,32 +1,25 @@
 import { Config, getStack } from '@pulumi/pulumi';
 
-export interface Auth {
-  adminUser: string;
-}
-
-export interface Hosts {
-  external: string;
-  internal: string;
-  aliases: {
-    external: string[];
-    internal: string[];
-  };
-}
-
-export interface Postgres {
-  username: string;
+export interface Backblaze {
+  accessKey: string;
+  keyName: string;
+  bucketName: string;
+  applicationKey: string;
 }
 
 export interface Versions {
-  keycloak: string;
+  bitnamiKubectl: string;
+  velero: string;
+  veleroCsi: string;
+  veleroAws: string;
 }
 
 const config = new Config();
 export const cluster = getStack();
 
-export const auth = config.requireObject<Auth>('auth');
-export const production = config.requireBoolean('production');
-export const myEmail = config.requireSecret('myEmail');
-export const myGoogleId = config.requireSecret('myGoogleId');
-export const hosts = config.requireObject<Hosts>('hosts');
 export const versions = config.requireObject<Versions>('versions');
+export const backblaze = config.requireObject<Backblaze>('backblaze');
+
+const minioConfig = new Config('minio');
+export const minioAccessKey = minioConfig.require('minioAccessKey');
+export const minioSecretKey = minioConfig.require('minioSecretKey');
--- apps/velero/index.ts
+++ apps/velero/index.ts
@@ -1,11 +1,17 @@
+import { interpolate } from '@pulumi/pulumi';
 import * as k8s from '@pulumi/kubernetes';
-import { ConfigMap, Namespace } from '@pulumi/kubernetes/core/v1';
+import { S3Bucket } from '@pulumi/minio';
+import { ConfigMap, Namespace, Secret } from '@pulumi/kubernetes/core/v1';
+import { Release } from '@pulumi/kubernetes/helm/v3';
 import { clusterIssuers, provider } from '@unstoppablemango/thecluster/cluster/from-stack';
-import { hosts, versions } from './config';
+import { backblaze, minioAccessKey, minioSecretKey, versions } from './config';
+import { join } from '@unstoppablemango/thecluster';
 
-const ns = new Namespace('velero', {
-  metadata: { name: 'velero' },
-}, { provider });
+const backupBucket = new S3Bucket('backup', { bucket: 'velero-backup' });
+const minioCredentialsKey = 'minio-credentials';
+const backblazeCredentialsKey = 'backblaze-credentials';
+
+const ns = Namespace.get('velero', 'velero', { provider });
 
 const config = new ConfigMap('velero', {
   metadata: {
@@ -16,41 +22,171 @@ const config = new ConfigMap('velero', {
   },
 }, { provider });
 
-const secret = new k8s.core.v1.Secret('velero', {
+const secret = new Secret('velero', {
   metadata: {
     name: 'velero',
     namespace: ns.metadata.name,
   },
   stringData: {
+    [minioCredentialsKey]: join([
+      '[default]',
+      `aws_access_key_id=${minioAccessKey}`,
+      `aws_secret_access_key=${minioSecretKey}\n`,
+    ], '\n'),
+    [backblazeCredentialsKey]: join([
+      '[default]',
+      `aws_access_key_id=${backblaze.accessKey}`,
+      `aws_secret_access_key=${backblaze.applicationKey}\n`,
+    ], '\n'),
   },
 }, { provider });
 
-const chart = new k8s.helm.v3.Chart('velero', {
-  path: './',
+const chart = new Release('velero', {
+  chart: './',
   namespace: ns.metadata.name,
-  skipCRDRendering: false,
+  createNamespace: false,
+  skipCrds: false,
+  atomic: true,
+  dependencyUpdate: true,
   values: {
     // https://github.com/vmware-tanzu/helm-charts/blob/main/charts/velero/values.yaml
     velero: {
       image: {
         repository: 'velero/velero',
-        tag: 'v1.12.2',
+        tag: versions.velero,
       },
-      initContainers: [{
-        name: 'velero-plugin-for-csi',
-        image: 'velero/velero-plugin-for-csi:v0.6.0',
-        volumeMounts: [{
-          mountPath: '/target',
-          name: 'pluginx',
-        }],
+      resources: {
+        requests: {
+          cpu: '500m',
+          memory: '128Mi',
+        },
+        limits: {
+          cpu: '1000m',
+          memory: '512Mi',
+        },
+      },
+      upgradeJobResources: {
+        requests: {
+          cpu: '50m',
+          memory: '128Mi',
+        },
+        limits: {
+          cpu: '100m',
+          memory: '256Mi',
+        },
+      },
+      initContainers: [
+        {
+          name: 'velero-plugin-for-csi',
+          image: `velero/velero-plugin-for-csi:${versions.veleroCsi}`,
+          volumeMounts: [{
+            mountPath: '/target',
+            name: 'plugins',
+          }],
+        },
+        {
+          name: 'velero-plugin-for-aws',
+          image: `velero/velero-plugin-for-aws:${versions.veleroAws}`,
+          volumeMounts: [{
+            mountPath: '/target',
+            name: 'plugins',
+          }],
+        },
+      ],
+      podSecurityContext: {
+        fsGroup: 1001,
+      },
+      containerSecurityContext: {
+        allowPrivilegeEscalation: false,
+        capabilities: { drop: ['ALL'] },
+        readOnlyRootFilesystem: true,
+      },
+      extraVolumes: [{
+        name: 'root-ca',
+        configMap: {
+          name: 'root-ca',
+        },
       }],
+      // The default bundle isn't included in here so the container fails to verify normal certs like backblaze
+      // extraVolumeMounts: [{
+      //   name: 'root-ca',
+      //   mountPath: '/etc/ssl/certs',
+      //   readOnly: true,
+      // }],
+      kubectl: {
+        image: {
+          repository: 'docker.io/bitnami/kubectl',
+          tag: versions.bitnamiKubectl,
+        },
+      },
       configuration: {
-        backupStorageLocation: [{
-          name: '',
-        }],
+        backupStorageLocation: [
+          {
+            name: 'minio',
+            provider: 'aws',
+            bucket: backupBucket.bucket,
+            // caCert: '', // TODO: How to get this in b64 in the values?
+            default: true,
+            // validationFrequency: '',
+            credential: {
+              name: secret.metadata.name,
+              key: minioCredentialsKey,
+            },
+            config: {
+              region: 'goodall-apt',
+              s3Url: 'http://thecluster-hl.minio.svc.cluster.local:9000',
+              publicUrl: 'https://s3.thecluster.io',
+            },
+          },
+          {
+            name: 'backblaze',
+            provider: 'aws',
+            bucket: backblaze.bucketName,
+            default: false,
+            validationFrequency: '1h',
+            credential: {
+              name: secret.metadata.name,
+              key: backblazeCredentialsKey,
+            },
+            config: {
+              region: 'us-west-002',
+              s3Url: 'https://s3.us-west-002.backblazeb2.com',
+              publicUrl: 'https://s3.us-west-002.backblazeb2.com',
+            },
+          },
+        ],
+        volumeSnapshotLocation: [],
+        namespace: ns.metadata.name,
+      },
+      snapshotsEnabled: false,
+      schedules: {
+        backblaze: {
+          disabled: false,
+          labels: {
+            cluster: 'pinkdiamond',
+          },
+          schedule: '0 4 * * *',
+          useOwnerReferencesInBackup: true,
+          template: {
+            ttl: '240h',
+            storageLocation: 'backblaze',
+            // includeNamespaces: [],
+          },
+        },
+        minio: {
+          disabled: true,
+          labels: {
+            cluster: 'pinkdiamond',
+          },
+          schedule: '0 * * * *',
+          useOwnerReferencesInBackup: true,
+          template: {
+            ttl: '240h',
+            storageLocation: 'backblaze',
+            // includeNamespaces: [],
+          },
+        },
       },
     },
   },
 }, { provider });
-
-export { hosts };
