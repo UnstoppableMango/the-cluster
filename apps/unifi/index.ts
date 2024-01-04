5e35fb5961a3ed7ffd2ec000d166f4e6aea9eb30
Erik Rasmussen <ecr.referee@hotmail.com>
2024-01-04 01:05:56 -0600

    Supposedly fix unifi

--- apps/unifi/index.ts
+++ apps/unifi/index.ts
@@ -1,6 +1,8 @@
 import * as k8s from '@pulumi/kubernetes';
 import { clusterIssuers, ingresses, provider } from '@unstoppablemango/thecluster/cluster/from-stack';
 
+const loadBalancerIP = '192.168..1.87';
+
 const ns = new k8s.core.v1.Namespace('unifi', {
   metadata: { name: 'unifi' },
 }, { provider });
@@ -13,7 +15,29 @@ const chart = new k8s.helm.v3.Chart('unifi', {
       image: {
         // tag: '',
       },
-    }
+      service: {
+        main: {
+          loadBalancerIP,
+          sharedKey: 'unifi-svc',
+        },
+        comm: {
+          loadBalancerIP,
+          sharedKey: 'unifi-svc',
+        },
+        stun: {
+          loadBalancerIP,
+          sharedKey: 'unifi-svc',
+        },
+        speedtest: {
+          loadBalancerIP,
+          sharedKey: 'unifi-svc',
+        },
+        guestportal: {
+          loadBalancerIP,
+          sharedKey: 'unifi-svc',
+        },
+      },
+    },
   },
 }, { provider });
 
@@ -25,6 +49,7 @@ const ingress = new k8s.networking.v1.Ingress('unifi', {
     annotations: {
       'pulumi.com/skipAwait': 'true',
       'cert-manager.io/cluster-issuer': clusterIssuers.staging,
+      'nginx.org/ssl-services': service.metadata.name,
     },
   },
   spec: {
