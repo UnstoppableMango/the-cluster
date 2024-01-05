d6b53f126d4100a39718a4a051ad3fd33192a667
Erik Rasmussen <ecr.referee@hotmail.com>
2024-01-04 19:54:48 -0600

    Moving things around

---
+++ tools/tctl/UnMango.TheCluster.CLI/Program.fs
@@ -0,0 +1,13 @@
+﻿open Argu
+open UnMango.TheCluster.CLI
+open UnMango.TheCluster.CLI.Commands
+
+[<EntryPoint>]
+let main args =
+    let reader = EnvironmentVariableConfigurationReader() :> IConfigurationReader
+    ArgumentParser.Create<Args>(programName = "tctl")
+    |> _.Parse(args, configurationReader = reader)
+    |> _.GetAllResults()
+    |> List.head
+    |> Root.run
+    |> _.Result
