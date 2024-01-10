483b8b88f8e1bab8c1241d8423ac8bede20a9152
Erik Rasmussen <ecr.referee@hotmail.com>
2024-01-10 16:12:38 +0000

    Some tweaks to make it run (#2072)
    * Some tweaks to make it run
    
    * Workflow stuff

--- tools/tctl/UnMango.TheCluster.CLI/Program.fs
+++ tools/tctl/UnMango.TheCluster.CLI/Program.fs
@@ -9,7 +9,7 @@ module New =
         match args with
         | Commands.AnyUnrecognized x -> Commands.handleUnrecognized x
         | Commands.ShouldShowUsage -> args.Parser.PrintUsage() |> Console.WriteLine |> (fun _ -> Task.FromResult(0))
-        | x -> x.Catch((fun () -> Project.create x), showUsage = x.IsUsageRequested)
+        | x -> x.Catch((fun () -> Project.create x), ErrorCode.PostProcess, showUsage = x.IsUsageRequested)
 
 [<EntryPoint>]
 let main args =
@@ -18,8 +18,7 @@ let main args =
     ArgumentParser.Create<Args.Root>(programName = "tctl")
     |> _.Parse(args, configurationReader = reader, raiseOnUsage = false, ignoreUnrecognized = true)
     |> function
-        | x when x.UnrecognizedCliParams.Length > 0 ->
-            Console.WriteLine("Unrecognized arguments: {0}", x.UnrecognizedCliParams)
-            |> (fun _ -> 1)
-        | x when x.Contains Args.New -> x.Catch(fun () -> x.GetResult Args.New |> New.run |> _.Result)
-        | x -> x.Parser.PrintUsage() |> Console.WriteLine |> (fun _ -> 0)
+        | Commands.AnyUnrecognized x -> Commands.handleUnrecognized x
+        | x when x.Contains Args.New -> x.Catch(fun () -> x.GetResult Args.New |> New.run)
+        | x -> x.Parser.PrintUsage() |> Console.WriteLine |> (fun _ -> Task.FromResult(0))
+    |> _.Result
