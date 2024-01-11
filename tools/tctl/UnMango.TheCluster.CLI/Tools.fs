fbb909a59c0e444e4d5fa47d8e3b0596b6ad378c
Erik Rasmussen <ecr.referee@hotmail.com>
2024-01-11 14:57:25 -0600

    Implement some tools

--- tools/tctl/UnMango.TheCluster.CLI/Tools.fs
+++ tools/tctl/UnMango.TheCluster.CLI/Tools.fs
@@ -12,3 +12,9 @@ module Npm =
     let command = Command("npm")
     let run = run command
     let install args = run ("install" :: args)
+    let ci args = run ("ci" :: args)
+
+module TalosCtl =
+    let command = Command("talosctl")
+    let run = run command
+    let health args = run ("health" :: args)
