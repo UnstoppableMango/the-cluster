e049e88f53935d1e404616bd5d1dc30ec62a75bc
Erik Rasmussen <ecr.referee@hotmail.com>
2024-01-10 22:47:06 -0600

    Add THEBUNDLE

--- infra/pki/index.ts
+++ infra/pki/index.ts
@@ -1,4 +1,6 @@
-import { output, secret } from '@pulumi/pulumi';
+import { all, output } from '@pulumi/pulumi';
+import { Bundle } from '@unstoppablemango/thecluster-crds/trust/v1alpha1';
+import { provider } from '@unstoppablemango/thecluster/cluster/from-stack';
 import { root, postgres, letsencrypt, selfSigned, keycloak } from './issuers';
 import { ns, trustNs } from './namespace';
 import { bundles, trustLabel } from './config';
@@ -6,6 +8,31 @@ import { bundles, trustLabel } from './config';
 export const namespace = ns.metadata.name;
 export const trustNamespace = trustNs.metadata.name;
 export { trustLabel, bundles };
+
+const bundle = new Bundle('thecluster-ca', {
+  metadata: { name: 'thecluster-ca' },
+  spec: {
+    sources: all([
+      output({ useDefaultCAs: true }),
+      root.bundle.spec.sources,
+      postgres.bundle.spec.sources,
+      keycloak.bundle.spec.sources,
+    ]).apply(x => x.flat()),
+    target: {
+      configMap: { key: bundles.key },
+      additionalFormats: {
+        jks: { key: bundles.jksKey },
+        pkcs12: { key: bundles.p12Key },
+      },
+      namespaceSelector: {
+        matchLabels: {
+          [trustLabel]: 'thebundle',
+        },
+      },
+    },
+  },
+}, { provider });
+
 
 export const clusterIssuers = {
   group: output('cert-manager.io'),
