8dc364883e947300b9cc6de825dd1ef3d7a82f00
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-01-16 02:24:43 +0000

    Add verdaccio (#2129)
    * Verdaccio F#
    
    * Switch back to ts

--- lib/dotnet/UnMango.TheCluster.FSharp/K8s.fs
+++ lib/dotnet/UnMango.TheCluster.FSharp/K8s.fs
@@ -1,5 +1,6 @@
 module UnMango.TheCluster.FSharp.K8s
 
+open Pulumi
 open Pulumi.FSharp
 open Pulumi.Kubernetes.Types.Inputs.Core.V1
 open Pulumi.Kubernetes.Types.Inputs.Meta.V1
@@ -16,13 +17,24 @@ module Core =
             let create name opts =
                 Kubernetes.Core.V1.Namespace.create name (NamespaceArgs(Metadata = (input >> metaName) name)) opts
 
+        module ConfigMap =
+            let create name ns data binaryData opts =
+                Kubernetes.Core.V1.ConfigMap.create
+                    name
+                    (ConfigMapArgs(Metadata = (Ops.input >> meta) name ns, Data = data, BinaryData = binaryData))
+                    opts
+
         module Secret =
             let create name ns data opts =
                 Kubernetes.Core.V1.Secret.create
                     name
-                    (SecretArgs(Metadata = meta (input name) ns, StringData = data))
+                    (SecretArgs(Metadata = (Ops.input >> meta) name ns, StringData = data))
                     opts
 
 let ns o n = Core.V1.Namespace.create n o
+
+let cmb o n s d b = Core.V1.ConfigMap.create n s d b o
+
+let cm o n s d = cmb o n s d (InputMap())
 
 let secret o n s d = Core.V1.Secret.create n s d o
