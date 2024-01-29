c8b1d78346a14d4fe33694fd59bfe5fa1c2209d1
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-01-28 18:23:04 -0600

    Fiddlin

--- clusters/rosequartz/spec/suite_test.go
+++ clusters/rosequartz/spec/suite_test.go
@@ -25,6 +25,17 @@ func TestHappy(t *testing.T) {
 
 		_, err = docker.NewContainer(ctx, "talos", &docker.ContainerArgs{
 			Image: image.RepoDigest,
+			Envs: pulumi.ToStringArray([]string{
+				"PLATFORM=container",
+				"TALOSSKU=2CPU-2048RAM",
+				"PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+			}),
+			Labels: &docker.ContainerLabelArray{
+				&docker.ContainerLabelArgs{
+					Label: pulumi.String("org.opencontainers.image.source"),
+					Value: pulumi.String("https://github.com/siderolabs/talos"),
+				},
+			},
 		})
 		if err != nil {
 			return err
@@ -38,7 +49,7 @@ func TestHappy(t *testing.T) {
 	stack, err := auto.UpsertStackInlineSource(ctx, "e2e", "rq-deps", deployFunc)
 	assert.NoError(t, err)
 
-	err = stack.Workspace().InstallPlugin(ctx, "docker", "v4.5.1")
+	err = stack.Workspace().InstallPlugin(ctx, "docker", "v4.5.1") // TODO: Put in a place renovate can find it
 	assert.NoError(t, err)
 
 	upStdout := optup.ProgressStreams(os.Stdout)
