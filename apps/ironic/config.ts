1c926d2a07ca8a90ab1f4b0efce7caba9a95c082
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-02-08 11:52:39 -0600

    WIP configuring ironic

--- apps/ironic/config.ts
+++ apps/ironic/config.ts
@@ -1,7 +1,13 @@
 import { Config } from '@pulumi/pulumi';
+import * as cluster from '@unstoppablemango/thecluster/cluster';
 
 export interface Versions {
 }
 
 const config = new Config();
 export const versions = config.getObject<Versions>('versions');
+
+export const clusterName = 'rosequartz';
+export const ref = cluster.ref(clusterName, getStack());
+export const provider = cluster.provider(ref, clusterName);
+export const cloudflare = config.requireObject<Cloudflare>('cloudflare');
