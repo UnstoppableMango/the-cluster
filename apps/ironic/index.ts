1c9962da629e43a4f26e18577571f3c77c3bc1c8
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-02-07 23:08:51 -0600

    WIP on the bits

--- apps/ironic/index.ts
+++ apps/ironic/index.ts
@@ -1,7 +1,22 @@
-import { Namespace } from '@pulumi/kubernetes/core/v1';
+import { Namespace, Secret } from '@pulumi/kubernetes/core/v1';
+import { kustomize } from '@pulumi/kubernetes';
 import { provider } from '@unstoppablemango/thecluster/cluster/from-stack';
-import { versions } from './config.ts';
 
 const ns = new Namespace('ironic', {
   metadata: { name: 'ironic' },
+}, { provider });
+
+const secret = new Secret('ironic', {
+  metadata: {
+    name: 'ironic',
+    namespace: ns.metadata.name,
+  },
+  stringData: {
+    IRONIC_HTPASSWD: '',
+    IRONIC_INSPECTOR_HTPASSWD: '',
+  },
+}, { provider });
+
+const app = new kustomize.Directory('ironic', {
+  directory: './',
 }, { provider });
