f89f250f5326bc3b6bddd7dd08c75c41a802139a
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-02-08 17:00:16 -0600

    Minor tweaks

--- apps/baremetal-operator/index.ts
+++ apps/baremetal-operator/index.ts
@@ -1,1 +1,10 @@
-import * as pulumi from "@pulumi/pulumi";
+import { ConfigMap, Namespace, Secret } from '@pulumi/kubernetes/core/v1';
+import { kustomize } from '@pulumi/kubernetes';
+import { provider } from '@unstoppablemango/thecluster/cluster/from-stack';
+
+const app = new kustomize.Directory('baremetal-operator', {
+  directory: './',
+}, {
+  provider,
+  dependsOn: [],
+});
