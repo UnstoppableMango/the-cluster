0e31cff14e60c36f24ce28bede98e6e0ad27da36
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-02-08 15:05:58 -0600

    Refactoring and get it working?

--- infra/cloudflare-tunnels/config.ts
+++ infra/cloudflare-tunnels/config.ts
@@ -9,14 +9,26 @@ export interface Cloudflare {
   zoneId: string;
 }
 
+export interface Versions {
+  cloudflared: string;
+}
+
 const config = new Config();
 export const clusterName = 'rosequartz';
 export const ref = cluster.ref(clusterName, getStack());
 export const provider = cluster.provider(ref, clusterName);
 export const cloudflare = config.requireObject<Cloudflare>('cloudflare');
+export const versions = config.requireObject<Versions>('versions');
 
 const caRef = new StackReference('ca', {
   name: `UnstoppableMango/thecluster-ca/${config.require('caStack')}`,
 });
 
 export const caPem = caRef.requireOutput('caPem');
+
+const operatorRef = new StackReference('operator-ref', {
+  name: `UnstoppableMango/thecluster-cloudflare-operator/${config.require('operatorStack')}`,
+});
+
+export const operatorNamespace = operatorRef.requireOutput('namespace');
+export const apiSecretsName = operatorRef.requireOutput('apiSecretsName');
--- infra/cloudflare-tunnels/index.ts
+++ infra/cloudflare-tunnels/index.ts
@@ -1,22 +1,24 @@
 import { Namespace, Secret } from '@pulumi/kubernetes/core/v1';
 import { ClusterTunnel } from '@unstoppablemango/thecluster-crds/networking/v1alpha1';
-import { provider, cloudflare, caPem } from './config';
+import { provider, cloudflare, caPem, versions, operatorNamespace, apiSecretsName } from './config';
+import { interpolate } from '@pulumi/pulumi';
 
 const ns = new Namespace('cloudflare-tunnels', {
   metadata: { name: 'cloudflare-tunnels' },
 }, { provider });
 
-const secret = new Secret('cloudflare-secrets', {
+const secret = new Secret('origin-ca-pool', {
   metadata: {
-    name: 'cloudflare-secrets',
+    name: 'origin-ca-pool',
     namespace: ns.metadata.name,
   },
   stringData: {
-    CLOUDFLARE_API_TOKEN: cloudflare.apiToken,
-    CLOUDFLARE_API_KEY: cloudflare.globalApiKey,
-    'tls.crt': caPem, // TODO: Probably find a better spot
+    'tls.crt': caPem,
   },
 }, { provider });
+
+const credentialsId = interpolate`${operatorNamespace}/${apiSecretsName}`;
+const credentials = Secret.get('credentials', credentialsId);
 
 const tunnel = new ClusterTunnel('thecluster.io', {
   metadata: {
@@ -28,11 +30,12 @@ const tunnel = new ClusterTunnel('thecluster.io', {
     },
     size: 3,
     originCaPool: secret.metadata.name,
+    image: `cloudflare/cloudflared:${versions.cloudflared}`,
     cloudflare: {
       accountId: cloudflare.accountId,
       domain: 'thecluster.io',
       email: cloudflare.email,
-      secret: secret.metadata.name,
+      secret: credentials.metadata.name,
     },
   },
 }, { provider });
