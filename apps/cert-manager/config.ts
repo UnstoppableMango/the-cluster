9e2b2c93feab6a18b02a83a855d54d4e18a2e46f
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-02-08 16:00:40 -0600

    Tweak cert-manager to work with the new convention

--- apps/cert-manager/config.ts
+++ apps/cert-manager/config.ts
@@ -1,3 +1,7 @@
-import { Config } from '@pulumi/pulumi';
+import { Config, getStack } from '@pulumi/pulumi';
+import * as cluster from '@unstoppablemango/thecluster/cluster';
 
 const config = new Config();
+export const clusterName = 'rosequartz';
+export const ref = cluster.ref(clusterName, getStack());
+export const provider = cluster.provider(ref, clusterName);
