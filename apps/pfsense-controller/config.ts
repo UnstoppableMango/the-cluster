559fa4190cd3f628ac3ca15837d1ed8e0082aa56
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-02-09 21:56:13 -0600

    WIP adding back the controller

---
+++ apps/pfsense-controller/config.ts
@@ -0,0 +1,14 @@
+import { Config, getStack } from '@pulumi/pulumi';
+import * as cluster from '@unstoppablemango/thecluster/cluster';
+
+export interface Versions {
+  controller: string;
+}
+
+const config = new Config();
+export const clusterName = 'rosequartz';
+export const ref = cluster.ref(clusterName, getStack());
+export const provider = cluster.provider(ref, clusterName);
+export const username = config.require('username');
+export const password = config.require('password');
+export const versions = config.requireObject<Versions>('versions');
