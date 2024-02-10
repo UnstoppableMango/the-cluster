d60513f898bd2119150e5fb168e8eb13a7631ab4
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-02-09 23:37:01 -0600

    More config

--- apps/pfsense-controller/index.ts
+++ apps/pfsense-controller/index.ts
@@ -1,6 +1,6 @@
-import { Chart } from '@pulumi/kubernetes/helm/v3';
-import { password, provider, username } from './config';
 import { Namespace } from '@pulumi/kubernetes/core/v1';
+import { Chart } from '@pulumi/kubernetes/helm/v3';
+import { password, provider, username, versions } from './config';
 
 const ns = new Namespace('pfsense-system', {
   metadata: { name: 'pfsense-system' },
@@ -12,9 +12,26 @@ const chart = new Chart('pfsense-controller', {
   values: {
     'pfsense-controller': {
       pfsense: {
-        url: '192.168.1.1',
+        url: 'http://192.168.1.1',
+        insecure: true,
         username,
         password,
+      },
+      config: {
+        'controller-id': 'thecluster',
+        enabled: true,
+        plugins: {
+          'metallb': { enabled: false },
+          'haproxy-declarative': { enabled: false },
+          'haproxy-ingress-proxy': { enabled: false },
+          'pfsense-dns-services': { enabled: false },
+          'pfsense-dns-ingresses': { enabeld: false },
+          'pfsense-dns-haproxy-ingress-proxy': { enabled: false },
+        },
+      },
+      image: {
+        repository: 'docker.io/travisghansen/kubernetes-pfsense-controller',
+        tag: versions.controller,
       },
     },
   },
