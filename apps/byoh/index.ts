c328adb8c02ef6c5ae29280f8535759ec58614c6
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-03-05 22:39:21 -0600

    Use custom image

--- apps/byoh/index.ts
+++ apps/byoh/index.ts
@@ -1,6 +1,18 @@
-import * as k8s from '@pulumi/kubernetes';
+import { CustomResourceOptions } from '@pulumi/pulumi';
+import { Chart } from '@pulumi/kubernetes/helm/v3';
 import { provider } from '@unstoppablemango/thecluster/cluster/from-stack';
 
-const chart = new k8s.helm.v3.Chart('byoh', {
+const chart = new Chart('byoh', {
   path: './',
+  transformations: [patchControllerImage],
 }, { provider });
+
+function patchControllerImage(obj: any, opt: CustomResourceOptions): void {
+  if (obj.kind !== 'Deployment') return;
+  if (obj.metadata.name !== 'byoh-controller-manager') return;
+
+  obj.spec.template.spec.containers.forEach((x: any) => {
+    if (x.name !== 'manager') return;
+    x.image = 'ghcr.io/unstoppablemango/byoh:v2.0.5';
+  });
+}
