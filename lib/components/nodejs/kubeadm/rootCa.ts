c8ccdf336fda6331f38d3ddb7b062d781aff30d5
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-03-12 23:31:43 -0500

    Some helper functions

--- lib/components/nodejs/kubeadm/rootCa.ts
+++ lib/components/nodejs/kubeadm/rootCa.ts
@@ -1,6 +1,7 @@
 import { ComponentResourceOptions, Input } from '@pulumi/pulumi';
 import { SelfSignedCert } from '@pulumi/tls';
 import { KeyPair, KeyPairArgs } from './keypair';
+import { Certificate, CertificateArgs } from './certificate';
 
 export interface RootCaArgs extends KeyPairArgs { }
 
@@ -27,5 +28,49 @@ export class RootCa extends KeyPair<SelfSignedCert> {
     this.cert = cert;
 
     this.registerOutputs({ cert });
+  }
+
+  public newCertificate(
+    name: string,
+    args: Omit<CertificateArgs, 'caCertPem' | 'caPrivateKeyPem'>,
+    opts?: ComponentResourceOptions,
+  ): Certificate {
+    return new Certificate(name, {
+      ...args,
+      caCertPem: this.cert.certPem,
+      caPrivateKeyPem: this.key.privateKeyPem,
+    }, opts);
+  }
+
+  public newAdminCertificate(
+    name: string,
+    args: Omit<
+      CertificateArgs,
+      'caCertPem' | 'caPrivateKeyPem' | 'commonName' | 'organization'
+    >,
+    opts?: ComponentResourceOptions,
+  ): Certificate {
+    return this.newCertificate(`${name}-admin`, {
+      ...args,
+      commonName: 'admin',
+      organization: 'system:masters',
+    }, opts);
+  }
+
+  public newKubeletCertificate(
+    name: string,
+    index: number,
+    ip: string,
+    args: Omit<
+      CertificateArgs,
+      'caCertPem' | 'caPrivateKeyPem' | 'commonName' | 'organization'
+    >,
+    opts?: ComponentResourceOptions,
+  ): Certificate {
+    return this.newCertificate(`${name}-node${index}`, {
+      ...args,
+      commonName: `system:node:${ip}`,
+      organization: 'system:nodes',
+    }, opts);
   }
 }
