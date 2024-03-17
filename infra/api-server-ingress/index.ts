10d729f44f2b9b02f5e0f8aa578090ce70ab31fe
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-03-17 00:02:06 -0500

    Create a service account for token access

--- infra/api-server-ingress/index.ts
+++ infra/api-server-ingress/index.ts
@@ -1,6 +1,7 @@
-import { Namespace, Service } from '@pulumi/kubernetes/core/v1';
+import { Namespace, Secret, Service, ServiceAccount } from '@pulumi/kubernetes/core/v1';
 import { TunnelBinding } from '@unstoppablemango/thecluster-crds/networking/v1alpha1';
 import { fqdn, provider } from './config';
+import { ClusterRole, ClusterRoleBinding } from '@pulumi/kubernetes/rbac/v1';
 
 const ns = Namespace.get('kube-system', 'kube-system', { provider });
 const service = Service.get('kubernetes', 'kubernetes', { provider });
@@ -22,4 +23,38 @@ const tunnel = new TunnelBinding('api-server', {
       caPool: 'kube-root-ca.crt',
     },
   }],
+}, { provider });
+
+const sa = new ServiceAccount('admin-pub', {
+  metadata: {
+    name: 'admin-pub',
+    namespace: ns.metadata.name,
+  },
+}, { provider });
+
+const sec = new Secret('admin-pub', {
+  metadata: {
+    name: 'admin-pub',
+    namespace: ns.metadata.name,
+    annotations: {
+      'kubernetes.io/service-account.name': sa.metadata.name,
+    },
+  },
+  type: 'kubernetes.io/service-account-token',
+}, { provider });
+
+const role = ClusterRole.get('cluster-admin', 'cluster-admin');
+
+const rb = new ClusterRoleBinding('admin-pub', {
+  metadata: { name: 'admin-pub' },
+  roleRef: {
+    apiGroup: 'rbac.authorization.k8s.io',
+    kind: role.kind,
+    name: role.metadata.name,
+  },
+subjects: [{
+  kind: sa.kind,
+  namespace: ns.metadata.name,
+  name: sa.metadata.name,
+}],
 }, { provider });
