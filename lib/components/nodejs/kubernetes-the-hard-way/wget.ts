5be02bcb7fa39d66decd23f64c6f24a84935d2c7
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-03-19 17:06:19 -0500

    Refactor wget to be more focused

--- lib/components/nodejs/kubernetes-the-hard-way/wget.ts
+++ lib/components/nodejs/kubernetes-the-hard-way/wget.ts
@@ -1,16 +1,24 @@
-import { ComponentResource, ComponentResourceOptions, Input, Output, interpolate, output } from '@pulumi/pulumi';
+import { ComponentResource, ComponentResourceOptions, Input, Output, all, interpolate, output } from '@pulumi/pulumi';
 import { Command } from '@pulumi/command/remote';
 import { remote } from '@pulumi/command/types/input';
 
 export interface WgetArgs {
   connection: Input<remote.ConnectionArgs>;
-  path: Input<string>;
+  directoryPrefix?: Input<string>;
+  httpsOnly?: Input<boolean>;
+  outputDocument?: Input<string>;
+  quiet?: Input<boolean>;
+  timestamping?: Input<boolean>;
   url: Input<string>;
 }
 
 export class Wget extends ComponentResource {
   public readonly command: Command;
-  public readonly path: Output<string>;
+  public readonly directoryPrefix: Output<string | undefined>;
+  public readonly httpsOnly: Output<boolean>;
+  public readonly outputDocument: Output<string | undefined>;
+  public readonly quiet: Output<boolean>;
+  public readonly timestamping: Output<boolean>;
   public readonly url: Output<string>;
 
   public get stderr(): Output<string> {
@@ -28,19 +36,47 @@ export class Wget extends ComponentResource {
   constructor(name: string, args: WgetArgs, opts?: ComponentResourceOptions) {
     super('thecluster:index:wget', name, args, opts);
 
+    const directoryprefix = output(args.directoryPrefix);
+    const httpsOnly = output(args.httpsOnly ?? true);
+    const outputDocument = output(args.outputDocument);
+    const quiet = output(args.quiet ?? false);
+    const timestamping = output(args.timestamping ?? true);
     const url = output(args.url);
-    const path = output(args.path);
+
+    const options: Output<string> = all([
+      directoryprefix,
+      httpsOnly,
+      outputDocument,
+      quiet,
+      timestamping,
+    ]).apply(([directoryPrefix, httpsOnly, outputDocument, quiet, timestamping]) => {
+      const options: string[] = [];
+
+      if (directoryPrefix) options.push(`--directory-prefix '${directoryPrefix}'`);
+      if (httpsOnly) options.push('--https-only');
+      if (outputDocument) options.push(`--output-document '${outputDocument}'`);
+      if (quiet) options.push('--quiet');
+      if (timestamping ?? true) options.push('--timestamping');
+
+      return options.join(' ');
+    });
 
     const command = new Command(name, {
       connection: args.connection,
-      create: interpolate`wget --https-only --timestamping -O ${path} ${url}`,
-      delete: interpolate`rm -f ${path}`,
+      create: interpolate`wget ${options} '${url}'`,
     }, { parent: this });
 
     this.command = command;
-    this.path = path;
+    this.directoryPrefix = directoryprefix;
+    this.httpsOnly = httpsOnly;
+    this.outputDocument = outputDocument;
+    this.quiet = quiet;
+    this.timestamping = timestamping;
     this.url = url;
 
-    this.registerOutputs({ command, path, url });
+    this.registerOutputs({
+      command, httpsOnly, outputDocument,
+      quiet, timestamping, url,
+    });
   }
 }
