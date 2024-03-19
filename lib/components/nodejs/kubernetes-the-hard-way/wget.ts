1b680b2e2de4e7e07dacdffc5cbf83297b8cdec1
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-03-18 22:48:58 -0500

    WIP on wget

--- lib/components/nodejs/kubernetes-the-hard-way/wget.ts
+++ lib/components/nodejs/kubernetes-the-hard-way/wget.ts
@@ -2,18 +2,16 @@ import { ComponentResource, ComponentResourceOptions, Input, Output, interpolate
 import { Command } from '@pulumi/command/remote';
 import { remote } from '@pulumi/command/types/input';
 
-export type InstallArgs = Omit<RemoteFileArgs, 'content'>;
-
-export interface RemoteFileArgs {
+export interface WgetArgs {
   connection: Input<remote.ConnectionArgs>;
   path: Input<string>;
-  content: Input<string>;
+  url: Input<string>;
 }
 
-export class RemoteFile extends ComponentResource {
+export class Wget extends ComponentResource {
   public readonly command: Command;
-  public readonly content: Output<string>;
   public readonly path: Output<string>;
+  public readonly url: Output<string>;
 
   public get stderr(): Output<string> {
     return this.command.stderr;
@@ -27,23 +25,22 @@ export class RemoteFile extends ComponentResource {
     return this.command.stdout;
   }
 
-  constructor(name: string, args: RemoteFileArgs, opts?: ComponentResourceOptions) {
-    super('thecluster:index:remoteFile', name, args, opts);
+  constructor(name: string, args: WgetArgs, opts?: ComponentResourceOptions) {
+    super('thecluster:index:wget', name, args, opts);
 
-    const content = output(args.content);
+    const url = output(args.url);
     const path = output(args.path);
 
     const command = new Command(name, {
       connection: args.connection,
-      stdin: content,
-      create: interpolate`tee ${path}`,
-      delete: interpolate`rm ${path}`,
+      create: interpolate`wget --https-only --timestamping -O ${path} ${url}`,
+      delete: interpolate`rm -f ${path}`,
     }, { parent: this });
 
     this.command = command;
-    this.content = content;
     this.path = path;
+    this.url = url;
 
-    this.registerOutputs({ command, content, path });
+    this.registerOutputs({ command, path, url });
   }
 }
