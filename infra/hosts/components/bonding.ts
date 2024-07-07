59f4c3b4d7f83852b54f84d3a78f6441219920e8
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-07-07 13:35:42 -0500

    Enhanced network bonding functionality
    The code changes introduce an enhancement to the network bonding functionality. The Bonding class now accepts a new argument 'bond' which is used to configure the bond interfaces, addresses, and parameters. YAML is used to stringify these configurations. Additionally, a new Bond object has been defined in the config file with properties like name, interfaces, addresses, and mode. This allows for more flexible and detailed configuration of network bonds.

--- infra/hosts/components/bonding.ts
+++ infra/hosts/components/bonding.ts
@@ -1,22 +1,62 @@
+import * as YAML from 'yaml';
 import { asset, ComponentResourceOptions } from '@pulumi/pulumi';
 import { remote } from '@pulumi/command';
+import { Tee } from '@unmango/pulumi-commandx/remote';
 import { CommandComponent, CommandComponentArgs } from './command';
+import { Bond } from '../config';
 
-export interface BondingArgs extends CommandComponentArgs {}
+export interface BondingArgs extends CommandComponentArgs {
+  bond: Bond;
+}
 
 export class Bonding extends CommandComponent {
   constructor(name: string, args: BondingArgs, opts?: ComponentResourceOptions) {
     super(`thecluster:infra:Bonding/?${name}`, name, args, opts);
     if (opts?.urn) return;
 
-    this.exec(remote.CopyToRemote, name, {
+    const { bond } = args;
+
+    this.exec(remote.CopyToRemote, 'systemd-module', {
       remotePath: '/etc/modules-load.d/bonding.conf',
       source: new asset.StringAsset('bonding'),
     });
 
-    this.exec(remote.Command, name, {
+    const modprobe = this.exec(remote.Command, 'modprobe', {
       create: 'modprobe bonding',
       delete: 'modprobe -r bonding',
     });
+
+    const content = YAML.stringify({
+      network: {
+        bonds: {
+          [bond.name]: {
+            interfaces: bond.interfaces,
+            addresses: bond.addresses,
+            parameters: {
+              mode: bond.mode,
+              'transmit-hash-policy': 'layer3+4',
+              'mii-monitor-interval': 1,
+            },
+          },
+        },
+      },
+    });
+
+    const remove = this.exec(remote.Command, 'remove-netplan', {
+      delete: 'netplan apply',
+    });
+
+    const file = '/etc/netplan/60-bonding.yaml';
+    const config = this.exec(Tee, 'netplan-config', {
+      stdin: content,
+      create: {
+        files: [file],
+      },
+      delete: `rm ${file}`,
+    }, { dependsOn: remove });
+
+    this.exec(remote.Command, 'apply-netplan', {
+      create: 'netplan apply',
+    }, { dependsOn: [modprobe, config] });
   }
 }
