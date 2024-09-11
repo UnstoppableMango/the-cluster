1c68f0c0c66aed5a521453c715342b661089de03
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-09-11 00:22:01 -0500

    Removing stuff that is now in other repos
    # Conflicts:
    #	infra/hosts/package-lock.json
    
    # Conflicts:
    #	infra/hosts/package-lock.json

--- clusters/pinkdiamond/config.ts
+++ clusters/pinkdiamond/config.ts
@@ -8,47 +8,9 @@ const Versions = z.object({
 
 export type Versions = z.infer<typeof Versions>;
 
-const Vlan = z.object({
-  tag: z.number(),
-  name: z.string(),
-  interface: z.string(),
-});
-
-export type Vlan = z.infer<typeof Vlan>;
-
-const AnyPrimitive = z.union([
-  z.string(),
-  z.boolean(),
-  z.number(),
-]);
-
-const Node = z.object({
-  hostname: z.string(),
-  arch: z.union([
-    z.literal('amd64'),
-    z.literal('arm64'),
-  ]),
-  ip: z.string(),
-  clusterIp: z.string(),
-  installDisk: z.string(),
-  qemu: z.boolean().optional(),
-  nodeLabels: z.record(AnyPrimitive).optional(),
-  nodeTaints: z.record(AnyPrimitive).optional(),
-  vlan: Vlan.optional(),
-});
-
-export type Node = z.infer<typeof Node>;
-
 export const config = new Config();
 export const stack = getStack();
 
-const getNodes: (key: string) => Node[] = (key) => {
-  const value = config.requireObject(key);
-  return z.array(Node).parse(value);
-}
-
-export const controlplanes: Node[] = getNodes('controlplanes');
-export const workers: Node[] = getNodes('workers');
 export const versions: Versions = Versions.parse(config.requireObject('versions'));
 
 const TheClusterTls = z.object({
@@ -56,10 +18,8 @@ const TheClusterTls = z.object({
   certPem: z.string(),
 });
 
-const caRef = new StackReference('ca', {
-  name: `UnstoppableMango/tls/prod`,
+const pik8s4Ref = new StackReference('pik8s4', {
+  name: `UnstoppableMango/hosts/pik8s4`,
 });
 
-const thecluster = caRef.requireOutput('thecluster').apply(TheClusterTls.parse);
-export const caPrivateKeyPem = thecluster.privateKeyPem;
-export const caCertPem = thecluster.certPem;
+export const kubeconfig = pik8s4Ref.requireOutput('kubeconfig').apply(String);
--- clusters/pinkdiamond/index.ts
+++ clusters/pinkdiamond/index.ts
@@ -1,101 +1,11 @@
-import { pipe } from 'fp-ts/lib/function';
-import * as Arr from 'fp-ts/Array';
-import { ComponentResource, ComponentResourceOptions } from '@pulumi/pulumi';
-import * as tls from '@pulumi/tls';
-import { types } from '@pulumi/command';
-import { Node, controlplanes, workers } from './config';
+import * as config from './config';
 
-interface Provisioned {
-  node: Node;
-  key: tls.PrivateKey;
-}
+// I'm not sure what I want to define as a "cluster" yet, and
+// therefore where resources will live. So for now simply
+// re-export the `kubeconfig` so that apps etc have a single
+// source of truth
 
-type NodeType = 'controlplane' | 'worker';
-type Tagged<T> = [NodeType, T];
-
-interface ClusterNodeArgs extends Provisioned {
-}
-
-class ClusterNode extends ComponentResource implements Provisioned {
-  public node: Node;
-  public key: tls.PrivateKey;
-  public connection: types.input.remote.ConnectionArgs;
-
-  constructor(name: string, args: ClusterNodeArgs, opts?: ComponentResourceOptions) {
-    super(`thecluster:index:ClusterNode/${name}`, name, args, opts);
-
-    const { node, key } = args;
-
-    this.node = node;
-    this.key = key;
-
-    this.connection = {
-      host: node.ip,
-      privateKey: key.privateKeyOpenssh,
-    };
-
-    this.registerOutputs({
-      node: this.node,
-      key: this.key,
-    });
-  }
-}
-
-type Exports = Record<string, ClusterNode>;
-
-const Tagged = {
-  tag(type: NodeType): (node: Node) => Tagged<Node> {
-    return (node) => [type, node];
-  },
-  map<T, V>(f: (x: T) => V): (tagged: Tagged<T>) => Tagged<V> {
-    return ([tag, x]) => [tag, f(x)];
-  },
-  tap<T>(f: (x: Tagged<T>) => void): (tagged: Tagged<T>) => Tagged<T> {
-    return (tagged) => {
-      f(tagged);
-      return tagged;
-    }
-  },
-  untag<T>(): (tagged: Tagged<T>) => T {
-    return ([_, x]) => x;
-  },
-};
-
-function toClusterNode(node: Node): ClusterNode {
-  const key = new tls.PrivateKey(node.ip, {
-    algorithm: 'ED25519',
-  }, { protect: true });
-
-  return new ClusterNode(
-    node.hostname,
-    { key, node },
-  );
-}
-
-function provision([type, node]: Tagged<ClusterNode>): void {
-  switch (type) {
-    case 'controlplane':
-      // node.configureControlPlanePorts();
-      break;
-    case 'worker':
-      // node.configureWorkerPorts();
-      // node.enableIpv4PacketForwarding();
-      // node.installWorkerTools();
-      break;
-  }
-}
-
-function toExports(acc: Exports, node: ClusterNode): Exports {
-  return { ...acc, [node.node.hostname]: node };
-}
-
-export const provisioned = pipe(
-  [
-    ...pipe(controlplanes, Arr.map(Tagged.tag('controlplane'))),
-    ...pipe(workers, Arr.map(Tagged.tag('worker'))),
-  ],
-  Arr.map(Tagged.map(toClusterNode)),
-  Arr.map(Tagged.tap(provision)),
-  Arr.map(Tagged.untag<ClusterNode>()),
-  Arr.reduce({}, toExports)
-);
+export const kubeconfig = config.kubeconfig.apply(x => {
+  // I know this is lazy but I'm tired and I just want this working right now
+  return x.replace('127.0.0.1', '192.168.1.100');
+});
