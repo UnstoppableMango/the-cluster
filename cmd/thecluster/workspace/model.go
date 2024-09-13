d9ab6eefec3e44e6fa0c0d930d859041158c353a
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-09-13 15:52:18 -0500

    WIP breaking apart workspace actions

--- cmd/thecluster/workspace/model.go
+++ cmd/thecluster/workspace/model.go
@@ -2,6 +2,7 @@ package workspace
 
 import (
 	"context"
+	"errors"
 	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
@@ -15,6 +16,7 @@ type model struct {
 	loading bool
 	prev    tea.Model
 	work    *tc.Workspace
+	pw      *auto.Workspace
 	errs    []error
 }
 
@@ -22,49 +24,70 @@ type workspaceError error
 
 type workspaceLoaded *auto.Workspace
 
-func loadWorkspace(m model) tea.Cmd {
-	return func() tea.Msg {
-		log := log.FromContext(m.ctx)
-		work, err := auto.NewLocalWorkspace(m.ctx,
-			auto.WorkDir("clusters/pinkdiamond"),
-		)
-		if err != nil {
-			log.Error("failed creating new local workspace", "err", err)
-			return workspaceError(err)
-		}
+type depsInstalled struct{}
 
-		err = work.Install(m.ctx, &auto.InstallOptions{
-			// Stdout: os.Stdout,
-			// Stderr: os.Stderr,
-		})
-		if err != nil {
-			log.Error("failed installing deps", "err", err)
-			return workspaceError(err)
-		}
+func (m model) loadWorkspace() tea.Msg {
+	log := log.FromContext(m.ctx)
+	work, err := auto.NewLocalWorkspace(m.ctx,
+		auto.WorkDir("clusters/pinkdiamond"),
+	)
+	if err != nil {
+		log.Error("failed creating new local workspace", "err", err)
+		return workspaceError(err)
+	}
 
-		// s, err := auto.SelectStack(m.ctx, "prod", work)
-		// if err != nil {
-		// 	log.Error("failed selecting stack", "err", err)
-		// 	return workspaceError(err)
-		// }
+	// s, err := auto.SelectStack(m.ctx, "prod", work)
+	// if err != nil {
+	// 	log.Error("failed selecting stack", "err", err)
+	// 	return workspaceError(err)
+	// }
 
-		// _, err = s.Preview(m.ctx, optpreview.ProgressStreams(os.Stdout))
-		// if err != nil {
-		// 	log.Error("failed previewing stack", "err", err)
-		// 	return workspaceError(err)
-		// }
+	// _, err = s.Preview(m.ctx, optpreview.ProgressStreams(os.Stdout))
+	// if err != nil {
+	// 	log.Error("failed previewing stack", "err", err)
+	// 	return workspaceError(err)
+	// }
 
-		return workspaceLoaded(&work)
+	return workspaceLoaded(&work)
+}
+
+func (m model) installDeps() tea.Msg {
+	if m.pw == nil {
+		return workspaceError(errors.New("workspace must be loaded"))
+	}
+
+	workspace := *m.pw
+	err := workspace.Install(m.ctx, &auto.InstallOptions{
+		// Stdout: os.Stdout,
+		// Stderr: os.Stderr,
+	})
+	if err != nil {
+		log.Error("failed installing deps", "err", err)
+		return workspaceError(err)
+	}
+
+	return depsInstalled{}
+}
+
+func (m model) selectStack() tea.Msg {
+	if m.pw == nil {
+		return workspaceError(errors.New("workspace must be loaded"))
+	}
+
+	s, err := auto.SelectStack(m.ctx, "prod", work)
+	if err != nil {
+		log.Error("failed selecting stack", "err", err)
+		return workspaceError(err)
 	}
 }
 
 func New(ctx context.Context, prev tea.Model, w *tc.Workspace) tea.Model {
-	return model{ctx, true, prev, w, []error{}}
+	return model{ctx, true, prev, w, nil, []error{}}
 }
 
 // Init implements tea.Model.
 func (m model) Init() tea.Cmd {
-	return loadWorkspace(m)
+	return m.loadWorkspace
 }
 
 // Update implements tea.Model.
--- go.mod
+++ go.mod
@@ -68,6 +68,7 @@ require (
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	github.com/sabhiram/go-gitignore v0.0.0-20210923224102-525f6e181f06 // indirect
+	github.com/sahilm/fuzzy v0.1.1 // indirect
 	github.com/santhosh-tekuri/jsonschema/v5 v5.3.1 // indirect
 	github.com/sergi/go-diff v1.3.2-0.20230802210424-5b0b94c5c0d3 // indirect
 	github.com/skeema/knownhosts v1.3.0 // indirect
