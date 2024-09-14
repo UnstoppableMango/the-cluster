23659692fca82969c0226831565199cdae56691f
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-09-13 23:26:38 -0500

    Some stuff to try to make a scanner happen (#2859)
    * WIP breaking apart workspace actions
    
    * More WIP on the state
    
    * Updated .gitignore and Makefile, added new scanner component
    
    - Updated .gitignore to ignore *.profile and report.json files
    - Refactored Makefile for better test coverage reporting
    - Introduced a new scanner component with corresponding tests
    - Renamed variables in workspace model for clarity
    - Added Ginkgo and Gomega testing libraries to go.mod
    
    * Optimized source file listing and test dependencies
    
    Updated the Makefile to improve efficiency and reliability. The source file listing now uses git ls-files with deduplication, excluding deleted files. Also, adjusted the Ginkgo reports dependency rule to ensure accurate triggering of tests when Go source files or modules change.
    
    * Refactor scanner model and enhance tests
    
    Removed the Render function from the scanner model as it was not being used. Enhanced the test coverage for the scanner model by adding new tests to check initialization of an empty scanner, return of text, and reading lines. Also removed unnecessary suite test file.
    
    * Updated CI workflow with test step
    
    A new step has been added to the GitHub Actions Continuous Integration (CI) workflow. This additional step runs tests before proceeding with the build process, ensuring that only code that passes all tests is built and deployed.
    
    * Updated CI workflow with buf-action
    
    The Continuous Integration (CI) workflow has been updated to include the use of 'buf-action'. This addition is set up to only perform setup tasks. The change aims to enhance the testing process, making it more efficient and reliable.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 _ := $(shell mkdir -p .make)
-WORKING_DIRECTORY := $(shell pwd)
+WORKING_DIR := $(shell pwd)
 
 STACK   := prod
 CLUSTER := pinkdiamond
@@ -7,16 +7,26 @@ CLUSTER := pinkdiamond
 ROOT_MODULES := apps clusters dbs infra
 MODULES := $(shell find ${ROOT_MODULES} -mindepth 1 -maxdepth 1 -type d)
 
-SRC        := $(shell git ls-files)
+GIT_LS_FILES ?= $(shell git ls-files --deduplicate)
+
+SRC        := $(filter-out $(shell git ls-files -d),$(GIT_LS_FILES))
 PROTO_SRC  := $(filter %.proto,${SRC})
 TS_SRC     := $(filter %.ts,${SRC})
 GO_GEN_SRC := $(PROTO_SRC:proto/%.proto=gen/go/%.pb.go)
 GO_SRC     := $(filter %.go,${SRC}) $(GO_GEN_SRC)
 
+COV_REPORT := cover.profile
+TEST_REPORT := report.json
+GINKGO_REPORTS := $(COV_REPORT) $(TEST_REPORT)
+
 PULUMI := pulumi
+GINKGO := go run github.com/onsi/ginkgo/v2/ginkgo
 
 tc: bin/thecluster $(TS_SRC)
 	$<
+
+test: $(GINKGO_REPORTS)
+testf: .make/clean_tests $(GINKGO_REPORTS)
 
 gen: $(PROTO_SRC:proto/%.proto=gen/go/%.pb.go)
 
@@ -28,3 +38,10 @@ bin/thecluster: $(filter cmd/%,${GO_SRC})
 
 gen/go/%.pb.go: proto/%.proto
 	buf generate $?
+
+$(GINKGO_REPORTS) &:: go.mod go.sum $(GO_SRC)
+	$(GINKGO) run --coverprofile=$(COV_REPORT) \
+	--race --trace --json-report=$(TEST_REPORT) -r ./...
+
+.make/clean_tests:
+	rm -f $(GINKGO_REPORTS)
--- cmd/thecluster/workspace/model.go
+++ cmd/thecluster/workspace/model.go
@@ -1,86 +1,116 @@
 package workspace
 
 import (
+	"bytes"
 	"context"
+	"errors"
 	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/log"
 	"github.com/pulumi/pulumi/sdk/v3/go/auto"
+	"github.com/pulumi/pulumi/sdk/v3/go/auto/optpreview"
 	tc "github.com/unstoppablemango/the-cluster/gen/go/io/unmango/thecluster/v1alpha1"
 )
 
-type model struct {
+type Model struct {
 	ctx     context.Context
 	loading bool
 	prev    tea.Model
 	work    *tc.Workspace
+	stack   *auto.Stack
 	errs    []error
+	buf     *bytes.Buffer
 }
 
-type workspaceError error
+type (
+	Err           error
+	Loaded        auto.Workspace
+	DepsInstalled struct{}
+	StackSelected auto.Stack
+)
 
-type workspaceLoaded *auto.Workspace
+func (m Model) loadWorkspace() tea.Msg {
+	log := log.FromContext(m.ctx)
+	work, err := auto.NewLocalWorkspace(m.ctx,
+		auto.WorkDir(m.work.WorkingDirectory),
+	)
+	if err != nil {
+		log.Error("failed creating new local workspace", "err", err)
+		return Err(err)
+	}
 
-func loadWorkspace(m model) tea.Cmd {
+	return Loaded(work)
+}
+
+func (m Model) installDeps() tea.Msg {
+	workspace := m.stack.Workspace()
+	err := workspace.Install(m.ctx, &auto.InstallOptions{
+		Stdout: m.buf,
+		Stderr: m.buf,
+	})
+	if err != nil {
+		log.Error("failed installing deps", "err", err)
+		return Err(err)
+	}
+
+	return DepsInstalled{}
+}
+
+func (m Model) selectStack(work auto.Workspace) tea.Cmd {
 	return func() tea.Msg {
-		log := log.FromContext(m.ctx)
-		work, err := auto.NewLocalWorkspace(m.ctx,
-			auto.WorkDir("clusters/pinkdiamond"),
-		)
+		s, err := auto.SelectStack(m.ctx, "prod", work)
 		if err != nil {
-			log.Error("failed creating new local workspace", "err", err)
-			return workspaceError(err)
+			log.Error("failed selecting stack", "err", err)
+			return Err(err)
 		}
 
-		err = work.Install(m.ctx, &auto.InstallOptions{
-			// Stdout: os.Stdout,
-			// Stderr: os.Stderr,
-		})
-		if err != nil {
-			log.Error("failed installing deps", "err", err)
-			return workspaceError(err)
-		}
-
-		// s, err := auto.SelectStack(m.ctx, "prod", work)
-		// if err != nil {
-		// 	log.Error("failed selecting stack", "err", err)
-		// 	return workspaceError(err)
-		// }
-
-		// _, err = s.Preview(m.ctx, optpreview.ProgressStreams(os.Stdout))
-		// if err != nil {
-		// 	log.Error("failed previewing stack", "err", err)
-		// 	return workspaceError(err)
-		// }
-
-		return workspaceLoaded(&work)
+		return StackSelected(s)
 	}
 }
 
+func (m Model) preview() tea.Msg {
+	if m.stack == nil {
+		return Err(errors.New("no stack selected"))
+	}
+
+	_, err := m.stack.Preview(m.ctx, optpreview.ProgressStreams(m.buf))
+	if err != nil {
+		log.Error("failed previewing stack", "err", err)
+		return Err(err)
+	}
+
+	return nil
+}
+
 func New(ctx context.Context, prev tea.Model, w *tc.Workspace) tea.Model {
-	return model{ctx, true, prev, w, []error{}}
+	return Model{ctx, true, prev, w, nil, []error{}, &bytes.Buffer{}}
 }
 
 // Init implements tea.Model.
-func (m model) Init() tea.Cmd {
-	return loadWorkspace(m)
+func (m Model) Init() tea.Cmd {
+	return m.loadWorkspace
 }
 
 // Update implements tea.Model.
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
-	case workspaceError:
+	case Err:
 		m.errs = append(m.errs, msg)
 		m.loading = false
-	case workspaceLoaded:
+	case Loaded:
+		return m, m.selectStack(msg)
+	case StackSelected:
 		m.loading = false
+		return m, m.installDeps
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "ctrl+c", "q":
 			return m, tea.Quit
 		case "esc":
 			return m.prev, nil
+		case "enter":
+			return m, m.preview
 		}
 	}
 
@@ -88,7 +118,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 // View implements tea.Model.
-func (m model) View() string {
+func (m Model) View() string {
 	if len(m.errs) > 0 {
 		b := strings.Builder{}
 		for _, err := range m.errs {
@@ -103,4 +133,4 @@ func (m model) View() string {
 	return m.work.WorkingDirectory
 }
 
-var _ tea.Model = model{}
+var _ tea.Model = Model{}
