9c07f14fe166d159d8b8e40fbb64c6e1e6388f6d
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-09-15 18:21:16 -0500

    Some work on a reconciler

--- internal/thecluster/reconciler.go
+++ internal/thecluster/reconciler.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/charmbracelet/log"
 	"github.com/pulumi/pulumi/sdk/v3/go/auto"
+	"github.com/pulumi/pulumi/sdk/v3/go/auto/optpreview"
 	tc "github.com/unstoppablemango/the-cluster/gen/go/io/unmango/thecluster/v1alpha1"
 )
 
@@ -17,9 +18,23 @@ func (r Reconciler) Reconcile(ctx context.Context, req *tc.ReconcileRequest) (*t
 	log := log.FromContext(ctx)
 	root := r.Config.Root
 
-	_, err := r.LoadWorkspace(ctx, req.Component)
+	ws, err := r.LoadWorkspace(ctx, req.Component)
 	if err != nil {
 		log.Error("failed loading workspace", "err", err)
+		return nil, err
+	}
+
+	log.Info("loaded workspace", "ws", ws.WorkDir())
+
+	s, err := auto.SelectStack(ctx, req.Stack, ws)
+	if err != nil {
+		log.Error("failed selecting stack", "err", err)
+		return nil, err
+	}
+
+	_, err = s.Preview(ctx, optpreview.ProgressStreams())
+	if err != nil {
+		log.Error("failed component preview", "err", err)
 		return nil, err
 	}
 
