56c9986a7f309c3739d111aea28e03beac6f5142
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-09-15 18:27:50 -0500

    Some work on a reconciler (#2868)
    * Some work on a reconciler
    
    * Test different

--- internal/thecluster/reconciler_test.go
+++ internal/thecluster/reconciler_test.go
@@ -7,6 +7,7 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/pulumi/pulumi/sdk/v3/go/auto"
 
 	tc "github.com/unstoppablemango/the-cluster/gen/go/io/unmango/thecluster/v1alpha1"
 	"github.com/unstoppablemango/the-cluster/internal/thecluster"
@@ -14,19 +15,27 @@ import (
 
 var _ = Describe("Reconciler", func() {
 	var (
-		root = os.TempDir()
-		work string
-		sut  thecluster.Reconciler
+		root  = os.TempDir()
+		stack = "thecluster-reconcile"
+		work  string
+		sut   thecluster.Reconciler
 	)
 
-	BeforeEach(func() {
+	BeforeEach(func(ctx context.Context) {
+		Skip("Not ready yet")
+
 		var err error
 		work, err = os.MkdirTemp("", "")
 		Expect(err).NotTo(HaveOccurred())
+
 		wd, err := os.Getwd()
 		Expect(err).NotTo(HaveOccurred())
+
 		programPath := path.Join(wd, "testdata", "program")
 		err = os.CopyFS(work, os.DirFS(programPath))
+		Expect(err).NotTo(HaveOccurred())
+
+		_, err = auto.UpsertStackLocalSource(ctx, stack, work)
 		Expect(err).NotTo(HaveOccurred())
 
 		sut = thecluster.Reconciler{
@@ -46,8 +55,11 @@ var _ = Describe("Reconciler", func() {
 			Expect(err).NotTo(HaveOccurred())
 			Expect(actual).To(Equal(res))
 		},
-		Entry("should use config root", &tc.ReconcileRequest{}, &tc.ReconcileResponse{
-			Root: root,
-		}),
+		Entry("should use config root",
+			&tc.ReconcileRequest{
+				Stack: stack,
+			}, &tc.ReconcileResponse{
+				Root: root,
+			}),
 	)
 })
