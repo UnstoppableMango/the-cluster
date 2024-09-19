a14335c27e207489cb4cef63faf8ce758cfa9105
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-09-19 00:10:14 -0500

    Why didnt I commit this last night

--- cmd/kubebuilder/theclusterv1alpha1/init.go
+++ cmd/kubebuilder/theclusterv1alpha1/init.go
@@ -1,28 +1,27 @@
 package theclusterv1alpha1
 
 import (
-	"sigs.k8s.io/kubebuilder/v4/pkg/config"
 	"sigs.k8s.io/kubebuilder/v4/pkg/machinery"
 	"sigs.k8s.io/kubebuilder/v4/pkg/plugin"
 	golangv4 "sigs.k8s.io/kubebuilder/v4/pkg/plugins/golang/v4"
 )
 
 type initSubcommand struct {
-	config config.Config
+	// config config.Config
 
-	// boilerplate options
-	license string
-	owner   string
+	// // boilerplate options
+	// license string
+	// owner   string
 
-	golang golangv4.Plugin
+	golangv4.Plugin
 }
 
 // Scaffold implements plugin.InitSubcommand.
 func (i *initSubcommand) Scaffold(fs machinery.Filesystem) error {
-	c := i.golang.GetInitSubcommand()
-	err := c.Scaffold(fs)
+	// c := i.golang.GetInitSubcommand()
+	// err := c.Scaffold(fs)
 
-	return err
+	return nil
 }
 
 var _ plugin.InitSubcommand = &initSubcommand{}
