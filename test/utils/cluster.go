5f1d09feb3894627c137009810a048a4de727990
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-09-22 15:40:35 -0500

    Added stop function and version control to TestCluster
    In this update, we've introduced a new stop function to the TestCluster struct in the utils package. This allows for graceful shutdown of clusters. We've also added a way to specify the k3s version when creating a new TestCluster instance. Additionally, we have included default deadline variable for stopping the cluster.
    
    On top of these changes, we've created new tests in cluster_test.go to ensure that our new features are working as expected. These tests cover creation of a new TestCluster, setting k3s version and stopping the cluster successfully even if it hasn't been started yet.

--- test/utils/cluster.go
+++ test/utils/cluster.go
@@ -3,12 +3,17 @@ package utils
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/testcontainers/testcontainers-go/modules/k3s"
 )
 
 const (
 	defaultK3sVersion = "1.31.0-k3s1"
+)
+
+var (
+	defaultDeadline = 1 * time.Minute
 )
 
 type k3sCluster struct {
@@ -43,4 +48,26 @@ func (c *TestCluster) Start(ctx context.Context) error {
 
 	c.k3s.ctr = ctr
 	return nil
+}
+
+func (c *TestCluster) Stop(ctx context.Context) error {
+	if c.k3s.ctr == nil {
+		return nil
+	}
+
+	return c.k3s.ctr.Stop(ctx, &defaultDeadline)
+}
+
+func (c TestCluster) Image() string {
+	return c.k3s.Image()
+}
+
+func (c TestCluster) Version() string {
+	return c.k3s.version
+}
+
+func WithK3sVersion(version string) TestClusterOption {
+	return func(tc *TestCluster) {
+		tc.k3s.version = version
+	}
 }
