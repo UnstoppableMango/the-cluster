c23d2449b570b0d6761e3f0a0d09d1f59a0ad646
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-09-22 16:03:29 -0500

    Enhanced e2e test setup and teardown
    The e2e test suite has been significantly improved. The changes include the addition of context management for better control over timeouts, running k3s in Docker before tests, retrieving kubeconfig from k3s, creating a temporary directory for storing kubeconfig, and cleaning up after tests by stopping the cluster and unsetting the KUBECONFIG environment variable.

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -1,16 +1,55 @@
 package e2e
 
 import (
-	"fmt"
+	"context"
+	"os"
+	"path"
 	"testing"
+	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+
+	"github.com/unstoppablemango/the-cluster/test/utils"
 )
 
-// Run e2e tests using the Ginkgo runner.
+var (
+	cluster    = utils.NewTestCluster()
+	kubeconfig []byte
+)
+
 func TestE2E(t *testing.T) {
 	RegisterFailHandler(Fail)
-	_, _ = fmt.Fprintf(GinkgoWriter, "Starting the-cluster suite\n")
 	RunSpecs(t, "e2e suite")
 }
+
+var _ = BeforeSuite(func(ctx context.Context) {
+	var (
+		cancel context.CancelFunc
+		err    error
+	)
+
+	ctx, cancel = context.WithDeadline(ctx, time.Now().Add(3*time.Minute))
+	defer cancel()
+
+	By("running k3s in docker")
+	Expect(cluster.Start(ctx)).To(Succeed())
+
+	By("retrieving the kubeconfig from k3s")
+	kubeconfig, err = cluster.GetKubeConfig(ctx)
+	Expect(err).NotTo(HaveOccurred())
+
+	By("creating a temporary directory")
+	tmp, err := os.MkdirTemp("", "")
+	Expect(err).NotTo(HaveOccurred())
+
+	p := path.Join(tmp, "kubeconfig")
+	Expect(os.WriteFile(p, kubeconfig, os.ModePerm)).To(Succeed())
+	Expect(os.Setenv("KUBECONFIG", p)).To(Succeed())
+})
+
+var _ = AfterSuite(func(ctx context.Context) {
+	By("stopping the cluster")
+	Expect(cluster.Stop(ctx)).To(Succeed())
+	Expect(os.Unsetenv("KUBECONFIG")).To(Succeed())
+})
