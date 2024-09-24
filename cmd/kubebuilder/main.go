db269364d440510791a913c432304d597a05699f
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-09-24 15:42:04 -0500

    WIP adding the original bundle back

--- cmd/kubebuilder/main.go
+++ cmd/kubebuilder/main.go
@@ -7,13 +7,23 @@ import (
 	tcv1alpha1 "github.com/unstoppablemango/the-cluster/pkg/kubebuilder/plugins/thecluster/v1alpha1"
 	"sigs.k8s.io/kubebuilder/v4/pkg/cli"
 	cfgv3 "sigs.k8s.io/kubebuilder/v4/pkg/config/v3"
+	"sigs.k8s.io/kubebuilder/v4/pkg/plugin"
+	golang "sigs.k8s.io/kubebuilder/v4/pkg/plugins/golang/v4"
 )
 
 func main() {
+	defaultBundle, err := plugin.NewBundleWithOptions(
+		plugin.WithPlugins(golang.Plugin{}),
+	)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed building CLI: %v", err)
+		os.Exit(1)
+	}
+
 	c, err := cli.New(
 		cli.WithCommandName("kubebuilder"),
 		cli.WithPlugins(tcv1alpha1.Plugin{}),
-		cli.WithDefaultPlugins(cfgv3.Version, tcv1alpha1.Plugin{}),
+		cli.WithDefaultPlugins(cfgv3.Version, defaultBundle),
 		cli.WithCompletion(),
 	)
 	if err != nil {
