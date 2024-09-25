3101e40a6ab62025ad0b2c32a58a9486a092f7db
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-09-25 10:26:37 -0500

    Refactor the test cluster to hopefully support different backends...

--- test/utils/cluster.go
+++ test/utils/cluster.go
@@ -19,24 +19,80 @@ var (
 	defaultDeadline = 1 * time.Minute
 )
 
+type cluster interface {
+	Start(context.Context) error
+	Stop(context.Context) error
+	Kubeconfig(context.Context) ([]byte, error)
+	Version() string
+	withLoggers([]io.Writer)
+}
+
+type dockerCluster interface {
+	Image() string
+}
+
 type k3sCluster struct {
-	ctr     *k3s.K3sContainer
-	version string
+	ctr          *k3s.K3sContainer
+	logConsumers []testcontainers.LogConsumer
+	version      string
+}
+
+// Kubeconfig implements cluster.
+func (c *k3sCluster) Kubeconfig(ctx context.Context) ([]byte, error) {
+	if c.ctr == nil {
+		return nil, errors.New("cluster has not been initialized")
+	}
+
+	return c.ctr.GetKubeConfig(ctx)
+}
+
+// Start implements cluster.
+func (c *k3sCluster) Start(ctx context.Context) error {
+	ctr, err := k3s.Run(ctx, c.Image(),
+		testcontainers.WithLogConsumers(c.logConsumers...),
+	)
+	if err != nil {
+		return err
+	}
+
+	c.ctr = ctr
+	return nil
+}
+
+// Stop implements cluster.
+func (c *k3sCluster) Stop(ctx context.Context) error {
+	if c.ctr == nil {
+		return errors.New("cluster has not been initialized")
+	}
+
+	return c.ctr.Stop(ctx, &defaultDeadline)
+}
+
+// Version implements cluster.
+func (c *k3sCluster) Version() string {
+	return c.version
+}
+
+func (c *k3sCluster) withLoggers(loggers []io.Writer) {
+	c.logConsumers = logAdapters(loggers)
 }
 
 func (c k3sCluster) Image() string {
 	return fmt.Sprintf("docker.io/rancher/k3s:v%s", c.version)
 }
 
+var _ cluster = &k3sCluster{}
+
 type TestCluster struct {
-	k3s     *k3sCluster
+	backend cluster
 	loggers []io.Writer
 }
 
 type TestClusterOption func(*TestCluster)
 
 func NewTestCluster(opts ...TestClusterOption) *TestCluster {
-	c := &TestCluster{&k3sCluster{nil, defaultK3sVersion}, []io.Writer{}}
+	k := &k3sCluster{version: defaultK3sVersion}
+	c := &TestCluster{k, []io.Writer{}}
 	for _, configure := range opts {
 		configure(c)
 	}
@@ -45,66 +101,52 @@ func NewTestCluster(opts ...TestClusterOption) *TestCluster {
 }
 
 func (c *TestCluster) Start(ctx context.Context) error {
-	ctr, err := k3s.Run(ctx, c.k3s.Image(),
-		testcontainers.WithLogConsumers(c.logAdapters()...),
-	)
-	if err != nil {
-		return err
-	}
-
-	c.k3s.ctr = ctr
-	return nil
+	return c.backend.Start(ctx)
 }
 
 func (c *TestCluster) Stop(ctx context.Context) error {
-	if c.k3s.ctr == nil {
-		c.log("container was not initialized\n")
-		return nil
-	}
-
-	return c.k3s.ctr.Stop(ctx, &defaultDeadline)
+	return c.backend.Stop(ctx)
 }
 
 func (c *TestCluster) GetKubeConfig(ctx context.Context) ([]byte, error) {
-	if c.k3s == nil || c.k3s.ctr == nil {
-		return nil, errors.New("")
-	}
-
-	return c.k3s.ctr.GetKubeConfig(ctx)
+	return c.backend.Kubeconfig(ctx)
 }
 
 func (c TestCluster) Image() string {
-	return c.k3s.Image()
+	if d, ok := c.backend.(dockerCluster); ok {
+		return d.Image()
+	}
+
+	return ""
 }
 
 func (c TestCluster) Version() string {
-	return c.k3s.version
+	return c.backend.Version()
 }
 
 func (c TestCluster) logAdapters() []testcontainers.LogConsumer {
-	adapters := make([]testcontainers.LogConsumer, len(c.loggers))
-	for i, l := range c.loggers {
-		adapters[i] = &logAdapter{l}
-	}
-
-	return adapters
+	return logAdapters(c.loggers)
 }
 
-func (c TestCluster) log(msg string) {
-	for _, l := range c.loggers {
-		_, _ = l.Write([]byte(msg))
-	}
-}
-
+// Deprecated: Use WithK3s instead
 func WithK3sVersion(version string) TestClusterOption {
 	return func(tc *TestCluster) {
-		tc.k3s.version = version
+		if k, ok := tc.backend.(*k3sCluster); ok {
+			k.version = version
+		}
+	}
+}
+
+func WithK3s(version string) TestClusterOption {
+	return func(tc *TestCluster) {
+		tc.backend = &k3sCluster{nil, tc.logAdapters(), version}
 	}
 }
 
 func WithLoggers(loggers ...io.Writer) TestClusterOption {
 	return func(tc *TestCluster) {
 		tc.loggers = loggers
+		tc.backend.withLoggers(loggers)
 	}
 }
 
@@ -118,3 +160,12 @@ func (l *logAdapter) Accept(msg testcontainers.Log) {
 }
 
 var _ testcontainers.LogConsumer = &logAdapter{}
+
+func logAdapters(loggers []io.Writer) []testcontainers.LogConsumer {
+	adapters := make([]testcontainers.LogConsumer, len(loggers))
+	for i, l := range loggers {
+		adapters[i] = &logAdapter{l}
+	}
+
+	return adapters
+}
