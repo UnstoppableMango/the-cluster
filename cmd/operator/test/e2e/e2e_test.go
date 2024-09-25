e695f2813e45a34f2c8fea8b7e16880166c8598e
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-09-24 22:04:25 -0500

    Updated project naming conventions
    The commit includes changes to the project's naming conventions. All instances of 'the-cluster' have been replaced with 'thecluster'. This change affects various aspects including Dockerfile, Makefile, and several configuration files. The Kustomize version has also been removed from the Makefile.

--- cmd/operator/test/e2e/e2e_test.go
+++ cmd/operator/test/e2e/e2e_test.go
@@ -27,7 +27,7 @@ import (
 	"github.com/unstoppablemango/the-cluster/test/utils"
 )
 
-const namespace = "the-cluster-system"
+const namespace = "thecluster-system"
 
 var _ = Describe("controller", Ordered, func() {
 	BeforeAll(func() {
@@ -60,7 +60,7 @@ var _ = Describe("controller", Ordered, func() {
 			var err error
 
 			// projectimage stores the name of the image used in the example
-			var projectimage = "example.com/the-cluster:v0.0.1"
+			var projectimage = "example.com/thecluster:v0.0.1"
 
 			By("building the manager(Operator) image")
 			cmd := exec.Command("make", "docker-build", fmt.Sprintf("IMG=%s", projectimage))
