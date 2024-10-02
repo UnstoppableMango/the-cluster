f351ca4d0abc8ccacc90fb8699e720d21369cd0f
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-10-02 09:56:57 -0500

    Init the rook app

---
+++ apps/rook/config.ts
@@ -0,0 +1,12 @@
+import { Config, getStack } from '@pulumi/pulumi';
+import * as cluster from '@unstoppablemango/thecluster/cluster';
+
+export interface Versions {
+  rook: string;
+}
+
+const config = new Config();
+export const versions = config.requireObject<Versions>('versions');
+export const clusterName = getStack();
+export const ref = cluster.ref(clusterName, 'prod');
+export const provider = cluster.provider(ref, clusterName);
