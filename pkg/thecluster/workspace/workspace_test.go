5f147a8047c482dd046f7f834add08ea5c69493d
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-10-04 22:40:49 -0500

    Init test suite

---
+++ pkg/thecluster/workspace/workspace_test.go
@@ -0,0 +1,12 @@
+package workspace_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/unstoppablemango/the-cluster/pkg/thecluster/workspace"
+)
+
+var _ = Describe("Workspace", func() {
+
+})
