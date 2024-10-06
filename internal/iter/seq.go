40ae53b4be7490d2d9a78f61d2d333eda5069afc
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-10-06 15:44:22 -0500

    Refactor sequence operations and add new file system utilities
    Significant changes include:
    - Removed FilterR, Filter, and Map functions from the internal/iter package. These were generic sequence operations that have been deemed unnecessary.
    - Added two new files to the pkg/fs package: export.go and reducer.go.
      - In export.go, a type alias for fs.FileInfo was created.
      - In reducer.go, several types and functions related to filesystem iteration and reduction were introduced. This includes Cursor interface, cursor struct with its methods, Reducer type alias, Accumulate type alias, NewReducer function and Reduce function.

--- internal/iter/seq.go
+++ internal/iter/seq.go
@@ -2,8 +2,6 @@ package iter
 
 import (
 	"iter"
-
-	"github.com/unstoppablemango/tdl/pkg/result"
 )
 
 type (
@@ -16,36 +14,6 @@ func Downcast[V any](seq Seq[V]) iter.Seq[V] {
 
 func Upcast[V any](seq iter.Seq[V]) Seq[V] {
 	return Seq[V](seq)
-}
-
-func FilterR[V any](seq Seq[result.R[V]]) Seq[V] {
-	filtered := Filter(seq, func(v result.R[V]) bool {
-		return v.IsOk()
-	})
-
-	return Map(filtered, func(v result.R[V]) V {
-		return v.GetValue()
-	})
-}
-
-func Filter[V any](seq Seq[V], predicate func(V) bool) Seq[V] {
-	return func(yield func(V) bool) {
-		seq(func(v V) bool {
-			if predicate(v) {
-				return yield(v)
-			}
-
-			return true
-		})
-	}
-}
-
-func Map[V, X any](seq Seq[V], f func(V) X) Seq[X] {
-	return func(yield func(X) bool) {
-		seq(func(v V) bool {
-			return yield(f(v))
-		})
-	}
 }
 
 func Pull[V any](seq Seq[V]) (next func() (V, bool), stop func()) {
---
+++ pkg/fs/export.go
@@ -0,0 +1,5 @@
+package fs
+
+import "io/fs"
+
+type FileInfo = fs.FileInfo
