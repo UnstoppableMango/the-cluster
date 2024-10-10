b2420d11da0c94493c5f0e95109a411d94040256
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-10-10 11:05:52 -0500

    Fix the execute test

--- pkg/thecluster/template/file_test.go
+++ pkg/thecluster/template/file_test.go
@@ -5,13 +5,28 @@ import (
 	. "github.com/onsi/gomega"
 
 	"github.com/spf13/afero"
+	"github.com/unstoppablemango/the-cluster/pkg/thecluster"
 	"github.com/unstoppablemango/the-cluster/pkg/thecluster/template"
 )
 
 var _ = Describe("File", func() {
 	Context("Execute", func() {
+		var (
+			path   string
+			mockFs thecluster.Fs
+		)
+
+		BeforeEach(func() {
+			path = "test.txt"
+			mockFs = afero.NewMemMapFs()
+			Expect(mockFs.Create(path)).NotTo(BeNil())
+		})
+
 		It("should work", func() {
-			f := template.NewFile("TODO", nil)
+			file, err := mockFs.Open(path)
+			Expect(err).NotTo(HaveOccurred())
+
+			f := template.NewFile(path, file)
 			fs := afero.NewMemMapFs()
 
 			Expect(f.Execute(fs, nil)).To(Succeed())
