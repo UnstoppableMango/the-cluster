198d065c3d7933283c56658261d9e23631eac2d4
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-10-11 16:58:47 -0500

    Docs

--- pkg/thecluster/template/list.go
+++ pkg/thecluster/template/list.go
@@ -6,7 +6,7 @@ import (
 	"github.com/unstoppablemango/the-cluster/pkg/thecluster"
 )
 
-// List lists all valid templates that can be discovered at path,
+// List lists all valid templates that can be discovered at path
 // and ignores any templates that produce errors.
 func List(workspace thecluster.Workspace, path string) iter.Seq[thecluster.TemplateGroup] {
 	return seqs.FilterR(Discover(workspace, path))
