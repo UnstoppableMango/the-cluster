190d9e4035cdb92359d372bc0936946a53adcde6
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-12-09 23:51:54 -0600

    Trying to get prometheus working

--- apps/pulumi-operator/index.ts
+++ apps/pulumi-operator/index.ts
@@ -1,56 +1,24 @@
-import * as path from 'path';
 import { getStack } from '@pulumi/pulumi';
 import { AccessToken } from '@pulumi/pulumiservice';
-import { Namespace, Secret } from '@pulumi/kubernetes/core/v1';
-import { Chart } from '@pulumi/kubernetes/helm/v3';
-import { Stack } from '@unstoppablemango/thecluster-crds/pulumi/v1';
-import { required } from '@unstoppablemango/thecluster';
-import { provider, shared } from '@unstoppablemango/thecluster/cluster/from-stack';
+import { Secret } from '@pulumi/kubernetes/core/v1';
 import { stack, versions } from './config';
-import { ConfigGroup } from '@pulumi/kubernetes/yaml';
+import { Directory } from '@pulumi/kubernetes/kustomize/v2';
 
-const ns = Namespace.get(
-  'pulumi-operator',
-  shared.namespaces.pulumiOperator,
-  { provider },
-);
-
-const crds = new ConfigGroup('crds', {
-  files: [
-    `https://raw.githubusercontent.com/pulumi/pulumi-kubernetes-operator/${versions.pulumiOperator}/deploy/crds/pulumi.com_programs.yaml`,
-    `https://raw.githubusercontent.com/pulumi/pulumi-kubernetes-operator/${versions.pulumiOperator}/deploy/crds/pulumi.com_stacks.yaml`,
-  ],
-}, { provider });
-
-const chart = new Chart('pulumi-operator', {
-  path: './',
-  namespace: ns.metadata.name,
-  skipCRDRendering: true,
-  values: {
-    'pulumi-kubernetes-operator': {
-      replicaCount: 2,
-      image: {
-        registry: 'ghcr.io',
-        repository: 'unstoppablemango/pulumi-kubernetes-operator-nodejs',
-        tag: versions.customImage,
-      },
-    },
-  },
-}, { provider });
+const manifests = new Directory('operator', {
+  namespace: 'pulumi-operator',
+  directory: `https://github.com/pulumi/pulumi-kubernetes-operator/operator/config/default/?ref=v2.0.0-beta.3`,
+});
 
 const accessToken = new AccessToken(`pulumi-operator-${getStack()}`, {
   description: 'Token for the operator to use to deploy stacks',
 });
 
 const secret = new Secret('pulumi-operator', {
-  metadata: {
-    name: 'pulumi-operator',
-    namespace: ns.metadata.name,
-  },
+  metadata: { namespace: 'pulumi-kubernetes-operator' },
   stringData: {
-    accessToken: accessToken.value.apply(required),
+    accessToken: accessToken.value,
   },
-}, { provider });
+});
 
 // const clusterStack = new Stack(stack.name, {
 //   metadata: {
