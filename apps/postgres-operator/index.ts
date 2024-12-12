02f44930815495d241c9dbd1934336d207d7a2d6
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-12-12 00:06:16 -0600

    Settin up more stuff

---
+++ apps/postgres-operator/index.ts
@@ -0,0 +1,22 @@
+import { Namespace } from '@pulumi/kubernetes/core/v1';
+import { Chart } from '@pulumi/kubernetes/helm/v4';
+
+const ns = new Namespace('postgres-operator', {
+  metadata: { namespace: 'postgres-operator' },
+});
+
+const chart = new Chart('operator', {
+  chart: 'postgres-operator',
+  repositoryOpts: {
+    repo: 'https://opensource.zalando.com/postgres-operator/charts/postgres-operator',
+  },
+  namespace: ns.metadata.name,
+});
+
+const uiChart = new Chart('ui', {
+  chart: 'postgres-operator-ui',
+  repositoryOpts: {
+    repo: 'https://opensource.zalando.com/postgres-operator/charts/postgres-operator-ui',
+  },
+  namespace: ns.metadata.name,
+});
