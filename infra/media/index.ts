52ad923e9b0e3938a6fa13bbd0f13b16436daaf0
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-06-19 19:19:30 -0500

    Re-up plex (#3079)
    * The plex config pvc
    
    * Move things around
    
    * Idk something
    
    * Configuring
    
    * Extra env, idfk
    
    * Tidying up

--- infra/media/index.ts
+++ infra/media/index.ts
@@ -1,6 +1,4 @@
-import { Job } from '@pulumi/kubernetes/batch/v1';
-import { Namespace, PersistentVolumeClaim, Pod } from '@pulumi/kubernetes/core/v1';
-import { Wireguard, WireguardPeer } from './crds/nodejs/vpn/v1alpha1';
+import { Namespace, PersistentVolumeClaim, Pod, Secret } from '@pulumi/kubernetes/core/v1';
 import { Chart } from '@pulumi/kubernetes/helm/v4';
 
 const ns = new Namespace('media', {
@@ -10,54 +8,54 @@ const ns = new Namespace('media', {
 const movies = new PersistentVolumeClaim('movies', {
   metadata: { namespace: ns.metadata.name },
   spec: {
-    storageClassName: 'bulk',
-    accessModes: ['ReadWriteOncePod'],
+    storageClassName: 'ec-cephfs',
+    accessModes: ['ReadWriteMany'],
     resources: {
       requests: {
         storage: '20Ti',
       },
     },
   },
-}, { protect: false });
+}, { protect: true });
 
-// const movies4k = new PersistentVolumeClaim('movies4k', {
-//   metadata: { namespace: ns.metadata.name },
-//   spec: {
-//     storageClassName: 'ec-cephfs',
-//     accessModes: ['ReadWriteOnce'],
-//     resources: {
-//       requests: {
-//         storage: '5Ti',
-//       },
-//     },
-//   },
-// }, { protect: false });
+const movies4k = new PersistentVolumeClaim('movies4k', {
+  metadata: { namespace: ns.metadata.name },
+  spec: {
+    storageClassName: 'ec-cephfs',
+    accessModes: ['ReadWriteMany'],
+    resources: {
+      requests: {
+        storage: '10Ti',
+      },
+    },
+  },
+}, { protect: false });
 
 const tv = new PersistentVolumeClaim('tv', {
   metadata: { namespace: ns.metadata.name },
   spec: {
-    storageClassName: 'bulk',
-    accessModes: ['ReadWriteOncePod'],
+    storageClassName: 'ec-cephfs',
+    accessModes: ['ReadWriteMany'],
     resources: {
       requests: {
         storage: '20Ti',
       },
     },
   },
-}, { protect: false });
+}, { protect: true });
 
-// const tv4k = new PersistentVolumeClaim('tv4k', {
-//   metadata: { namespace: ns.metadata.name },
-//   spec: {
-//     storageClassName: 'ec-cephfs',
-//     accessModes: ['ReadWriteOnce'],
-//     resources: {
-//       requests: {
-//         storage: '10Ti',
-//       },
-//     },
-//   },
-// }, { protect: false });
+const tv4k = new PersistentVolumeClaim('tv4k', {
+  metadata: { namespace: ns.metadata.name },
+  spec: {
+    storageClassName: 'ec-cephfs',
+    accessModes: ['ReadWriteMany'],
+    resources: {
+      requests: {
+        storage: '10Ti',
+      },
+    },
+  },
+}, { protect: false });
 
 const anime = new PersistentVolumeClaim('anime', {
   metadata: { namespace: ns.metadata.name },
@@ -85,22 +83,62 @@ const music = new PersistentVolumeClaim('music', {
   },
 }, { protect: true });
 
-const test = new Pod('mounty', {
+const plexConfig = new PersistentVolumeClaim('plex-config', {
   metadata: { namespace: ns.metadata.name },
   spec: {
-    containers: [{
-      name: 'shell',
-      image: 'ubuntu',
-      command: ['bash', '-c', '--'],
-      args: ['while true; do sleep 30; done;'],
-      volumeMounts: [
-        { name: 'movies', mountPath: '/mnt/movies' },
-        { name: 'tv', mountPath: '/mnt/tv' },
-        { name: 'anime', mountPath: '/mnt/anime' },
-        { name: 'music', mountPath: '/mnt/music' },
-      ],
-    }],
-    volumes: [
+    storageClassName: 'ssd-rbd',
+    accessModes: ['ReadWriteOncePod'],
+    resources: {
+      requests: {
+        storage: '500Gi',
+      },
+    },
+  },
+}, { protect: true });
+
+const plex = new Chart('plex', {
+  chart: 'plex-media-server',
+  repositoryOpts: {
+    repo: 'https://raw.githubusercontent.com/plexinc/pms-docker/gh-pages',
+  },
+  name: 'plex',
+  namespace: ns.metadata.name,
+  values: {
+    pms: {
+      configExistingClaim: plexConfig.metadata.name,
+      resources: {
+        limits: {
+          cpu: '16',
+          memory: '32Gi'
+        },
+        requests: {
+          cpu: '4',
+          memory: '4Gi',
+        },
+      },
+    },
+    image: {
+      tag: '1.41.8.9834-071366d65',
+    },
+    extraEnv: {
+      HOSTNAME: 'plex',
+      TZ: 'America/Chicago',
+      // What do the numbers mean
+      PLEX_UPDATE_CHANNEL: "8",
+      ALLOWED_NETWORKS: '192.168.1.1/24',
+    },
+    service: {
+      type: 'LoadBalancer',
+    },
+    extraVolumeMounts: [
+      { name: 'movies', mountPath: '/mnt/movies' },
+      { name: 'movies4k', mountPath: '/mnt/movies4k' },
+      { name: 'tv', mountPath: '/mnt/tv' },
+      { name: 'tv4k', mountPath: '/mnt/tv4k' },
+      { name: 'anime', mountPath: '/mnt/anime' },
+      { name: 'music', mountPath: '/mnt/music' },
+    ],
+    extraVolumes: [
       {
         name: 'movies',
         persistentVolumeClaim: {
@@ -108,9 +146,21 @@ const test = new Pod('mounty', {
         },
       },
       {
+        name: 'movies4k',
+        persistentVolumeClaim: {
+          claimName: movies4k.metadata.name,
+        },
+      },
+      {
         name: 'tv',
         persistentVolumeClaim: {
           claimName: tv.metadata.name,
+        },
+      },
+      {
+        name: 'tv4k',
+        persistentVolumeClaim: {
+          claimName: tv4k.metadata.name,
         },
       },
       {
@@ -126,78 +176,8 @@ const test = new Pod('mounty', {
         },
       },
     ],
+    dnsConfig: {
+      options: [{ name: 'ndots', value: '0' }]
+    },
   },
 });
-
-// const plexConfig = new PersistentVolumeClaim('plex-config', {
-//   metadata: { namespace: ns.metadata.name },
-//   spec: {
-//     storageClassName: 'unsafe-rbd',
-//   },
-// });
-
-// const plex = new Chart('plex', {
-//   chart: 'plex-media-server',
-//   repositoryOpts: {
-//     repo: 'https://raw.githubusercontent.com/plexinc/pms-docker/gh-pages',
-//   },
-// });
-
-// const rsyncScript: string = `
-// #!/bin/bash
-
-// export DEBIAN_FRONTEND=noninteractive
-// apt-get update && apt-get upgrade -y
-// apt-get install -y rsync
-// rsync -avuhp --info=progress2 /mnt/src/ /mnt/dst
-// `;
-
-// const moviesRsync = new Job('movies', {
-//   metadata: {
-//     namespace: ns.metadata.name,
-//     annotations: {
-//       'pulumi.com/skipAwait': 'true',
-//     },
-//   },
-//   spec: {
-//     template: {
-//       spec: {
-//         restartPolicy: 'Never',
-//         containers: [{
-//           name: 'rsync',
-//           image: `ubuntu:noble-20240904.1`,
-//           command: ['bash', '-c', rsyncScript],
-//           volumeMounts: [
-//             { name: 'src', mountPath: '/mnt/src' },
-//             { name: 'dst', mountPath: '/mnt/dst' },
-//           ],
-//           resources: {
-//             requests: {
-//               cpu: '100m',
-//               memory: '4Gi',
-//             },
-//             limits: {
-//               cpu: '8',
-//               memory: '8Gi',
-//             },
-//           },
-//         }],
-//         volumes: [
-//           {
-//             name: 'dst',
-//             persistentVolumeClaim: {
-//               claimName: movies.metadata.name,
-//             },
-//           },
-//           {
-//             name: 'src',
-//             nfs: {
-//               server: '192.168.1.11',
-//               path: '/tank2/media/movies',
-//             },
-//           },
-//         ],
-//       },
-//     },
-//   },
-// }, { dependsOn: movies });
