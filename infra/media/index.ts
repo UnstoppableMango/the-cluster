b855989d79da69afeed311fde982acd92739d7f9
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-06-19 19:18:52 -0500

    Tidying up

--- infra/media/index.ts
+++ infra/media/index.ts
@@ -1,25 +1,9 @@
-import { Job } from '@pulumi/kubernetes/batch/v1';
 import { Namespace, PersistentVolumeClaim, Pod, Secret } from '@pulumi/kubernetes/core/v1';
-import { Wireguard, WireguardPeer } from './crds/nodejs/vpn/v1alpha1';
 import { Chart } from '@pulumi/kubernetes/helm/v4';
-import { Config } from '@pulumi/pulumi';
 
 const ns = new Namespace('media', {
   metadata: { name: 'media' },
 });
-
-const moviesTmp = new PersistentVolumeClaim('movies-tmp', {
-  metadata: { namespace: ns.metadata.name },
-  spec: {
-    storageClassName: 'bulk',
-    accessModes: ['ReadWriteOncePod'],
-    resources: {
-      requests: {
-        storage: '20Ti',
-      },
-    },
-  },
-}, { protect: false });
 
 const movies = new PersistentVolumeClaim('movies', {
   metadata: { namespace: ns.metadata.name },
@@ -32,29 +16,16 @@ const movies = new PersistentVolumeClaim('movies', {
       },
     },
   },
-}, { protect: false });
+}, { protect: true });
 
-// const movies4k = new PersistentVolumeClaim('movies4k', {
-//   metadata: { namespace: ns.metadata.name },
-//   spec: {
-//     storageClassName: 'ec-cephfs',
-//     accessModes: ['ReadWriteOnce'],
-//     resources: {
-//       requests: {
-//         storage: '5Ti',
-//       },
-//     },
-//   },
-// }, { protect: false });
-
-const tvTmp = new PersistentVolumeClaim('tv-tmp', {
+const movies4k = new PersistentVolumeClaim('movies4k', {
   metadata: { namespace: ns.metadata.name },
   spec: {
-    storageClassName: 'bulk',
-    accessModes: ['ReadWriteOncePod'],
+    storageClassName: 'ec-cephfs',
+    accessModes: ['ReadWriteMany'],
     resources: {
       requests: {
-        storage: '20Ti',
+        storage: '10Ti',
       },
     },
   },
@@ -71,20 +42,20 @@ const tv = new PersistentVolumeClaim('tv', {
       },
     },
   },
-}, { protect: false });
+}, { protect: true });
 
-// const tv4k = new PersistentVolumeClaim('tv4k', {
-//   metadata: { namespace: ns.metadata.name },
-//   spec: {
-//     storageClassName: 'ec-cephfs',
-//     accessModes: ['ReadWriteOnce'],
-//     resources: {
-//       requests: {
-//         storage: '10Ti',
-//       },
-//     },
-//   },
-// }, { protect: false });
+const tv4k = new PersistentVolumeClaim('tv4k', {
+  metadata: { namespace: ns.metadata.name },
+  spec: {
+    storageClassName: 'ec-cephfs',
+    accessModes: ['ReadWriteMany'],
+    resources: {
+      requests: {
+        storage: '10Ti',
+      },
+    },
+  },
+}, { protect: false });
 
 const anime = new PersistentVolumeClaim('anime', {
   metadata: { namespace: ns.metadata.name },
@@ -123,67 +94,7 @@ const plexConfig = new PersistentVolumeClaim('plex-config', {
       },
     },
   },
-});
-
-// const test = new Pod('mounty', {
-//   metadata: { namespace: ns.metadata.name },
-//   spec: {
-//     containers: [{
-//       name: 'shell',
-//       image: 'ubuntu',
-//       command: ['bash', '-c', '--'],
-//       args: ['while true; do sleep 30; done;'],
-//       volumeMounts: [
-//         // { name: 'movies', mountPath: '/mnt/movies' },
-//         // { name: 'tv', mountPath: '/mnt/tv' },
-//         { name: 'anime', mountPath: '/mnt/anime' },
-//         { name: 'music', mountPath: '/mnt/music' },
-//         { name: 'plex', mountPath: '/mnt/plex' },
-//       ],
-//     }],
-//     volumes: [
-//       // {
-//       //   name: 'movies',
-//       //   persistentVolumeClaim: {
-//       //     claimName: moviesTmp.metadata.name,
-//       //   },
-//       // },
-//       // {
-//       //   name: 'tv',
-//       //   persistentVolumeClaim: {
-//       //     claimName: tvTmp.metadata.name,
-//       //   },
-//       // },
-//       // {
-//       //   name: 'anime',
-//       //   persistentVolumeClaim: {
-//       //     claimName: anime.metadata.name,
-//       //   },
-//       // },
-//       // {
-//       //   name: 'music',
-//       //   persistentVolumeClaim: {
-//       //     claimName: music.metadata.name,
-//       //   },
-//       // },
-//       // {
-//       //   name: 'plex',
-//       //   persistentVolumeClaim: {
-//       //     claimName: plexConfig.metadata.name,
-//       //   },
-//       // },
-//     ],
-//   },
-// });
-
-const config = new Config();
-
-const plexSecret = new Secret('plex', {
-  metadata: { namespace: ns.metadata.name },
-  stringData: {
-    claim: config.requireSecret('plexClaim'),
-  },
-});
+}, { protect: true });
 
 const plex = new Chart('plex', {
   chart: 'plex-media-server',
@@ -195,13 +106,6 @@ const plex = new Chart('plex', {
   values: {
     pms: {
       configExistingClaim: plexConfig.metadata.name,
-      claimSecret: {
-        name: plexSecret.metadata.name,
-        key: 'claim',
-        // I think this is a bug in the chart
-        // https://github.com/plexinc/pms-docker/blob/master/charts/plex-media-server/templates/statefulset.yaml
-        value: 'blah',
-      },
       resources: {
         limits: {
           cpu: '16',
@@ -213,11 +117,14 @@ const plex = new Chart('plex', {
         },
       },
     },
+    image: {
+      tag: '1.41.8.9834-071366d65',
+    },
     extraEnv: {
       HOSTNAME: 'plex',
       TZ: 'America/Chicago',
       // What do the numbers mean
-      // PLEX_UPDATE_CHANNEL: "5"
+      PLEX_UPDATE_CHANNEL: "8",
       ALLOWED_NETWORKS: '192.168.1.1/24',
     },
     service: {
@@ -225,7 +132,9 @@ const plex = new Chart('plex', {
     },
     extraVolumeMounts: [
       { name: 'movies', mountPath: '/mnt/movies' },
+      { name: 'movies4k', mountPath: '/mnt/movies4k' },
       { name: 'tv', mountPath: '/mnt/tv' },
+      { name: 'tv4k', mountPath: '/mnt/tv4k' },
       { name: 'anime', mountPath: '/mnt/anime' },
       { name: 'music', mountPath: '/mnt/music' },
     ],
@@ -233,13 +142,25 @@ const plex = new Chart('plex', {
       {
         name: 'movies',
         persistentVolumeClaim: {
-          claimName: moviesTmp.metadata.name,
+          claimName: movies.metadata.name,
+        },
+      },
+      {
+        name: 'movies4k',
+        persistentVolumeClaim: {
+          claimName: movies4k.metadata.name,
         },
       },
       {
         name: 'tv',
         persistentVolumeClaim: {
           claimName: tv.metadata.name,
+        },
+      },
+      {
+        name: 'tv4k',
+        persistentVolumeClaim: {
+          claimName: tv4k.metadata.name,
         },
       },
       {
@@ -255,64 +176,8 @@ const plex = new Chart('plex', {
         },
       },
     ],
+    dnsConfig: {
+      options: [{ name: 'ndots', value: '0' }]
+    },
   },
 });
-
-// const rsyncScript: string = `
-// #!/bin/bash
-
-// export DEBIAN_FRONTEND=noninteractive
-// apt-get update && apt-get upgrade -y
-// apt-get install -y rsync
-// rsync -avuhp --info=progress2 /mnt/src/ /mnt/dst
-// `;
-
-// const moviesRsync = new Job('movies', {
-//   metadata: {
-//     namespace: ns.metadata.name,
-//     annotations: {
-//       'pulumi.com/skipAwait': 'true',
-//     },
-//   },
-//   spec: {
-//     template: {
-//       spec: {
-//         restartPolicy: 'Never',
-//         containers: [{
-//           name: 'rsync',
-//           image: `ubuntu:noble-20240904.1`,
-//           command: ['bash', '-c', rsyncScript],
-//           volumeMounts: [
-//             { name: 'src', mountPath: '/mnt/src' },
-//             { name: 'dst', mountPath: '/mnt/dst' },
-//           ],
-//           resources: {
-//             requests: {
-//               cpu: '100m',
-//               memory: '4Gi',
-//             },
-//             limits: {
-//               cpu: '8',
-//               memory: '8Gi',
-//             },
-//           },
-//         }],
-//         volumes: [
-//           {
-//             name: 'dst',
-//             persistentVolumeClaim: {
-//               claimName: movies.metadata.name,
-//             },
-//           },
-//           {
-//             name: 'src',
-//             nfs: {
-//               server: '192.168.1.11',
-//               path: '/tank2/media/movies',
-//             },
-//           },
-//         ],
-//       },
-//     },
-//   },
-// }, { dependsOn: movies });
