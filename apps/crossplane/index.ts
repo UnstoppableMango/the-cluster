9b61ebb47003e6e6675115e2d7b7d94492360f48
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-06-26 20:54:56 -0500

    Adds crossplane application
    Adds the crossplane application using pulumi
    This enables crossplane installation for the cluster.
    It creates a namespace and deploys the crossplane helm chart.

---
+++ apps/crossplane/index.ts
@@ -0,0 +1,17 @@
+import { Namespace } from '@pulumi/kubernetes/core/v1';
+import { Chart } from '@pulumi/kubernetes/helm/v4';
+
+const ns = new Namespace('crossplane-system', {
+  metadata: { name: 'crossplane-system' },
+});
+
+const chart = new Chart('crossplane', {
+  chart: 'crossplane',
+  repositoryOpts: {
+    repo: 'https://charts.crossplane.io/stable',
+  },
+  namespace: ns.metadata.name,
+  values: {
+    replicas: 2,
+  },
+});
