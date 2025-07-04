751cf454c6beb953782220eb36a7f3b74a3eb4e1
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-07-04 01:02:30 -0500

    WIP on pki

--- infra/pki/index.ts
+++ infra/pki/index.ts
@@ -1,41 +1,58 @@
-import { StackReference } from '@pulumi/pulumi';
+import { CustomResource } from '@pulumi/kubernetes/apiextensions';
+import { Secret } from '@pulumi/kubernetes/core/v1';
+import { Config, Output, StackReference } from '@pulumi/pulumi';
 import { CertRequest, LocallySignedCert, PrivateKey } from '@pulumi/tls';
+import * as z from 'zod/v4';
+
+const CA = z.object({
+	certPem: z.string(),
+	privateKeyPem: z.string(),
+});
+
+const CloudflareConfig = z.object({
+	apiToken: z.string(),
+});
+
+type CA = z.infer<typeof CA>;
+type CloudflareConfig = z.infer<typeof CloudflareConfig>;
 
 const _25Years = 25 * 365 * 24;
 const _1Week = 7 * 24;
+
+const config = new Config();
 
 const pki = new StackReference('pki', {
 	name: 'UnstoppableMango/pki/prod',
 });
 
-export const key = new PrivateKey('thecluster.io', {
-	algorithm: 'RSA',
-	rsaBits: 4096,
-});
+const ca = pki.requireOutput('thecluster').apply(CA.parse);
+const cf = config.requireSecretObject('cloudflare').apply(CloudflareConfig.parse);
 
-export const request = new CertRequest('thecluster.io', {
-	privateKeyPem: key.privateKeyPem,
-	subject: {
-		commonName: 'thecluster.io',
-		country: 'US',
-		organization: 'UnMango',
-		organizationalUnit: 'UnstoppableMango',
+const apiTokenKey = 'api-token';
+const secret = new Secret('cloudflare', {
+	metadata: {},
+	stringData: {
+		[apiTokenKey]: cf.apiToken,
 	},
-	uris: [],
-	dnsNames: [],
-	ipAddresses: [],
 });
 
-export const cert = new LocallySignedCert('thecluster.io', {
-	isCaCertificate: true,
-	allowedUses: [
-		'cert_signing',
-		'crl_signing',
-		'digital_signature',
-	],
-	caCertPem: ca.cert.certPem,
-	caPrivateKeyPem: ca.key.privateKeyPem,
-	certRequestPem: request.certRequestPem,
-	validityPeriodHours: _25Years,
-	earlyRenewalHours: _1Week,
+const cfIssuer = new CustomResource('cloudflare', {
+	apiVersion: 'cert-manager.io/v1',
+	kind: 'Issuer',
+	metadata: { namespace: '' },
+	spec: {
+		acme: {
+			server: '',
+			solvers: [{
+				dns01: {
+					cloudflare: {
+						apiTokenSecretRef: {
+							name: secret.metadata.name,
+							key: apiTokenKey,
+						},
+					},
+				},
+			}],
+		},
+	},
 });
