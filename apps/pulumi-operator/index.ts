95db06643615f4cf9eb432458232b1b620628945
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-07-05 02:20:53 -0500

    Limit max concurrent reconciles (#3112)
    Because I is poor and the stacks keep failing on concurrent runs

--- apps/pulumi-operator/index.ts
+++ apps/pulumi-operator/index.ts
@@ -1,7 +1,5 @@
 import { Namespace, Secret } from '@pulumi/kubernetes/core/v1';
 import { Chart } from '@pulumi/kubernetes/helm/v4';
-import { getStack } from '@pulumi/pulumi';
-import { AccessToken } from '@pulumi/pulumiservice';
 import { versions } from './config';
 
 const ns = new Namespace('pulumi-operator', {
@@ -12,6 +10,11 @@ const chart = new Chart('pulumi-operator', {
 	chart: 'oci://ghcr.io/pulumi/helm-charts/pulumi-kubernetes-operator',
 	version: versions.pulumiOperator,
 	namespace: ns.metadata.name,
+	values: {
+		extraEnv: [
+			{ name: 'MAX_CONCURRENT_RECONCILES', value: '1' },
+		],
+	},
 });
 
 export { versions };
