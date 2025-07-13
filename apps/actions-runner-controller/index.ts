28c489fdbcb1f5ac2c7a959d0d838f19b49f6ce3
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-07-13 00:43:50 -0500

    Set up some actions runners for the unmango org (#3126)

--- apps/actions-runner-controller/index.ts
+++ apps/actions-runner-controller/index.ts
@@ -1,5 +1,16 @@
 import { Namespace } from '@pulumi/kubernetes/core/v1';
 import { Chart } from '@pulumi/kubernetes/helm/v4';
+import { Config } from '@pulumi/pulumi';
+import z from 'zod/v4';
+
+const Versions = z.object({
+	chart: z.string(),
+});
+
+type Versions = z.infer<typeof Versions>;
+
+const config = new Config();
+const versions = Versions.parse(config.requireObject('versions'));
 
 const ns = new Namespace('arc-system', {
 	metadata: { name: 'arc-system' },
@@ -7,7 +18,7 @@ const ns = new Namespace('arc-system', {
 
 const chart = new Chart('gha-runner-scale-set', {
 	chart: 'oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set-controller',
-	version: '0.10.1',
+	version: versions.chart,
 	namespace: ns.metadata.name,
 	values: {
 		replicaCount: 2,
