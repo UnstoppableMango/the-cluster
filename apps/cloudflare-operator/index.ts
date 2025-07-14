8382e7ed5536b18cb08e28fc80c0e9ef9d40d1f4
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-07-13 20:33:14 -0500

    Struggle bussing, but we got there

--- apps/cloudflare-operator/index.ts
+++ apps/cloudflare-operator/index.ts
@@ -1,21 +1,56 @@
-import { kustomize } from '@pulumi/kubernetes';
-import { Secret } from '@pulumi/kubernetes/core/v1';
+import z from 'zod/v4';
+import { Config } from '@pulumi/pulumi';
+import { Namespace, Secret } from '@pulumi/kubernetes/core/v1';
 import { cloudflare } from './config';
+import { Chart } from '@pulumi/kubernetes/helm/v4';
 
-const kustomization = new kustomize.Directory('cloudflare-operator', {
-	directory: 'https://github.com/unmango/cloudflare-operator.git/config/default?ref=v0.0.2&submodules=false',
+const Versions = z.object({
+	chart: z.string(),
+	cloudflareOperator: z.string(),
 });
 
-const ns = kustomization.getResource('v1/Namespace', 'cloudflare-operator-system');
+type Versions = z.infer<typeof Versions>;
+
+const config = new Config();
+const versions = Versions.parse(config.requireObject('versions'));
+
+const ns = new Namespace('cloudflare-system', {
+	metadata: { name: 'cloudflare-system' },
+});
+
+const apiTokenKey = 'api-token';
 
 const secret = new Secret('api-secrets', {
-	metadata: {
-		name: 'api-secrets',
-		namespace: ns.metadata.name,
-	},
+	metadata: { namespace: ns.metadata.name },
 	stringData: {
-		CLOUDFLARE_API_TOKEN: cloudflare.apiToken,
-		CLOUDFLARE_API_KEY: cloudflare.globalApiKey,
+		[apiTokenKey]: cloudflare.apiToken,
+	},
+});
+
+const chart = new Chart('cloudflare-operator', {
+	chart: 'cloudflare-operator',
+	version: versions.chart,
+	repositoryOpts: {
+		repo: 'https://unmango.github.io/cloudflare-operator',
+	},
+	namespace: ns.metadata.name,
+	values: {
+		auth: {
+			enabled: true,
+			apiTokenRef: {
+				name: secret.metadata.name,
+				key: apiTokenKey,
+			},
+		},
+		controllerManager: {
+			replicas: 2,
+			container: {
+				image: {
+					repository: 'ghcr.io/unmango/cloudflare-operator',
+					tag: versions.cloudflareOperator,
+				},
+			},
+		},
 	},
 });
 
