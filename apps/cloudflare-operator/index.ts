3d07fa5894173c74d83248bb330d38d32584c374
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-07-13 20:37:29 -0500

    Switch to the home grown cloudflare operator (#3128)
    * Switch to the other cloudflare operator
    
    * Struggle bussing, but we got there
    
    * Some config stuff

--- apps/cloudflare-operator/index.ts
+++ apps/cloudflare-operator/index.ts
@@ -1,21 +1,61 @@
-import { kustomize } from '@pulumi/kubernetes';
-import { Secret } from '@pulumi/kubernetes/core/v1';
-import { cloudflare } from './config';
+import { Namespace, Secret } from '@pulumi/kubernetes/core/v1';
+import { Chart } from '@pulumi/kubernetes/helm/v4';
+import { Config } from '@pulumi/pulumi';
+import z from 'zod/v4';
 
-const kustomization = new kustomize.Directory('cloudflare-operator', {
-	directory: './',
+const Cloudflare = z.object({
+	apiToken: z.string(),
 });
 
-const ns = kustomization.getResource('v1/Namespace', 'cloudflare-operator-system');
+const Versions = z.object({
+	chart: z.string(),
+	cloudflareOperator: z.string(),
+});
+
+type Cloudflare = z.infer<typeof Cloudflare>;
+type Versions = z.infer<typeof Versions>;
+
+const config = new Config();
+const cloudflare = Cloudflare.parse(config.requireObject('cloudflare'));
+const versions = Versions.parse(config.requireObject('versions'));
+
+const ns = new Namespace('cloudflare-system', {
+	metadata: { name: 'cloudflare-system' },
+});
+
+const apiTokenKey = 'api-token';
 
 const secret = new Secret('api-secrets', {
-	metadata: {
-		name: 'api-secrets',
-		namespace: ns.metadata.name,
-	},
+	metadata: { namespace: ns.metadata.name },
 	stringData: {
-		CLOUDFLARE_API_TOKEN: cloudflare.apiToken,
-		CLOUDFLARE_API_KEY: cloudflare.globalApiKey,
+		[apiTokenKey]: cloudflare.apiToken,
+	},
+});
+
+const chart = new Chart('cloudflare-operator', {
+	chart: 'cloudflare-operator',
+	version: versions.chart,
+	repositoryOpts: {
+		repo: 'https://unmango.github.io/cloudflare-operator',
+	},
+	namespace: ns.metadata.name,
+	values: {
+		auth: {
+			enabled: true,
+			apiTokenRef: {
+				name: secret.metadata.name,
+				key: apiTokenKey,
+			},
+		},
+		controllerManager: {
+			replicas: 2,
+			container: {
+				image: {
+					repository: 'ghcr.io/unmango/cloudflare-operator',
+					tag: versions.cloudflareOperator,
+				},
+			},
+		},
 	},
 });
 
