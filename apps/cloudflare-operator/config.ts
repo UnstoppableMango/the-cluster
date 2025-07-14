cc01352891fa9e0fa1149312f749032f79559850
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-07-13 20:36:23 -0500

    Some config stuff

--- apps/cloudflare-operator/config.ts
+++
@@ -1,9 +0,0 @@
-import { Config, getStack } from '@pulumi/pulumi';
-
-export interface Cloudflare {
-	apiToken: string;
-	globalApiKey: string;
-}
-
-const config = new Config();
-export const cloudflare = config.requireObject<Cloudflare>('cloudflare');
--- apps/cloudflare-operator/index.ts
+++ apps/cloudflare-operator/index.ts
@@ -1,17 +1,22 @@
-import z from 'zod/v4';
-import { Config } from '@pulumi/pulumi';
 import { Namespace, Secret } from '@pulumi/kubernetes/core/v1';
-import { cloudflare } from './config';
 import { Chart } from '@pulumi/kubernetes/helm/v4';
+import { Config } from '@pulumi/pulumi';
+import z from 'zod/v4';
+
+const Cloudflare = z.object({
+	apiToken: z.string(),
+});
 
 const Versions = z.object({
 	chart: z.string(),
 	cloudflareOperator: z.string(),
 });
 
+type Cloudflare = z.infer<typeof Cloudflare>;
 type Versions = z.infer<typeof Versions>;
 
 const config = new Config();
+const cloudflare = Cloudflare.parse(config.requireObject('cloudflare'));
 const versions = Versions.parse(config.requireObject('versions'));
 
 const ns = new Namespace('cloudflare-system', {
