1eda13d4bf81c5526e7309129e3c192358b3e31c
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-07-14 21:55:13 -0500

    Hardcode the original mon configuration

--- apps/rook/index.ts
+++ apps/rook/index.ts
@@ -1,6 +1,7 @@
 import z from 'zod/v4';
 import * as k8s from '@pulumi/kubernetes';
 import { Config } from '@pulumi/pulumi';
+import { ConfigMap, Secret } from '@pulumi/kubernetes/core/v1';
 
 const Versions = z.object({
 	chart: z.string(),
@@ -13,6 +14,34 @@ const versions = Versions.parse(config.requireObject('versions'));
 
 const ns = new k8s.core.v1.Namespace('rook-ceph', {
 	metadata: { name: 'rook-ceph' },
+});
+
+const rookCephMonSecret = new Secret('rook-ceph-mon', {
+	metadata: {
+		name: 'rook-ceph-mon',
+		namespace: ns.metadata.name,
+		finalizers: ['ceph.rook.io/disaster-protection'],
+	},
+	stringData: {
+		'ceph-secret': config.requireSecret('ceph-secret'),
+		'ceph-username': config.requireSecret('ceph-username'),
+		'mon-secret': config.requireSecret('mon-secret'),
+		fsid: config.requireSecret('fsid'),
+	},
+});
+
+const rookCephMonEndpoints = new ConfigMap('rook-ceph-mon-endpoints', {
+	metadata: {
+		name: 'rook-ceph-mon-endpoints',
+		namespace: ns.metadata.name,
+		finalizers: ['ceph.rook.io/disaster-protection'],
+	},
+	data: {
+		'csi-cluster-config-json': '[{"clusterID":"rook-ceph","monitors":["10.43.98.167:6789","10.43.75.167:6789","10.43.59.5"],"namespace":""}]',
+		data: 'c=10.43.98.167:6789,d=10.43.75.167:6789,b=10.43.59.5',
+		mapping: '{"node":{"c":{"Name":"castor","Hostname":"castor","Address":"192.168.1.13"},"b":{"Name":"zeus","Hostname":"zeus","Address":"192.168.1.10"},"d":{"Name":"zeus","Hostname":"zeus","Address":"192.168.1.10"}}}',
+		maxMonId: '4',
+	},
 });
 
 const chart = new k8s.helm.v3.Chart('rook', {
