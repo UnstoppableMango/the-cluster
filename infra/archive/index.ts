2c26d7669df4a48dd2b6d429666d3ad21911d99c
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-08-13 22:23:40 -0500

    fmt

--- infra/archive/index.ts
+++ infra/archive/index.ts
@@ -2,46 +2,46 @@ import { Job } from '@pulumi/kubernetes/batch/v1';
 import { Namespace, PersistentVolumeClaim, Pod } from '@pulumi/kubernetes/core/v1';
 
 const ns = new Namespace('archive', {
-  metadata: { name: 'archive' },
+	metadata: { name: 'archive' },
 });
 
 const defaultPvc = new PersistentVolumeClaim('default', {
-  metadata: {
-    name: 'default',
-    namespace: ns.metadata.name,
-  },
-  spec: {
-    accessModes: ['ReadWriteMany'],
-    storageClassName: 'default-cephfs',
-    resources: {
-      requests: {
-        storage: '1Ti',
-      },
-    },
-  },
+	metadata: {
+		name: 'default',
+		namespace: ns.metadata.name,
+	},
+	spec: {
+		accessModes: ['ReadWriteMany'],
+		storageClassName: 'default-cephfs',
+		resources: {
+			requests: {
+				storage: '1Ti',
+			},
+		},
+	},
 });
 
 const test = new Pod('mounty', {
-  metadata: { namespace: ns.metadata.name },
-  spec: {
-    containers: [{
-      name: 'shell',
-      image: 'ubuntu',
-      command: ['bash', '-c', '--'],
-      args: ['while true; do sleep 30; done;'],
-      volumeMounts: [
-        { name: 'default', mountPath: '/mnt' },
-      ],
-    }],
-    volumes: [
-      {
-        name: 'default',
-        persistentVolumeClaim: {
-          claimName: defaultPvc.metadata.name,
-        },
-      },
-    ],
-  },
+	metadata: { namespace: ns.metadata.name },
+	spec: {
+		containers: [{
+			name: 'shell',
+			image: 'ubuntu',
+			command: ['bash', '-c', '--'],
+			args: ['while true; do sleep 30; done;'],
+			volumeMounts: [
+				{ name: 'default', mountPath: '/mnt' },
+			],
+		}],
+		volumes: [
+			{
+				name: 'default',
+				persistentVolumeClaim: {
+					claimName: defaultPvc.metadata.name,
+				},
+			},
+		],
+	},
 });
 
 const rsyncScript: string = `
@@ -54,60 +54,60 @@ rsync -avuhp --info=progress2 /mnt/src/ /mnt/dst
 `;
 
 const rsync = new Job('vm-110', {
-  metadata: {
-    namespace: ns.metadata.name,
-    annotations: {
-      'pulumi.com/skipAwait': 'true',
-    },
-  },
-  spec: {
-    template: {
-      spec: {
-        nodeSelector: {
-          'kubernetes.io/hostname': 'zeus',
-        },
-        restartPolicy: 'Never',
-        containers: [{
-          name: 'rsync',
-          image: `ubuntu:noble-20240904.1`,
-          command: ['bash', '-c', rsyncScript],
-          volumeMounts: [
-            { name: 'src', mountPath: '/mnt/src' },
-            { name: 'dst', mountPath: '/mnt/dst', subPath: 'vms/vm-110-disk-0' },
-          ],
-          resources: {
-            requests: {
-              cpu: '100m',
-              memory: '4Gi',
-            },
-            limits: {
-              cpu: '8',
-              memory: '8Gi',
-            },
-          },
-        }],
-        volumes: [
-          {
-            name: 'dst',
-            persistentVolumeClaim: {
-              claimName: defaultPvc.metadata.name,
-            },
-          },
-          // {
-          //   name: 'src',
-          //   nfs: {
-          //     server: '192.168.1.10',
-          //     path: '/mnt',
-          //   },
-          // },
-          {
-            name: 'src',
-            hostPath: {
-              path: '/mnt',
-            },
-          },
-        ],
-      },
-    },
-  },
+	metadata: {
+		namespace: ns.metadata.name,
+		annotations: {
+			'pulumi.com/skipAwait': 'true',
+		},
+	},
+	spec: {
+		template: {
+			spec: {
+				nodeSelector: {
+					'kubernetes.io/hostname': 'zeus',
+				},
+				restartPolicy: 'Never',
+				containers: [{
+					name: 'rsync',
+					image: `ubuntu:noble-20240904.1`,
+					command: ['bash', '-c', rsyncScript],
+					volumeMounts: [
+						{ name: 'src', mountPath: '/mnt/src' },
+						{ name: 'dst', mountPath: '/mnt/dst', subPath: 'vms/vm-110-disk-0' },
+					],
+					resources: {
+						requests: {
+							cpu: '100m',
+							memory: '4Gi',
+						},
+						limits: {
+							cpu: '8',
+							memory: '8Gi',
+						},
+					},
+				}],
+				volumes: [
+					{
+						name: 'dst',
+						persistentVolumeClaim: {
+							claimName: defaultPvc.metadata.name,
+						},
+					},
+					// {
+					//   name: 'src',
+					//   nfs: {
+					//     server: '192.168.1.10',
+					//     path: '/mnt',
+					//   },
+					// },
+					{
+						name: 'src',
+						hostPath: {
+							path: '/mnt',
+						},
+					},
+				],
+			},
+		},
+	},
 }, { dependsOn: defaultPvc });
