f680afdc40f8e2a3505311baa8e3b95935a1989c
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-08-31 15:14:32 -0500

    Remove thecluster-stacks

--- infra/stacks/config.ts
+++
@@ -1,9 +0,0 @@
-import { Config } from '@pulumi/pulumi';
-
-interface Versions {
-	pulumiImage: string;
-}
-
-const config = new Config();
-
-export const versions = config.requireObject<Versions>('versions');
