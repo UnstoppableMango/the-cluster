e61e50ae1794b0a53c9690637e00188cf944b1f1
Erik Rasmussen <erik.rasmussen@unmango.dev>
2026-04-19 15:21:33 -0500

    Move external-snapshotter into flux (#3883)

--- apps/external-snapshotter/index.ts
+++ apps/external-snapshotter/index.ts
@@ -1,12 +1,2 @@
-import * as k8s from '@pulumi/kubernetes';
-import { versions } from './config';
-
-const crds = new k8s.kustomize.Directory('snapshot-controller-crds', {
-	directory:
-		`https://github.com/kubernetes-csi/external-snapshotter/tree/${versions.externalSnapshotter}/client/config/crd`,
-});
-
-new k8s.kustomize.Directory('snapshot-controller', {
-	directory:
-		`https://github.com/kubernetes-csi/external-snapshotter/tree/${versions.externalSnapshotter}/deploy/kubernetes/snapshot-controller`,
-}, { dependsOn: crds.ready });
+// External snapshotter is now managed by Flux.
+// See flux/infrastructure/controllers/flux-system/external-snapshotter.yml
