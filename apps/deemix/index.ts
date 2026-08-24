06a0e2a395a512c9aede7971957a6ab98144df11
UnstoppableMango <erik.rasmussen@unmango.dev>
2026-08-24 05:17:55 -0500

    chore: Removing all pulumi code

--- apps/deemix/index.ts
+++
@@ -1,3 +0,0 @@
-// Kubernetes resources for deemix are now managed by Flux (flux/apps/deemix/).
-// This stack only provisions the Keycloak client used for oauth2-proxy auth.
-export * from './oauth';
