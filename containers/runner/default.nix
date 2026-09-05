230defac8147b4a0f66ca1bcbf88945301813bbc
Erik Rasmussen <erik.rasmussen@unmango.dev>
2026-09-04 21:31:10 -0500

    deps: bump the actions runner to 2.337.0 (#4203)
    2.334.0 is deprecated, so GitHub will start refusing registrations from it.
    
    The manifests still pin v2.334.0 by digest and cannot move until CI publishes
    v2.337.0, since the digest does not exist before the push. They follow in a
    second commit.

--- containers/runner/default.nix
+++ containers/runner/default.nix
@@ -6,10 +6,10 @@
         # https://github.com/actions/runner/tree/main/images/Dockerfile
         fromImage = pkgs.dockerTools.pullImage {
           imageName = "ghcr.io/actions/actions-runner";
-          imageDigest = "sha256:ee54ad8776606f29434f159196529b7b9c83c0cb9195c1ff5a7817e7e570dcfe";
-          hash = "sha256-XQFr8ppWDztVi7f6e+6KhzppqWQg+5uVvmDQNMd41CI=";
+          imageDigest = "sha256:e5496277be5d09bc968b3d64911b74e219ac4a3f2edce956a3ecf9271bea1ef4";
+          hash = "sha256-8kvFXvzLgLUssnF/ktQ3cx2/0omYybEZP9sSc6n5cJY=";
           finalImageName = "ghcr.io/actions/actions-runner";
-          finalImageTag = "2.330.0";
+          finalImageTag = "2.337.0";
         };
 
         name = "thecluster-runner";
